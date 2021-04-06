82b37b64fc208c48749906982cee1e9645cd53f5
moriya <g.ff.k.96@gmail.com>
2021-04-06 17:36:21 +0900

    vueプロジェクト

---
+++ vue/vite_app/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <HelloWorld title="Hello" message="※これは Vue3のサンプルプロジェクトです。" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
