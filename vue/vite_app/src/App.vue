9d4a577e7ad43466f449b0af1835e8e138cfa531
moriya <g.ff.k.96@gmail.com>
2021-04-08 13:13:48 +0900

    計算アプリケーションサンプル

--- vue/vite_app/src/App.vue
+++ vue/vite_app/src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <HelloWorld v-bind:title="message" v-on:result-event="appAction"/>
-  <hr>
-  <p>{{ result }}</p>
+    <Calc v-bind:title="message" v-on:result-event="appAction"/>
+    <div class="mt-3 text-left">
+        <table class="table" v-html="log"></table>
+    </div>
+    <div>
+        <button class="btn btn-danger" v-on:click="doClear">Clear log</button>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calc from './components/Calc.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Calc
   },
   data(){
     return{
-      message:'HELLO',
-      result:'no event.',
+      message:'CALC',
+      result:[],
     }
   },
+  computed:{
+    log(){
+      let table = '<tr><th>Expression</th><th>Value</th></tr>'
+      if (this.result.length > 0){
+        for(var i in this.result){
+          table += '<tr><td>' + this.result[i][0] + '</td><th>' + this.result[i][1] + '</th></tr>'
+        }
+      }
+      return table
+    }
+  },
+  created(){
+    let items = localStorage.getItem('log')
+    let logs = JSON.parse(items)
+    if (logs != null){ this.result = logs }
+  },
   methods:{
-    appAction(message){
-      this.result = '(*** you send:"' + message + '". ***)'
+    appAction(exp, res){
+      this.result.unshift([exp, res])
+      var log = JSON.stringify(this.result)
+      localStorage.setItem('log', log)
+    },
+    doClear(){
+      if (confirm('ログをすべて消去します。')){
+        localStorage.removeItem('log')
+        this.result = []
+      }
     }
   }
 }
