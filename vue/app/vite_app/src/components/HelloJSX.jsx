33312faea613d5d95ebc40aecd0595bc964acc3e
moriya <g.ff.k.96@gmail.com>
2021-04-18 23:32:51 +0900

    クラス属性を埋め込み

--- vue/app/vite_app/src/components/HelloJSX.jsx
+++ vue/app/vite_app/src/components/HelloJSX.jsx
@@ -4,11 +4,17 @@ export default{
         title:String,
         msg:String
     },
+    data(){
+        return{
+            cls_title: 'text-danger h1',
+            cls_msg: 'text-primary h5'
+        }
+    },
     render(h){
         return(
             <div class="alert alert-primary">
-                <h2>{this.title}</h2>
-                <p>{this.msg}</p>
+                <h2 class={this.cls_title}>{this.title}</h2>
+                <p class={this.cls_msg}>{this.msg}</p>
             </div>
         )
     }
