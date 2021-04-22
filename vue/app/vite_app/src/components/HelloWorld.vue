7eb41186f5c0122ce1831ddeabc9f07335480846
moriyakazunori <k.moriya@l-owcal.com>
2021-04-22 14:18:24 +0900

    watchersの使い方

--- vue/app/vite_app/src/components/HelloWorld.vue
+++ vue/app/vite_app/src/components/HelloWorld.vue
@@ -3,50 +3,46 @@
         <h1>{{ title }}</h1>
         <p>{{ message }}</p>
         <hr>
-        <p class="h5">val:{{ val }}</p>
         <div class="form-group text-left">
-            <label>* 2:</label>
-            <input type="number" v-model="a" class="form-control">
+            <label>Value:</label>
+            <input type="number" v-model="val" class="form-control">
         </div>
-        <div class="form-group text-left">
-            <label>^ 2:</label>
-            <input type="number" v-model="b" class="form-control">
-        </div>
+        <table>
+            <tr><th>add:</th><td>{{ add }}</td></tr>
+            <tr><th>sub:</th><td>{{ sub }}</td></tr>
+            <tr><th>multiple:</th><td>{{ multiple }}</td></tr>
+            <tr><th>divide:</th><td>{{ div }}</td></tr>
+        </table>
     </div>
 </template>
 
 <script>
 export default {
     name: 'HelloWorld',
-    props: {
-        title: String,
-    },
     data() {
         return{
-            message: '算術プロパティの使用',
+            title: 'Watchers',
+            message: '値の監視',
             val: 0,
+            add: 0,
+            sub: 0,
+            multiple: 0,
+            div:0,
         }
     },
-    computed:{
-        a: {
-            get(){
-                return this.val *2
-            },
-            set(value){
-                this.val = Math.floor(value)/2
-            },
-        },
-        b: {
-            get(){
-                return this.val * this.val
-            },
-            set(value){
-                this.val = Math.floor(Math.sqrt(value))
-            },
-        },
-        created(){
-            this.val = 10
-        },
-    }
+    watch:{
+        val(newValue, oldValue){
+            console.log(oldValue + '-> ' + newValue)
+            this.val = newValue
+            var val = parseInt(this.val)
+            this.add = Math.floor(val + 2)
+            this.sub = Math.floor(val - 2)
+            this.multiple = Math.floor(val * 2)
+            this.div = Math.floor(val / 2)
+        }
+    },
+    created(){
+        this.val = 6
+    },
 }
 </script>
