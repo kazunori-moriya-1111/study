26c6349acb9c650d54d7f2de35ce02f7f1f78c62
moriya <g.ff.k.96@gmail.com>
2021-04-23 18:27:48 +0900

    キーインベントの実装

--- vue/app/vite_app/src/components/HelloWorld.vue
+++ vue/app/vite_app/src/components/HelloWorld.vue
@@ -3,12 +3,7 @@
         <h1>{{ title }}</h1>
         <pre v-on:click="clear">{{ message }}</pre>
         <hr>
-        <div id="out" class="out" v-on:click="a_event">A
-            <div id="mid" class="mid" v-on:click.self="b_event">B
-                <div id="in" class="in" v-on:click="c_event">C
-                </div>
-            </div>
-        </div>
+        <div><input type="text" v-on:keydown="type" class="form-control"></div>
     </div>
 </template>
 
@@ -18,18 +13,16 @@ export default {
     data: function() {
         return{
             title: 'Event',
-            message: 'イベントの伝播について。\n',
+            message: '',
         }
     },
     methods:{
-        a_event(event){
-            this.message += "A-Event [" + event.target.id + '→' + event.currentTarget .id + "]\n"
-        },
-        b_event(event){
-            this.message += "B-Event [" + event.target.id + '→' + event.currentTarget .id + "]\n"
-        },
-        c_event(event){
-            this.message += "C-Event [" + event.target.id + '→' + event.currentTarget .id + "]\n"
+        type(event){
+            this.message += event.key + ' '
+            if (event.key == "Escape"){
+                this.message = ''
+            }
+            event.target.value = ''
         },
         clear(){
             this.message = "イベントの伝播について。\n"
@@ -37,28 +30,3 @@ export default {
     },
 }
 </script>
-
-<style>
-pre {
-    font-size:16pt;
-    line-height: 1.25;
-}
-div.out {
-    padding: 0px;
-    background-color: #eee;
-    width:300px;
-    height:200px;
-}
-div.mid {
-    padding: 0px;
-    background-color: #ddd;
-    width:200px;
-    height:170px;
-}
-div.in {
-    padding: 0px;
-    background-color: #ccc;
-    width:100px;
-    height:140px;
-}
-</style>
