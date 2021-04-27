a06e6b3f66e28a4f73f44c0fdd5f3afab7f13ce6
moriya <g.ff.k.96@gmail.com>
2021-04-27 13:21:05 +0900

    JSXでコンポーネントを使用する

--- vue/app/vite_app/src/App.vue
+++ vue/app/vite_app/src/App.vue
@@ -1,14 +1,14 @@
 <template>
-    <HelloWorld title="Composition API" msg="This is Composition API sample" />
+    <HelloJSX title="Composition API" msg="This is Composition API sample" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloJSX from './components/HelloJSX.jsx'
 
 export default {
     name: 'App',
     components: {
-        HelloWorld
+        HelloJSX
     },
 }
 </script>
