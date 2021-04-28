5714386c0bd44b64c35402f1cd6e9603de88cb0d
moriya <g.ff.k.96@gmail.com>
2021-04-28 18:07:56 +0900

    reactiveによる参照

--- vue/app/vite_app/src/components/HelloWorld.vue
+++ vue/app/vite_app/src/components/HelloWorld.vue
@@ -6,19 +6,19 @@
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 
 export default {
     props: {
         title:String
     },
     setup(props){
-        const data = ref({
+        const data = reactive({
             msg: 'This is ref-value!',
             count: 0
         })
         setInterval(()=>{
-            data.value.count++
+            data.count++
         }, 1000)
         return{
             data
