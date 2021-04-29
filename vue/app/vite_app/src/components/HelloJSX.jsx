c8fb3585feb4a18007c4d3b964afdc731b275abb
moriya <g.ff.k.96@gmail.com>
2021-04-29 14:31:51 +0900

    URLパラメータの使い方

--- vue/app/vite_app/src/components/HelloJSX.jsx
+++ vue/app/vite_app/src/components/HelloJSX.jsx
@@ -2,13 +2,16 @@ import {ref, reactive} from 'vue'
 
 export default{
     name: 'HelloJSX',
+    props:{
+        name: String
+    },
     setup(props){
         const data = reactive({
             title: 'HelloJSX',
             msg: 'This is JSX component sample.',
         })
         return() => <div class="alert alert-primary">
-            <h1>{data.title}</h1>
+            <h1>{data.title} [{props.name}]</h1>
             <p>{data.msg}</p>
             </div>
     }
--- vue/app/vite_app/src/components/HelloWorld.vue
+++ vue/app/vite_app/src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="alert alert-primary">
-        <h1>{{data.title}}</h1>
+        <h1>{{data.title + '[' + name + ']'}}</h1>
         <p class="mt-3 h5">{{data.msg}}</p>
     </div>
 </template>
@@ -10,9 +10,12 @@ import { ref, reactive } from 'vue'
 
 export default {
     name: 'HelloWorld',
+    props:{
+        name: String,
+    },
     setup(props, context){
         const data = reactive({
-            title: 'HelloWorld',
+            title: 'Router',
             msg: 'This is HelloWorld component.',
         })
         return{
