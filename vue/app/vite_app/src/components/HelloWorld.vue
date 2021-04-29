12588e86b0eda5beae2e8d1130dbd86615b53079
moriya <g.ff.k.96@gmail.com>
2021-04-29 19:23:18 +0900

    複数のミューテーションを実行するアクションの使い方

--- vue/app/vite_app/src/components/HelloWorld.vue
+++ vue/app/vite_app/src/components/HelloWorld.vue
@@ -4,9 +4,7 @@
         <p class="mt-3 h5">{{$store.state.message}}</p>
         <hr>
         <div class="alert alert-secondary"
-            @click.exact="$store.commit({type:'count', message:'add 1!', add:1})"
-            @click.shift="$store.commit({type:'count', message:'add 5!', add:5})"
-            @click.ctrl="$store.commit({type:'count', message:'add 10!', add:10})">
+            @click="$store.dispatch('doit')">
             <a class="h5"
                 @click.stop="$store.commit('reset')">
                 clicked: {{ $store.state.counter }}
