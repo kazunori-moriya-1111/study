4105bcc278d5803f61235424b8ecb675e821e71c
moriya <g.ff.k.96@gmail.com>
2021-04-29 19:08:04 +0900

    ミューテーションの引数にオブジェクトを設定する

--- vue/app/vite_app/src/components/HelloWorld.vue
+++ vue/app/vite_app/src/components/HelloWorld.vue
@@ -3,10 +3,10 @@
         <h1>{{data.title}}</h1>
         <p class="mt-3 h5">{{$store.state.message}}</p>
         <hr>
-        <div class="alert alert-dark"
-            @click.exact="$store.commit('count' ,1)"
-            @click.shift="$store.commit('count' ,2)"
-            @click.ctrl="$store.commit('count' ,3)">
+        <div class="alert alert-secondary"
+            @click.exact="$store.commit({type:'count', message:'add 1!', add:1})"
+            @click.shift="$store.commit({type:'count', message:'add 5!', add:5})"
+            @click.ctrl="$store.commit({type:'count', message:'add 10!', add:10})">
             <a class="h5"
                 @click.stop="$store.commit('reset')">
                 clicked: {{ $store.state.counter }}
