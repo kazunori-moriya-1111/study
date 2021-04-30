e09bbeccb88d8d4b1b77c69dd97d1a8a9b41a49e
moriya <g.ff.k.96@gmail.com>
2021-04-30 14:46:26 +0900

    vue側でget処理を追加

--- vue/app/vite_app/src/components/HelloWorld.vue
+++ vue/app/vite_app/src/components/HelloWorld.vue
@@ -1,29 +1,33 @@
 <template>
-    <div class="alert alert-primary">
+    <section class="alert alert-primary">
         <h1>{{data.title}}</h1>
-        <p class="mt-3 h5">{{$store.state.message}}</p>
-        <hr>
-        <div class="alert alert-dark"
-            @click="$store.commit('doit')">
-            <a class="h5"
-                @click.stop="$store.commit('reset')">
-                clicked: {{ $store.state.counter }}
-            </a>
-        </div>
-    </div>
+        <p>{{data.message}}</p>
+        <textarea v-model="data.mydata" rows="5" class="form-control"></textarea>
+    </section>
 </template>
 
 <script>
-import { ref, reactive } from 'vue'
+import axios from 'axios'
+import { reactive, onMounted } from 'vue'
+
+const url = "http://127.0.0.1:8000/v1/"
 
 export default {
     setup(props){
         const data = reactive({
-            title: 'Vuex',
+            title: 'Axios',
+            message: 'This is axios sample.',
+            mydata: '',
         })
-        return{
-            data
+        const getData = async()=>{
+            let result = await axios.get(url)
+            console.log(result.data)
+            data.mydata = result.data
         }
-    }
+        onMounted(()=>{
+            getData()
+        })
+        return{ data, getData}
+    },
 }
 </script>
