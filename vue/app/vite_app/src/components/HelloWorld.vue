d7b66e4a0c3b0d455a3a1bc62d09675e7962beab
moriya <g.ff.k.96@gmail.com>
2021-05-04 08:30:14 +0900

    idでグレップしたjsonデータを取得する

--- vue/app/vite_app/src/components/HelloWorld.vue
+++ vue/app/vite_app/src/components/HelloWorld.vue
@@ -2,23 +2,27 @@
     <section class="alert alert-primary">
         <h1>{{data.title}}</h1>
         <p>{{data.message}}</p>
+        <div class="form-group">
+            <input type="number" class="form-control" v-model="data.id">
+            <button class="btn btn-primary m-2" @click="doClick">click</button>
+        </div>
         <table class="table table-light table-striped">
             <tbody class="text-left">
                 <tr>
                     <th style="width:200px;">ID</th>
-                    <td>{{data.json_data.id}}</td>
+                    <td>{{data.json_data ? data.json_data.id : '-'}}</td>
                 </tr>
                 <tr>
                     <th>User ID</th>
-                    <td>{{data.json_data.user_id}}</td>
+                    <td>{{data.json_data ? data.json_data.user_id : '-'}}</td>
                 </tr>
                 <tr>
                     <th>Title</th>
-                    <td>{{data.json_data.title}}</td>
+                    <td>{{data.json_data ? data.json_data.title : '-'}}</td>
                 </tr>
                 <tr>
                     <th>Body</th>
-                    <td>{{data.json_data.body}}</td>
+                    <td>{{data.json_data ? data.json_data.body : '-'}}</td>
                 </tr>
             </tbody>
         </table>
@@ -29,23 +33,22 @@
 import axios from 'axios'
 import { reactive, onMounted } from 'vue'
 
+let url = "http://127.0.0.1:8000/v1/"
+
 export default {
     setup(props){
         const data = reactive({
             title: 'Axios',
             message: 'This is axios sample.',
+            id:0,
             json_data: '',
         })
-        const getData = async()=>{
-            const url = "http://127.0.0.1:8000/v1/1"
-            axios.get(url).then((result)=>{
+        const doClick = ()=>{
+            axios.get(url + data.id).then((result)=>{
                 data.json_data = result.data
             })
         }
-        onMounted(()=>{
-            getData()
-        })
-        return{ data, getData}
+        return{ data, doClick }
     },
 }
 </script>
