f5c2c3f445dcc3fe5e8732bea4179f7b010c68ab
moriya <g.ff.k.96@gmail.com>
2021-05-04 08:17:56 +0900

    新たなモデルをマイグレーション

--- django_docker/src/api/models.py
+++ django_docker/src/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class SampleModel(models.Model):
+    user_id = models.IntegerField()
+    title = models.CharField(max_length=200)
+    body = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.user_id
