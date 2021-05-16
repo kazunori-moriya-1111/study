224178564eea9233517ed95571135f4e609e8380
moriya <g.ff.k.96@gmail.com>
2021-05-16 18:15:53 +0900

    DBモデル更新

--- django_docker/src/api/models.py
+++ django_docker/src/api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+import uuid
 
 
 # Create your models here.
@@ -21,7 +22,7 @@ class User(models.Model):
 
 class Body(models.Model):
     # Userテーブルの主キーと1対他のリレーションを定義
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, db_column='user_id', on_delete=models.CASCADE)
     date = models.DateField(auto_now_add=True)
     weight = models.DecimalField(max_digits="3", decimal_places="1")
 
@@ -34,26 +35,25 @@ class Body(models.Model):
 
 
 class Menu(models.Model):
-    menu_id = models.UUIDField(primary_key=True)
+    menu_id = models.UUIDField(primary_key=True, default=uuid.uuid4())
     # Userテーブルの主キーと1対他のリレーションを定義
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, db_column='user_id', on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     calorie = models.IntegerField()
     gram = models.IntegerField()
 
     # 複合ユニークキーを設定
     class Meta:
-        # 1ユーザの1日の体重は1件
+        # 1ユーザの同じ名前の食事登録は不可
         constraints = [
             models.UniqueConstraint(fields=['user_id', 'name'], name='unique_user_menu'),
         ]
 
 
 class Meal(models.Model):
-    # Userテーブルの主キーと1対他のリレーションを定義
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    meal_id = models.UUIDField(primary_key=True, default=uuid.uuid4())
+    menu_id = models.ForeignKey(Menu, db_column='menu_id', on_delete=models.CASCADE)
     date = models.DateField(auto_now_add=True)
     choices = ((1, '朝食'), (2, '昼食'), (3, '夜食'))
     bld = models.IntegerField(choices=choices)
-    menu_id = models.ForeignKey(Menu, on_delete=models.CASCADE)
     count = models.IntegerField()
