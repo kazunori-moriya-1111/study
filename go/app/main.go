a3c878708b7ddb16e9a8f5493fee7f68a9bea417
kazunori_moriya <g.ff.k.96@gmail.com>
2021-07-07 23:46:06 +0900

    変数定義の方法

--- go/app/main.go
+++ go/app/main.go
@@ -8,4 +8,19 @@ import (
 func main() {
 	fmt.Println("Hello golang from docker!")
 	fmt.Println(math.Pi)
+	fmt.Printf("数値 =%d\n", 5)
+	//明示的な定義
+	var n int
+	n = 8
+	fmt.Println(n)
+	//暗黙的な定義
+	i := 1
+	fmt.Println(i)
+	//関数の戻り値の型で暗黙的に定義
+	x := one()
+	fmt.Println(x)
+}
+
+func one() int {
+	return 1
 }
