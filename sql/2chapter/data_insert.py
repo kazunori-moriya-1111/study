a52aff6baa8c8e900fdc7675821f4b59e1226349
kazunori_moriya <g.ff.k.96@gmail.com>
2021-07-10 16:02:35 +0900

    csv inset処理

---
+++ sql/2chapter/data_insert.py
@@ -0,0 +1,24 @@
+import sqlalchemy
+import const
+import pandas as pd
+
+# DB接続
+DATABASE = 'mysql+pymysql'
+USER = const.USER
+PASSWORD = const.PASSWORD
+HOST = const.HOST
+PORT = '3306'
+DB_NAME = const.DBNAME
+
+CONNECT_STR = '{}://{}:{}@{}:{}/{}'.format(DATABASE, USER, PASSWORD, HOST, PORT, DB_NAME)
+ENGINE = sqlalchemy.create_engine(CONNECT_STR)
+
+# dml_csv読み込み
+dml_csv_path = r'./dml/2_1.csv'
+df = pd.read_csv(dml_csv_path, encoding='utf-8')
+
+# import処理
+tbl_name = 'address'
+df.to_sql(con=ENGINE, name=tbl_name, if_exists='replace', index=None)
+
+print('insert complete')
