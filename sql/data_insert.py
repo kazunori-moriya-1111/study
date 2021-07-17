f1f85a4a2ee07d5d388ff20f7042234db3d3f2d3
kazunori_moriya <g.ff.k.96@gmail.com>
2021-07-17 12:43:42 +0900

    path修正

--- sql/data_insert.py
+++ sql/data_insert.py
@@ -14,11 +14,11 @@ CONNECT_STR = '{}://{}:{}@{}:{}/{}'.format(DATABASE, USER, PASSWORD, HOST, PORT,
 ENGINE = sqlalchemy.create_engine(CONNECT_STR)
 
 # dml_csv読み込み
-dml_csv_path = r'./dml/2_1.csv'
+dml_csv_path = r'./3chapter/dml/3_1.csv'
 df = pd.read_csv(dml_csv_path, encoding='utf-8')
 
 # import処理
-tbl_name = 'address'
+tbl_name = 'Items'
 df.to_sql(con=ENGINE, name=tbl_name, if_exists='replace', index=None)
 
 print('insert complete')
