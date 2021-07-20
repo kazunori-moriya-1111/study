1af0d9658e47d83ce1bc6d1d90a34e947c060ccd
kazunori_moriya <g.ff.k.96@gmail.com>
2021-07-20 09:35:04 +0900

    新規テーブル、dml追加

--- sql/create_table.py
+++ sql/create_table.py
@@ -9,16 +9,16 @@ conn = pymysql.connect(host=const.HOST,
                        cursorclass=pymysql.cursors.DictCursor)
 
 # テーブル作成処理(もし存在したらテーブルを削除する)
-create_table_name = 'HotelRooms'
+create_table_name = 'Persons'
 
 with conn.cursor() as cursor:
     drop_table_sql = "drop table if exists {0}".format(create_table_name)
     cursor.execute(drop_table_sql)
     create_table_sql = """CREATE TABLE {0} (
-                          room_nbr INTEGER,
-                          start_date DATE,
-                          end_date DATE,
-                          primary key(room_nbr, start_date)
+                          name VARCHAR(8) NOT NULL PRIMARY KEY,
+                          age INTEGER NOT NULL,
+                          height FLOAT NOT NULL,
+                          weight FLOAT NOT NULL
                           )""".format(create_table_name)
     cursor.execute(create_table_sql)
 
