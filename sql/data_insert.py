1459676ae3ad54f89daa609ef9710461ba46bc98
kazunori_moriya <g.ff.k.96@gmail.com>
2021-07-22 14:44:11 +0900

    dml追加

--- sql/data_insert.py
+++ sql/data_insert.py
@@ -14,11 +14,11 @@ CONNECT_STR = '{}://{}:{}@{}:{}/{}'.format(DATABASE, USER, PASSWORD, HOST, PORT,
 ENGINE = sqlalchemy.create_engine(CONNECT_STR)
 
 # dml_csv読み込み
-dml_csv_path = r'./5chapter/dml/5_4.csv'
+dml_csv_path = r'./6chapter/dml/6_1.csv'
 df = pd.read_csv(dml_csv_path, encoding='utf-8')
 
 # import処理
-tbl_name = 'PostalHistory2'
+tbl_name = 'Employees'
 df.to_sql(con=ENGINE, name=tbl_name, if_exists='append', index=None)
 
 print('insert complete')
