b7b53feb283331a5a32e9ec44a7490b018686317
kazunori_moriya <g.ff.k.96@gmail.com>
2021-07-22 11:47:07 +0900

    郵便番号のランキングを求める

---
+++ sql/5chapter/sql/5_6.sql
@@ -0,0 +1,15 @@
+SELECT
+	pcode,
+	district,
+	CASE
+		when pcode = '4130033' then 0
+		when pcode like '413003%' THEN 1
+		when pcode like '41300%' THEN 2
+		when pcode like '4130%' THEN 3
+		when pcode like '413%' THEN 4
+		when pcode like '41%' THEN 5
+		when pcode like '4%' THEN 6
+		else null
+	end as 'rank'
+from
+	PotalCode
