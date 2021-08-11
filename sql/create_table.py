e98803e5770c9cc8d5895f59dd28a64ba079b19a
kazunori_moriya <g.ff.k.96@gmail.com>
2021-08-11 23:11:26 +0900

    dml ddl追加

--- sql/create_table.py
+++ sql/create_table.py
@@ -9,14 +9,16 @@ conn = pymysql.connect(host=const.HOST,
                        cursorclass=pymysql.cursors.DictCursor)
 
 # テーブル作成処理(もし存在したらテーブルを削除する)
-create_table_name = 'Weights'
+create_table_name = 'Weights2'
 
 with conn.cursor() as cursor:
     drop_table_sql = "drop table if exists {0}".format(create_table_name)
     cursor.execute(drop_table_sql)
     create_table_sql = """CREATE TABLE {0} (
-                          student_id CHAR(4) PRIMARY KEY,
-                          weight INTEGER
+                          class INTEGER NOT NULL,
+                          student_id CHAR(4) NOT NULL,
+                          weight INTEGER NOT NULL,
+                          primary key(class, student_id)
                           )""".format(create_table_name)
     cursor.execute(create_table_sql)
 
