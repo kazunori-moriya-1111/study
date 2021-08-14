88cfdd661fa2e05a91f9cacc5dc49ea77105308a
kazunori_moriya <g.ff.k.96@gmail.com>
2021-08-15 00:23:48 +0900

    javascript テンプレート文字列の使い方

--- react/app/index.js
+++ react/app/index.js
@@ -50,3 +50,17 @@ console.log(val5)
 
 val5.push("monkey")
 console.log(val5)
+
+//テンプレート文字列
+const name = "J";
+const age = 28
+
+//「私の名前はJです。年齢は28歳です。」
+
+// 従来の方法
+const message1 = "私の名前は" + name + "です。年齢は" + age + "です。"
+console.log(message1)
+
+//テンプレート文字列を用いた方法
+const message2 = `私の名前は${name}です。年齢は${age}です。`;
+console.log(message2)
