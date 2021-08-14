6cc5df8ad05e52dd19752da2ad36407e6814e6d4
kazunori_moriya <g.ff.k.96@gmail.com>
2021-08-15 00:52:16 +0900

    javascript 関数のデフォルト値

--- react/app/index.js
+++ react/app/index.js
@@ -105,3 +105,8 @@ console.log(message5)
 const [name2, age2] = myProfile1;
 const message6 = `名前は${name2}です。年齢は${age2}歳です。`
 console.log(message6)
+
+//デフォルト値、引数など
+const sayHello = (name = "ゲスト") => console.log(`こんにちは!${name}さん!`)
+sayHello("J")
+sayHello()
