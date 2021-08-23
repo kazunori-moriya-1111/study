e7367a953846b1145dbbbd2a6f462c26162750d2
kazunori_moriya <g.ff.k.96@gmail.com>
2021-08-23 21:27:26 +0900

    cssを適用

--- react/react_todo/src/App.js
+++ react/react_todo/src/App.js
@@ -4,31 +4,31 @@ import "./styles.css";
 export const App = () => {
   return (
     <>
-      <div>
+      <div className="input-area">
         <input placeholder="TODOを入力" />
         <button>追加</button>
       </div>
-      <div>
+      <div className="incomplete-area">
         <p>未完了のTODO</p>
         <ul>
-          <div>
+          <div className="list-row">
             <li>あああああ</li>
             <button>完了</button>
             <button>削除</button>
           </div>
-          <div>
+          <div className="list-row">
             <li>いいいいい</li>
             <button>完了</button>
             <button>削除</button>
           </div>
         </ul>
       </div>
-      <div>
-      <ul>
-        <div>
-          <li>ううううう</li>
-          <button>戻す</button>
-        </div>
+      <div className="complete-area">
+        <ul>
+          <div className="list-row">
+            <li>ううううう</li>
+            <button>戻す</button>
+          </div>
         </ul>
       </div>
     </>
