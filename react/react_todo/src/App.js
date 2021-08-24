f7d78598a6a1dc336bedd2897034a0bdf2b0ff67
kazunori_moriya <g.ff.k.96@gmail.com>
2021-08-24 23:18:46 +0900

    reactを使用したモックへ変更

--- react/react_todo/src/App.js
+++ react/react_todo/src/App.js
@@ -1,34 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./styles.css";
 
 export const App = () => {
+  // eslint-disable-next-line
+  const [todoText, setTodotext] = useState('')
+  // eslint-disable-next-line
+  const [incompleteTodos, setIncompleteTodos] = useState([
+    "あああああ",
+    "いいいいい"
+  ])
+  // eslint-disable-next-line
+  const [completeTodos, setCompleteTodos] = useState(["ううううう"])
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" />
+        <input placeholder="TODOを入力" value={todoText}/>
         <button>追加</button>
       </div>
       <div className="incomplete-area">
         <p>未完了のTODO</p>
         <ul>
-          <div className="list-row">
-            <li>あああああ</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
-          <div className="list-row">
-            <li>いいいいい</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
+          {incompleteTodos.map((todo) => {
+            return(
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            )
+          })}
         </ul>
       </div>
       <div className="complete-area">
         <ul>
-          <div className="list-row">
-            <li>ううううう</li>
-            <button>戻す</button>
-          </div>
+        {completeTodos.map((todo) => {
+            return(
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            )
+          })}
         </ul>
       </div>
     </>
