6f81c4f2aacfba6b2b10f9b4a2a2999c261e7609
kazunori_moriya <g.ff.k.96@gmail.com>
2021-08-26 21:56:38 +0900

    完了の機能を実装

--- react/react_todo/src/App.js
+++ react/react_todo/src/App.js
@@ -31,6 +31,15 @@ export const App = () => {
     newTodos.splice(index, 1)
     setIncompleteTodos(newTodos)
   }
+  // 完了ボタンの動作
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...incompleteTodos]
+    newIncompleteTodos.splice(index, 1)
+    
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]]
+    setIncompleteTodos(newIncompleteTodos)
+    setCompleteTodos(newCompleteTodos)
+  }
   return (
     <>
       <div className="input-area">
@@ -44,8 +53,8 @@ export const App = () => {
             return(
               <div key={todo} className="list-row">
                 <li>{todo}</li>
-                <button>完了</button>
                 {/* onCllickで関数に引数を渡したい場合アロー関数で書かないと引数を渡すごとに動作してしまう */}
+                <button onClick={() => onClickComplete(index)}>完了</button>
                 <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             )
