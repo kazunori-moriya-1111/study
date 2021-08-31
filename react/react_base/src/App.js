de1de53fb5631f2deb4d321ebc41cca5db1bf218
kazunori_moriya <g.ff.k.96@gmail.com>
2021-08-31 17:43:03 +0900

    propsの使い方

--- react/react_base/src/App.js
+++ react/react_base/src/App.js
@@ -1,19 +1,25 @@
 const App = () =>  {
-  const greeting = "Hi!, dom"
-  const dom = <h1 className="">{greeting}</h1>
+  const profiles = [
+    { name: "Taro", age:10 },
+    { name: "Hanako", age:5 },
+    { name: "Noname"}
+  ]
   return (
     <>
-      {dom}
-      <label htmlFor="bar">bar</label>
-      <input type="text" onClick={() => {console.log("ssdd")}}/>
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+          profiles.map((profile, index) => {
+            return <User name={profile.name} age={profile.age} key={index}/>
+          })
+      }
     </>
   );
 }
 
-const Cat = () => {
-  return <div>Meow</div>
+const User = (props) => {
+  return <div>Hi, I am {props.name}, and {props.age} years old!</div>
+}
+
+User.defaultProps = {
+  age: 1
 }
 export default App
