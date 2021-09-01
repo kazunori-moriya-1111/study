89f0cc49fafb08ed1570c2a3520ce6f4a0527ff2
kazunori_moriya <g.ff.k.96@gmail.com>
2021-09-01 17:12:35 +0900

    prop-typeesの使い方

--- react/react_base/src/App.js
+++ react/react_base/src/App.js
@@ -1,7 +1,9 @@
+import PropTypes from 'prop-types'
+
 const App = () =>  {
   const profiles = [
-    { name: "Taro", age:10 },
-    { name: "Hanako", age:5 },
+    { name: "Taro", age: 10 },
+    { name: "Hanako", age: 5 },
     { name: "Noname"}
   ]
   return (
@@ -14,12 +16,19 @@ const App = () =>  {
     </>
   );
 }
-
+// コンポーネントを定義
 const User = (props) => {
   return <div>Hi, I am {props.name}, and {props.age} years old!</div>
 }
 
+// コンポーネント引数のデフォルト値を設定
 User.defaultProps = {
   age: 1
+}
+
+// Userコンポーネントに対して型を定義する isRequiredを付与するとpropsに定義されていないとwarningを発生させる
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
 export default App
