70fc7f3dd6f793be16304ec2f97ff9b03dd99eab
kazunori_moriya <g.ff.k.96@gmail.com>
2021-09-06 23:54:05 +0900

    現状をプッシュ。一時中断

--- react/react_base/src/actions/index.js
+++ react/react_base/src/actions/index.js
@@ -1,9 +1,15 @@
 import axios from 'axios'
 export const READ_EVENTS = 'READ_EVENTS'
+export const CREATE_EVENT = 'CREATE_EVENT'
 
+const ROOT_URL = 'http://localhost:8000/api/v1'
 // アクションクリエイター:アクションをリターンする関数
 export const readEvents = () => async dispatch => {
-    const ROOT_URL = 'http://localhost:8000/api/v1'
     const response = await axios.get(`${ROOT_URL}/event/`)
     dispatch({ type: READ_EVENTS, response })
+}
+
+export const postEvent = values => async dispatch => {
+    const response = await axios.post(`${ROOT_URL}/event/`, values)
+    dispatch({ type: CREATE_EVENT, response })
 }
