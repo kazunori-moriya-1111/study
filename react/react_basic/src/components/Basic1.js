60f7655578479a518bb773aae3786feca4c12be7
kazunori_moriya <g.ff.k.96@gmail.com>
2021-09-08 19:58:41 +0900

    Functionalコンポーネントの使い方

---
+++ react/react_basic/src/components/Basic1.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Basic1 = () =>{
+    return(
+        <div>
+            <h1>Hello React1</h1>
+            <h1>Hello React2</h1>
+        </div>
+    )
+}
+
+export default Basic1
