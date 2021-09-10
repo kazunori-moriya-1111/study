e1f97f7c87e5ca8eeaede163dc6f73754446286a
kazunori_moriya <g.ff.k.96@gmail.com>
2021-09-10 21:11:51 +0900

    イベントハンドラーの定義

--- react/react_basic/src/components/Basic1.js
+++ react/react_basic/src/components/Basic1.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
 const Basic1 = (props) =>{
+    const clickHandler = () =>{
+        console.log("click")
+    }
     return(
         <div>
+            <button onClick={clickHandler}>Click</button>
             <h1>Hello {props.name}</h1>
             <h1>Hello React2</h1>
         </div>
