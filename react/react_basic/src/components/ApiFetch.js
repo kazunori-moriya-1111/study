68bedafdf54aae44c39a836a4598d6eba4fc5de3
kazunori_moriya <g.ff.k.96@gmail.com>
2021-09-15 18:41:29 +0900

    特定のIDでpostする

--- react/react_basic/src/components/ApiFetch.js
+++ react/react_basic/src/components/ApiFetch.js
@@ -4,28 +4,39 @@ import axios from 'axios'
 const ApiFetch = () =>{
 
     const [posts, setPosts] = useState([])
+    const [id, setId] = useState(1)
+    const [clicked, setClicked] = useState(false)
 
+    const handlerClicked = () => {
+        setClicked(!clicked)
+    }
     useEffect(() => {
         // axiosを使用した場合
-        // axios.get('https://jsonplaceholder.typicode.com/posts')
-        // .then(res => {
-        //     setPosts(res.data)
-        // })
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        .then(res => {
+            setPosts(res.data)
+        })
 
         // fetchの場合(HTMLの形で返ってくる)
-        fetch('https://jsonplaceholder.typicode.com/posts', {method: 'GET'})
-        .then(res => res.json())
-        .then(data => {
-            setPosts(data)
-        })
-    }, [])
+        // fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {method: 'GET'})
+        // .then(res => res.json())
+        // .then(data => {
+        //     setPosts(data)
+        // })
+    // eslint-disable-next-line 
+    }, [clicked])
     return(
         <div>
-            <ul>
+            <input type="text" value={id} onChange={evt=>setId(evt.target.value)} />
+            <br />
+            <button type="button" onClick={handlerClicked}>Get Post</button>
+            <br />
+            {posts.title}
+            {/* <ul>
                 {
                     posts.map(post => <li key={post.id}>{post.title}</li>)
                 }
-            </ul>
+            </ul> */}
         </div>
     )
 }
