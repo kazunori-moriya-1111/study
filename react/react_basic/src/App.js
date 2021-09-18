77e404ddb79a8eef1305b7dc23d3d214b61836e4
kazunori_moriya <g.ff.k.96@gmail.com>
2021-09-18 11:44:10 +0900

    useCallbackによるレンダリングの最適化

--- react/react_basic/src/App.js
+++ react/react_basic/src/App.js
@@ -8,8 +8,10 @@ import AppContext from './contexts/AppContext';
 // import B from './components/B'
 // import BasicReducer from './components/BasicReducer';
 // import CompB from './components/CompB';
-import React, {useReducer} from 'react'
-import Memo from './components/Memo';
+import React, {useReducer, useState, useCallback} from 'react'
+import CountDisplay from './components/CountDisplay';
+import CountClick from './components/CountClick';
+// import Memo from './components/Memo';
 
 const initialState = 0
 const reducer = (currentState, action) => {
@@ -28,6 +30,18 @@ const reducer = (currentState, action) => {
 function App() {
   // count:現在のstateを参照できる変数 dispatch:コマンドを引数に取ってstateを変化させる
   const [count, dispatch] = useReducer(reducer, initialState)
+
+  const [count1, setCount1] = useState(0)
+  const [count2, setCount2] = useState(0)
+
+  const AddCount1 = useCallback(() => {
+      setCount1(prevCount1 => prevCount1 + 1)
+  }, [])
+
+  const AddCount2 = useCallback(() => {
+      setCount2(prevCount2 => prevCount2 + 1)
+  }, [])
+
   return (
     // 複数の値をuseContextで与える
     <AppContext.Provider value={{countProvided: count, dispatchProvided: dispatch}}>
@@ -40,7 +54,12 @@ function App() {
           {/* <BasicReducer /> */}
           {/* Count {count}
           <CompB /> */}
-          <Memo />
+          {/* <Memo /> */}
+          <CountDisplay name="count1" count={count1}/>
+          {/* 挟んだ要素はchildrenで使える */}
+          <CountClick handleClick={AddCount1}>AddCount1</CountClick>
+          <CountDisplay name="count2" count={count2}/>
+          <CountClick handleClick={AddCount2}>AddCount2</CountClick>
         </header>
       </div>
     </AppContext.Provider>
