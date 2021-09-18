f338adf4b107521b796a50fdaee90a6e6ef7ea67
kazunori_moriya <g.ff.k.96@gmail.com>
2021-09-19 00:39:10 +0900

    djangoの管理サイトへcssを適用する

--- django_docker/src/app/settings.py
+++ django_docker/src/app/settings.py
@@ -128,6 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "app/static")
 # windows HOMEで開発するのみ必要な設定
 ALLOWED_HOSTS = ['*', ]
