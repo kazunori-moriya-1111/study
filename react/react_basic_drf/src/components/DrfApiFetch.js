89edac6c3bb1e61950c1ed52866a4d77c8491341
kazunori_moriya <g.ff.k.96@gmail.com>
2021-09-20 15:45:08 +0900

    idを指定したpost

--- react/react_basic_drf/src/components/DrfApiFetch.js
+++ react/react_basic_drf/src/components/DrfApiFetch.js
@@ -4,20 +4,38 @@ import axios from 'axios'
 const DrfApiFetch = () =>{
 
     const [tasks, setTasks] = useState([])
+    const [selectedTask, setSelectedTask] = useState([])
+    const [id, setId] = useState(2)
 
+    // useEffectの第二引数を空にすることで最初のレンダリング時のみに実行する関数を表現
     useEffect(() => {
-        axios.get('http://localhost:8000/api/tasks',{
+        axios.get('http://localhost:8000/api/tasks/',{
             headers:{
                 'Authorization' : 'Token 1b0734ef46b2790a8512679604d6592c276181e2'
             }
         })
         .then(res => {setTasks(res.data)})
-    })
+    }, [])
+
+    const getTask = () => {
+        axios.get(`http://localhost:8000/api/tasks/${id}/`,{
+            headers:{
+                'Authorization' : 'Token 1b0734ef46b2790a8512679604d6592c276181e2'
+            }
+        })
+        .then(res => {setSelectedTask(res.data)})
+    }
     return(
         <div>
             <ul>
                 {tasks.map(task => <li key={task.id}>{task.title} {task.id}</li>)}
             </ul>
+            Set ID<br />
+            <input type='text' value={id} onChange={env => (setId(env.target.value))}></input>
+            <br />
+            <button type='button' onClick={() => getTask()}>Get tasks</button>
+            <br />
+            <h3>{ selectedTask.title }</h3>
         </div>
     )
 }
