4b227db0498c1de7c0158ccdb98ea1b9f61f8786
kazunori_moriya <g.ff.k.96@gmail.com>
2021-09-25 14:33:50 +0900

    cssmoduleの適用

--- react/react_stepup/src/App.js
+++ react/react_stepup/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { useState, useCallback, useMemo} from 'react'
 import { ChildArea } from './ChildArea';
 import { InlineStyle } from './components/InlineStyle';
+import { CssModules } from './components/CssModules';
 
 function App() {
 
@@ -32,6 +33,7 @@ function App() {
       <button style={style} onClick={onClickOpen}>表示</button>
       <ChildArea open={open} onClickClose={onClickClose}/>
       <InlineStyle />
+      <CssModules />
     </div>
   );
 }
