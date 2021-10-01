837700203d675cb041b30f89b13dba5c91d0120e
kazunori_moriya <g.ff.k.96@gmail.com>
2021-10-01 21:52:47 +0900

    react-router使い方

--- react/react_stepup/src/App.js
+++ react/react_stepup/src/App.js
@@ -1,45 +1,68 @@
 import './App.css';
-import { useState, useCallback, useMemo} from 'react'
-import { ChildArea } from './ChildArea';
-import { InlineStyle } from './components/InlineStyle';
-import { CssModules } from './components/CssModules';
-import { StyledJsx } from './components/StyledJsx';
-import { StyledComponents } from './components/StyledComponents';
-import { Emotion } from './components/Emotion';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom'
+import { Home } from './Home';
+import { Page1 } from './Page1';
+import { Page2 } from './Page2';
+// import { useState, useCallback, useMemo} from 'react'
+// import { ChildArea } from './ChildArea';
+// import { InlineStyle } from './components/InlineStyle';
+// import { CssModules } from './components/CssModules';
+// import { StyledJsx } from './components/StyledJsx';
+// import { StyledComponents } from './components/StyledComponents';
+// import { Emotion } from './components/Emotion';
 
 function App() {
 
-  const [text, setText] = useState('')
-  const [open, setOpen] = useState(false)
+  // const [text, setText] = useState('')
+  // const [open, setOpen] = useState(false)
 
-  const onChnegetext = (e) => setText(e.target.value)
-  const onClickOpen = () => setOpen(!open)
+  // const onChnegetext = (e) => setText(e.target.value)
+  // const onClickOpen = () => setOpen(!open)
 
-  // propsで渡す関数を常に同じものにして、props変化によるレンダリングを防ぐ
-  const onClickClose = useCallback(() => setOpen(false), [])
+  // // propsで渡す関数を常に同じものにして、props変化によるレンダリングを防ぐ
+  // const onClickClose = useCallback(() => setOpen(false), [])
 
-  // 変数のMemo化
-  const temp = useMemo(() => 1 + 3, [])
-  console.log(temp)
+  // // 変数のMemo化
+  // const temp = useMemo(() => 1 + 3, [])
+  // console.log(temp)
 
-  // インラインスタイル適用
-  const style = {
-    border: 'solid'
-  }
+  // // インラインスタイル適用
+  // const style = {
+  //   border: 'solid'
+  // }
   return (
-    <div className="App">
-      {console.log("Appのレンダリング")}
-      <input value={text} onChange={onChnegetext}/>
-      <br />
-      <br />
-      <button sytle={style} onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} onClickClose={onClickClose}/>
-      <InlineStyle />
-      <CssModules />
-      <StyledJsx />
-      <StyledComponents />
-      <Emotion />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        {/* {console.log("Appのレンダリング")}
+        <input value={text} onChange={onChnegetext}/>
+        <br />
+        <br />
+        <button sytle={style} onClick={onClickOpen}>表示</button>
+        <ChildArea open={open} onClickClose={onClickClose}/>
+        <InlineStyle />
+        <CssModules />
+        <StyledJsx />
+        <StyledComponents />
+        <Emotion /> */}
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
+      </div>
+      <Switch>
+        {/* exact = 完全一致の場合レンダリングする */}
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/page1">
+          <Page1 />
+        </Route>
+        <Route path="/page2">
+          <Page2 />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
