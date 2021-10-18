142a930484f79d74b1c7b56c8eded6139a02db73
kazunori_moriya <g.ff.k.96@gmail.com>
2021-10-18 21:55:02 +0900

    vuexの使い方

--- vue/app/vite_app2/src/App.vue
+++ vue/app/vite_app2/src/App.vue
@@ -1,36 +1,16 @@
 <template>
   <div id="app">
-    <div>
-      <router-link to="/index/taro" class="btn btn-primary mx-2">
-        Go to Top
-      </router-link>
-      <router-link to="/jsx/hanako" class="btn btn-warning">
-        Go to JSX
-      </router-link>
-    </div>
-    <hr>
-    <h5>default view</h5>
-    <router-view />
-    <hr>
-    <h5>A & B</h5>
-    <router-view name="first"></router-view>
-    <router-view name="second"></router-view>
+    <HelloWorld />
   </div>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
   name: 'app',
-  created() {
-    console.log("***** App Created! *****")
-  },
-  mounted() {
-    console.log("----- App Mouted! -----")
-  },
-  data() {
-    return {
-      title: 'Router'
-    }
+  components: {
+    HelloWorld
   }
 }
 </script>
