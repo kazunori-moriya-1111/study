239481f69ef0569b335a93389cd5048d07e48d99
kazunori_moriya <g.ff.k.96@gmail.com>
2021-10-18 22:10:44 +0900

    mutationによるstoreの操作

--- vue/app/vite_app2/src/components/HelloWorld.vue
+++ vue/app/vite_app2/src/components/HelloWorld.vue
@@ -2,6 +2,14 @@
   <div class="alert alert-primary">
     <h1>{{ data.title }}</h1>
     <p class="mt-3 h5">{{ $store.state.message }}</p>
+    <hr>
+    <!-- eslint-disable vue/use-v-on-exact -->
+    <div class="btn btn-secondary" @click="$store.commit('count')" @click.shift="$store.commit('reset')">
+    <!-- eslint-enable -->
+      <a class="h5">
+        clickd: {{ $store.state.counter }}
+      </a>
+    </div>
   </div>
 </template>
 
