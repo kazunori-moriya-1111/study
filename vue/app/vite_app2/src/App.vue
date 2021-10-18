1a553f206520683b1a157fc6404941fb4c901039
kazunori_moriya <g.ff.k.96@gmail.com>
2021-10-18 10:37:58 +0900

    名前付きrouter-viewの使い方

--- vue/app/vite_app2/src/App.vue
+++ vue/app/vite_app2/src/App.vue
@@ -9,7 +9,12 @@
       </router-link>
     </div>
     <hr>
-    <router-view></router-view>
+    <h5>default view</h5>
+    <router-view />
+    <hr>
+    <h5>A & B</h5>
+    <router-view name="first"></router-view>
+    <router-view name="second"></router-view>
   </div>
 </template>
 
