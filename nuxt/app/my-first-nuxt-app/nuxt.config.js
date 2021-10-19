b3288a83ec6262dbaf4916dddaebff7a9b8affa3
kazunori_moriya <g.ff.k.96@gmail.com>
2021-10-19 21:02:54 +0900

    axiosの使い方

--- nuxt/app/my-first-nuxt-app/nuxt.config.js
+++ nuxt/app/my-first-nuxt-app/nuxt.config.js
@@ -36,8 +36,11 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxtjs/axios'
   ],
+  axios: {
 
+  },
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   }
--- nuxt/app/my-first-nuxt-app/pages/index.vue
+++ nuxt/app/my-first-nuxt-app/pages/index.vue
@@ -3,5 +3,12 @@
 </template>
 
 <script>
-export default {}
+export default {
+  async mounted() {
+    console.log(
+      JSON.stringify(await
+      this.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js'), true, ' ')
+    )
+  }
+}
 </script>
