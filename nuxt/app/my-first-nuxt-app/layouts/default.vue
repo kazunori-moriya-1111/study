3502a6cfde486d0a37473e5499de70bf77917caf
kazunori_moriya <g.ff.k.96@gmail.com>
2021-10-21 20:11:35 +0900

    layoutsの使い方

---
+++ nuxt/app/my-first-nuxt-app/layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <span>default layout</span>
+    <AppNavigation />
+    <hr>
+    <nuxt />
+    <hr>
+    <footer>
+      fotter
+    </footer>
+  </div>
+</template>
+
+<script>
+import AppNavigation from '~/components/AppNavigation.vue'
+
+export default {
+  components: {
+    AppNavigation
+  }
+}
+</script>
--- nuxt/app/my-first-nuxt-app/pages/index.vue
+++ nuxt/app/my-first-nuxt-app/pages/index.vue
@@ -1,60 +1,9 @@
 <template>
-  <section class="container">
-    <div>
-      <h3>Nuxt.js のタグがつけられた投稿一覧</h3>
-      <ul>
-        <li v-for="item in items" :key="item.id">
-          <h4>
-            <span>{{ item.title }}</span>
-            <small>
-              <span>by </span>
-              <nuxt-link :to="`/users/${item.user.id}`">
-                {{ item.user.id }}
-              </nuxt-link>
-            </small>
-          </h4>
-          <div>{{ item.body.slice(0, 130) }}・・・・・・</div>
-          <p><a :href="item.url">{{ item.url }}</a></p>
-        </li>
-      </ul>
-    </div>
-  </section>
+  <div>
+    <h1>Index page</h1>
+  </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
 
-export default {
-  async asyncData({ store }){
-    if (store.getters['items'].length){
-      return
-    }
-    await store.dispatch('fetchItems')
-  },
-  // 値を監視して変更があったときに動作する
-  computed: {
-    ...mapGetters(['items'])
-  }
-}
 </script>
-
-<style>
-.container {
-  min-height: 100vh;
-  padding: 16px;
-}
-
-h3 {
-  margin: 16px 0;
-  padding: 8px 0;
-  border-bottom: solid 1px #e5e5e5;
-}
-
-li + li {
-  margin: 16px 0;
-}
-
-p {
-  margin: 8px 0;
-}
-</style>
