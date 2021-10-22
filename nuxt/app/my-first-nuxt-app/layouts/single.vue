99b58769f0992be4f5cba46750097c1ce9dbf2e4
kazunori_moriya <g.ff.k.96@gmail.com>
2021-10-22 15:38:38 +0900

    個別にlayoutを適用する

---
+++ nuxt/app/my-first-nuxt-app/layouts/single.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <span>default layout</span>
+    <AppNavigation />
+    <hr>
+    <nuxt />
+    <nuxt-link to="/">トップへ戻る</nuxt-link>
+    <hr>
+    <footer>
+      fotter
+    </footer>
+  </div>
+</template>
+
+<script>
+import AppNavigation from '~/components/AppNavigation.vue'
+
+export default {
+  components: {
+    AppNavigation
+  }
+}
+</script>
