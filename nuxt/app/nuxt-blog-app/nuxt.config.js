8ce8223dda250ac0e6ab173ad8f4e3ae5b44e509
kazunori_moriya <g.ff.k.96@gmail.com>
2021-10-29 22:52:14 +0900

    element-ui導入

--- nuxt/app/nuxt-blog-app/nuxt.config.js
+++ nuxt/app/nuxt-blog-app/nuxt.config.js
@@ -22,10 +22,13 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    'element-ui/lib/theme-chalk/index.css',
+    '~/assets/common.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '@/plugins/elementUi'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
