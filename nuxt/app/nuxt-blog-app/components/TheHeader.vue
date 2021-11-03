aa4c510cbe77e801954a61e26c4a32721c9947f2
kazunori_moriya <g.ff.k.96@gmail.com>
2021-11-03 18:28:36 +0900

    ログイン機能を実装

--- nuxt/app/nuxt-blog-app/components/TheHeader.vue
+++ nuxt/app/nuxt-blog-app/components/TheHeader.vue
@@ -7,13 +7,24 @@
       投稿一覧
     </el-menu-item>
 
-    <no-ssr>
-      <el-menu-item index="4" style="float: rigth;" :router="{path: '/'}">
-        <span>ログイン</span>
-      </el-menu-item>
-    </no-ssr>
+    <el-menu-item index="4" style="float: rigth;" :router="{path: `/users/${user.id}`}" v-if="user">
+      <span>{{ user.id }}</span>
+    </el-menu-item>
+    <el-menu-item index="4" style="float: rigth;" :router="{path: '/'}" v-else>
+      <span>ログイン</span>
+    </el-menu-item>
     <el-menu-item index="5" style="float: rigth" :router="{path: '/posts/new'}">
       新規登録
     </el-menu-item>
   </el-menu>
 </template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters(['user'])
+  }
+}
+</script>
