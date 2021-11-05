ae4315c8b23f2696d1f31263e655d3be09fbc869
kazunori_moriya <g.ff.k.96@gmail.com>
2021-11-05 20:13:42 +0900

    ルーティング設定の修正

--- nuxt/app/nuxt-blog-app/components/TheHeader.vue
+++ nuxt/app/nuxt-blog-app/components/TheHeader.vue
@@ -3,16 +3,16 @@
     <el-menu-item index="1" style="pointer-events:none;">
       Nuxt Diary App
     </el-menu-item>
-    <el-menu-item index="2" :router="{path: '/posts/'}">
+    <el-menu-item index="2" :route="{path: '/posts/'}">
       投稿一覧
     </el-menu-item>
-    <el-menu-item index="4" style="float: rigth;" :router="{path: `/users/${user.id}`}" v-if="user">
+    <el-menu-item index="4" style="float: rigth;" :route="{path: `/users/${user.id}`}" v-if="user">
       <span>{{ user.id }}</span>
     </el-menu-item>
-    <el-menu-item index="4" style="float: rigth;" :router="{path: '/'}" v-else>
+    <el-menu-item index="4" style="float: rigth;" :route="{path: '/'}" v-else>
       <span>ログイン</span>
     </el-menu-item>
-    <el-menu-item index="5" style="float: rigth" :router="{path: '/posts/new'}">
+    <el-menu-item index="5" style="float: rigth" :route="{path: '/posts/new'}">
       新規登録
     </el-menu-item>
   </el-menu>
