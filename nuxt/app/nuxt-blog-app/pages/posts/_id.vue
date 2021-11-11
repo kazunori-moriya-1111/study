6eb50978f1233f6b70aef11f15b681ea35d1598a
kazunori_moriya <g.ff.k.96@gmail.com>
2021-11-11 20:20:50 +0900

    個別ページ実装

---
+++ nuxt/app/nuxt-blog-app/pages/posts/_id.vue
@@ -0,0 +1,58 @@
+<template>
+  <section class="container posts-page">
+    <div style="flex 1">
+      <el-card v-if="post">
+        <div slot="header" class="clearfix">
+          <h2>{{ post.title }}</h2>
+          <small>by {{ post.user.id }}</small>
+        </div>
+        <p>{{ post.body }}</p>
+        <p class="text-right">{{ post.create_at | time }}</p>
+      </el-card>
+      <p>
+        <nuxt-link to="/posts">&lt; 投稿一覧へ</nuxt-link>
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+import moment from '~/plugins/moment.js'
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  async asyncData({ store, route, error }) {
+    console.log(route)
+    const { id } = route.params
+    console.log(id)
+    if (store.getters['posts/posts'].find(p => p.id === id)){
+      return
+    }
+    try {
+      await store.dispatch('posts/fetchPost', { id })
+      if (!(store.getters['posts/posts'].find(p => p.id === this.$route.params.id))) {
+        throw new Error('post not found')
+      }
+    } catch (e) {
+      error({ statusCode: 404 })
+    }
+  },
+  computed: {
+    post() {
+      return this.posts.find(p => p.id === this.$route.params.id)
+    },
+    ...mapGetters('posts', ['posts'])
+  },
+  filters: {
+    time(val) {
+      return moment(val).format('YYYY/MM/DD HH:mm:ss')
+    }
+  }
+}
+</script>
+
+<style>
+.posts-page .el-table__row{
+  cursor: pointer;
+}
+</style>
