9c4eeaabe304b888801ca130a6018de7edccdc56
kazunori_moriya <g.ff.k.96@gmail.com>
2021-11-22 19:18:41 +0900

    推論型の使い方

--- typescript/app/index.js
+++ typescript/app/index.js
@@ -1,3 +1,9 @@
 var hello = 'hello';
 console.log(hello);
 var hasValue = true;
+var float = 3.14;
+var cnout = 10;
+var negative = -0.12;
+var single = 'hello';
+var double = "hello";
+var back = "hello";
--- typescript/app/index.ts
+++ typescript/app/index.ts
@@ -1,3 +1,9 @@
 let hello : string = 'hello';
 console.log(hello);
-let hasValue : boolean = true;
+let hasValue = true;
+let float = 3.14;
+let cnout = 10
+let negative = -0.12;
+let single = 'hello';
+let double = "hello";
+let back = `hello`;
