54adbb67fbb1f00feba38926260d054fba6e6658
kazunori_moriya <g.ff.k.96@gmail.com>
2021-11-30 15:57:11 +0900

    routerの定義分割

--- react/react_stepup/src/App.js
+++ react/react_stepup/src/App.js
@@ -1,10 +1,6 @@
 import './App.css';
-import { BrowserRouter, Link, Switch, Route } from 'react-router-dom'
-import { Home } from './Home';
-import { Page1 } from './Page1';
-import { Page2 } from './Page2';
-import { Page1DetailA } from './Page1DetailA';
-import { Page1DetailB } from './Page1DetailB';
+import { BrowserRouter, Link } from 'react-router-dom'
+import { Router } from "./router/Router"
 // import { useState, useCallback, useMemo} from 'react'
 // import { ChildArea } from './ChildArea';
 // import { InlineStyle } from './components/InlineStyle';
@@ -52,29 +48,7 @@ function App() {
         <br />
         <Link to="/page2">Page2</Link>
       </div>
-      <Switch>
-        {/* exact = 完全一致の場合レンダリングする */}
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/page1" render={({match: { url } }) => (
-          <Switch>
-            <Route exact path={url}>
-              <Page1 />
-            </Route>
-            <Route path={`${url}/detailA`}>
-              <Page1DetailA />
-            </Route>
-            <Route path={`${url}/detailB`}>
-              <Page1DetailB />
-            </Route>
-          </Switch>
-        )}>
-        </Route>
-        <Route path="/page2">
-          <Page2 />
-        </Route>
-      </Switch>
+      <Router />
     </BrowserRouter>
   );
 }
