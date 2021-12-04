bd56c4d234060a20d4a7484ddad405a5c9bc815d
kazunori_moriya <g.ff.k.96@gmail.com>
2021-12-04 17:12:26 +0900

    historyを使用した画面遷移

--- react/react_stepup/src/Page1DetailA.jsx
+++ react/react_stepup/src/Page1DetailA.jsx
@@ -1,11 +1,14 @@
-import { useLocation } from "react-router-dom"
+import { useLocation, useHistory } from "react-router-dom"
 
 export const Page1DetailA = () => {
     const { state } = useLocation()
+    const history = useHistory()
+    const onClickBack = () => history.goBack();
     return(
         <div>
             <h1>Page1DetailAです</h1>
             <p>{ state }</p>
+            <button onClick={onClickBack}>戻る</button>
         </div>
     )
 }
