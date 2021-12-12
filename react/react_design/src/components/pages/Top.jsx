723ac6baf66a000a5488b4adf8dcc5fed563469b
kazunori_moriya <g.ff.k.96@gmail.com>
2021-12-12 18:02:43 +0900

    axios * react

--- react/react_design/src/components/pages/Top.jsx
+++ react/react_design/src/components/pages/Top.jsx
@@ -3,6 +3,7 @@ import { SecondaryButton } from "../atoms/button/SecondaryButton"
 import { useNavigate } from 'react-router-dom'
 import { useContext } from "react";
 import { UserContext } from "../../providers/UseProvider";
+import axios from "axios"
 
 export const Top = () => {
   const navigate = useNavigate();
@@ -16,12 +17,26 @@ export const Top = () => {
     navigate("/users");
   }
 
+  const onClickUsers = () => {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+      console.log(res.data)
+    }).catch((err) => {console.log(err)})
+  }
+
+  const onClickUser1 = () => {
+    axios.get("https://jsonplaceholder.typicode.com/users?id=1").then((res) => {
+      console.log(res.data)
+    }).catch((err) => {console.log(err)})
+  }
   return(
     <SContainer>
       <h2>TOPページです</h2>
       <SecondaryButton onClick={onClickAdmin}>管理者ユーザー</SecondaryButton>
       <br /><br />
       <SecondaryButton onClick={onClickGeneral}>一般ユーザー</SecondaryButton>
+      <br /><br />
+      <button onClick={onClickUsers}>users</button>
+      <button onClick={onClickUser1}>id=1のusers</button>
     </SContainer>
   )
 }
