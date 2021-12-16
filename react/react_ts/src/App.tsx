c27f51a4f1ee46e84b0e648d7b1d0a598590b81d
kazunori_moriya <g.ff.k.96@gmail.com>
2021-12-16 19:26:36 +0900

    コンポーネント自体への型定義

--- react/react_ts/src/App.tsx
+++ react/react_ts/src/App.tsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { useState } from 'react';
 import { Todo } from './Todo';
 import { TodoType } from './types/todo'
+import { Text } from './Text';
 
 function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -14,6 +15,7 @@ function App() {
 
   return (
     <div className="App">
+      <Text color="red" fontSize="18px"></Text>
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo: TodoType) => (
         <Todo title={todo.title} userId={todo.userId} completed={todo.completed} key={todo.id}/>
--- react/react_ts/src/Todo.tsx
+++ react/react_ts/src/Todo.tsx
@@ -1,10 +1,9 @@
 import { TodoType } from './types/todo'
+import { VFC } from "react";
 
-export const Todo = (
-  // 必要な要素を選択するPick Partialを死使用することで任意の引数を表現できる
-  props: Pick<TodoType, "title" | "userId"> & Partial<Pick<TodoType, "completed">>
-  ) => {
+// 必要な要素を選択するPick Partialを死使用することで任意の引数を表現できる
+export const Todo: VFC<Pick<TodoType, "title" | "userId"> & Partial<Pick<TodoType, "completed">>> = (props) => {
   const { title, userId, completed = false } = props;
   const completedMark = completed ? "[完]" : "[未]"
-  return<p>{`${completedMark}${title}(ユーザー:${userId})`}</p>
+  return<p>{`${completedMark} ${title}(ユーザー:${userId})`}</p>
 }
