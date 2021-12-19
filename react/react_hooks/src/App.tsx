6f19575d4fbe606098e2780283e0eabdb10df068
kazunori_moriya <g.ff.k.96@gmail.com>
2021-12-19 21:51:27 +0900

    axios動作確認

--- react/react_hooks/src/App.tsx
+++ react/react_hooks/src/App.tsx
@@ -1,5 +1,9 @@
+import axios from 'axios';
+import { useState } from 'react';
 import './App.css';
 import { UserCard } from './components/UserCard';
+import { User } from './types/api/user';
+import { userProfile } from './types/userProfile';
 
 const user = {
   id : 1,
@@ -7,10 +11,27 @@ const user = {
   email: 'kazu@gmail.com',
   address: 'kazu@sai',
 }
+
+
 function App() {
+  const [userProfiles, setUserProfiles] = useState<Array<userProfile>>([])
+  const onCLickFetchUsr = () => {
+    axios.get<Array<User>>('https://jsonplaceholder.typicode.com/users').then((response) => {
+      const data = response.data.map((user) => ({
+        id : user.id,
+        name : `${user.name}(${user.username})`,
+        email : user.email,
+        address : `${user.address.city} ${user.address.suite} ${user.address.city}`
+      }))
+      setUserProfiles(data)
+    })
+  }
   return (
     <div className="App">
-        <UserCard user={user}></UserCard>
+      <button onClick={onCLickFetchUsr}>データ取得</button>
+      {userProfiles.map((user) => (
+        <UserCard key={user.id} user={user}></UserCard>
+      ))}
     </div>
   );
 }
