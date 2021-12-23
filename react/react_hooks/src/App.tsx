c6a4df6efa7c3074234c0e83e1132108f9756d3b
kazunori_moriya <g.ff.k.96@gmail.com>
2021-12-23 18:57:03 +0900

    loading error情報を表示

--- react/react_hooks/src/App.tsx
+++ react/react_hooks/src/App.tsx
@@ -7,7 +7,13 @@ import { userProfile } from './types/userProfile';
 
 function App() {
   const [userProfiles, setUserProfiles] = useState<Array<userProfile>>([])
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
   const onCLickFetchUser = () => {
+    setLoading(true);
+    setError(false);
+
     axios.get<Array<User>>('https://jsonplaceholder.typicode.com/users').then((response) => {
       //取得したデータを整形してdataへ格納
       const data = response.data.map((user) => ({
@@ -17,14 +23,28 @@ function App() {
         address : `${user.address.city} ${user.address.suite} ${user.address.city}`
       }))
       setUserProfiles(data)
+    }).catch(() => {
+      setError(true)
+    }).finally(() => {
+      setLoading(false);
     })
   }
   return (
     <div className="App">
       <button onClick={onCLickFetchUser}>データ取得</button>
-      {userProfiles.map((user) => (
-        <UserCard key={user.id} user={user}></UserCard>
-      ))}
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}>データの取得に失敗しました。</p>
+      ) : loading ? (
+        <p>loading...</p>
+      ) : (
+        //errorでもなくloadingでも無い場合は結果を表示する
+        <>
+        {userProfiles.map((user) => (
+          <UserCard key={user.id} user={user}></UserCard>
+        ))}
+        </>
+      )}
     </div>
   );
 }
