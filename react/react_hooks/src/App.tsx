10158313c69bfa7703c0cad2f4fc40974dc86cc3
kazunori_moriya <g.ff.k.96@gmail.com>
2021-12-26 15:08:42 +0900

    カスタムフックの利用

--- react/react_hooks/src/App.tsx
+++ react/react_hooks/src/App.tsx
@@ -1,38 +1,12 @@
-import axios from 'axios';
-import { useState } from 'react';
 import './App.css';
+import { useAllUsers } from './hooks/useAllUsers';
 import { UserCard } from './components/UserCard';
-import { User } from './types/api/user';
-import { userProfile } from './types/userProfile';
 
 function App() {
-  const [userProfiles, setUserProfiles] = useState<Array<userProfile>>([])
-  // 読み込み中か判定するstate
-  const [loading, setLoading] = useState(false);
-  // API取得結果がerrorか判定するstate
-  const [error, setError] = useState(false);
+  const { getUSers, userProfiles, loading, error} = useAllUsers();
 
-  const onCLickFetchUser = () => {
-    setLoading(true);
-    setError(false);
+  const onCLickFetchUser = () => getUSers()
 
-    axios.get<Array<User>>('https://jsonplaceholder.typicode.com/users').then((response) => {
-      //取得したデータを整形してdataへ格納
-      const data = response.data.map((user) => ({
-        id : user.id,
-        name : `${user.name}(${user.username})`,
-        email : user.email,
-        address : `${user.address.city} ${user.address.suite} ${user.address.city}`
-      }))
-      setUserProfiles(data)
-    }).catch(() => {
-      // エラーをキャッチしたら実行される
-      setError(true)
-    }).finally(() => {
-      // then or catchの後実行される
-      setLoading(false);
-    })
-  }
   return (
     <div className="App">
       <button onClick={onCLickFetchUser}>データ取得</button>
