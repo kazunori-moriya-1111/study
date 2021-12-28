5142e2c8df79c671adb80186a410579501b49f7e
kazunori_moriya <g.ff.k.96@gmail.com>
2021-12-28 17:51:39 +0900

    ヘッダーの画面遷移を実装

--- react/react_chakra/src/components/organisms/layout/Header.tsx
+++ react/react_chakra/src/components/organisms/layout/Header.tsx
@@ -1,26 +1,37 @@
-import { memo, VFC } from 'react'
+import { memo, useCallback, VFC } from 'react'
 import { Box, Flex, Heading, Link, useDisclosure } from '@chakra-ui/react'
+import { useNavigate } from 'react-router-dom'
 
 import { MenuIconButton } from '../../atoms/button/MenuIconBotton';
 import { MenuDrawer } from '../../molecules/MenuDrawer';
 
 export const Header: VFC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const navigate = useNavigate();
+
+  const onClickHome = useCallback(() => navigate("/home"), [])
+  const onClickUserManegement = useCallback(() => navigate("/home/user_manegement"), [])
+  const onClickSetting = useCallback(() => navigate("/home/setting"), [])
   return (
     <>
     <Flex as="nav" bg="teal.500" color="gray.50" align="center" justify="space-between" padding={{ base: 3 , md: 5 }}>
-      <Flex align="center" as="a" mr={8} _hover={{ cursor: 'pointer'}}>
+      <Flex align="center" as="a" mr={8} _hover={{ cursor: 'pointer'}} onClick={onClickHome}>
         <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>ユーザ管理アプリ</Heading>
       </Flex>
       <Flex align="center" fontSize="sm" flexGrow={2} display={{ base: "none", md: "flex" }}>
         <Box pr={4}>
-          <Link>ユーザー一覧</Link>
+          <Link onClick={onClickUserManegement}>ユーザー一覧</Link>
         </Box>
-        <Link>設定</Link>
+        <Link onClick={onClickSetting}>設定</Link>
       </Flex>
       <MenuIconButton onOpen={onOpen} />
     </Flex>
-    <MenuDrawer isOpen={isOpen} onClose={onClose} />
+    <MenuDrawer
+      isOpen={isOpen}
+      onClose={onClose}
+      onClickHome={onClickHome}
+      onClickUserManegement={onClickUserManegement}
+      onClickSetting={onClickSetting}/>
     </>
   )
 })
