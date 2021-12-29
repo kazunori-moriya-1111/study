4d3a146d492d45238fbabad751a77614f5d30bf2
kazunori_moriya <g.ff.k.96@gmail.com>
2021-12-29 15:46:13 +0900

    userCardの実装

--- react/react_chakra/src/components/pages/UserManegement.tsx
+++ react/react_chakra/src/components/pages/UserManegement.tsx
@@ -1,5 +1,13 @@
 import { memo, VFC } from 'react'
+import { Wrap, WrapItem } from '@chakra-ui/react'
+import { UserCard } from '../organisms/user/UserCard'
 
 export const UserManegement: VFC = memo(() => {
-  return <p>ユーザ管理ページです</p>
+  return(
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <UserCard ImageUrl={"https://source.unsplash.com/random"} userName="m" fullName="kafe"></UserCard>
+      </WrapItem>
+    </Wrap>
+  )
 })
