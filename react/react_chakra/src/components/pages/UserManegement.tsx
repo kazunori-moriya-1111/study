55ab72fc0bcbc73d89c5bc55754e8c04de161cfb
kazunori_moriya <g.ff.k.96@gmail.com>
2021-12-29 16:38:12 +0900

    ユーザ一覧取得機能実装

--- react/react_chakra/src/components/pages/UserManegement.tsx
+++ react/react_chakra/src/components/pages/UserManegement.tsx
@@ -1,13 +1,35 @@
-import { memo, VFC } from 'react'
-import { Wrap, WrapItem } from '@chakra-ui/react'
+import { memo, useEffect, VFC } from 'react'
+import { Center, Spinner, Wrap, WrapItem } from '@chakra-ui/react'
 import { UserCard } from '../organisms/user/UserCard'
+import { useAllUsers } from '../../hooks/useAllUsers'
 
 export const UserManegement: VFC = memo(() => {
+  const { getUsers, users, loading } = useAllUsers()
+
+  //第二引数を空配列にすることで最初の一回のみ動作するようになる
+  useEffect(() => getUsers(), [])
+
   return(
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard ImageUrl={"https://source.unsplash.com/random"} userName="m" fullName="kafe"></UserCard>
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        // ローディング中の表示内容
+        <Center h="100vh">
+          <Spinner/>
+        </Center>
+      ) : (
+        // ローディング後の表示内容
+        <Wrap p={{ base: 4, md: 10 }} justify="center">
+          {users.map((user) => (
+            <WrapItem key={user.id}>
+              <UserCard
+                ImageUrl={"https://source.unsplash.com/random"}
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   )
 })
