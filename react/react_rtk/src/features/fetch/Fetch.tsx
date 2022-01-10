18a55ad21a7f85325aab60e046e3ded2771fbd4c
kazunori_moriya <g.ff.k.96@gmail.com>
2022-01-10 12:15:21 +0900

    UC 80 check

--- react/react_rtk/src/features/fetch/Fetch.tsx
+++ react/react_rtk/src/features/fetch/Fetch.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@chakra-ui/react";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchAsyncGet, selectUsers } from "./fetchSlice";
@@ -27,16 +28,44 @@ interface user {
 }
 
 export const Fetch = () => {
+  // UC true 80
+  // 当たり 31970/1313967 1~31970
+  // 転落 4110/1313967 31971 ~ 36080
+  var random = 0
+  var atari = 0
+  var tenraku = 0
+  const judge = () => {
+    do {
+      random = Math.floor( Math.random() * 1313967) + 1
+    } while (random >= 36081)
+    console.log("random:",random)
+    var result = random <= 31970 ? true : false
+    // console.log(result)
+    return result
+  }
+  const probability = () => {
+    const trailCount = 100000
+    for (let i = 0; i < trailCount; i++){
+      console.log(judge())
+      judge() ? atari++ : tenraku++
+    }
+    console.log("atari:", atari,"tenraku:", tenraku)
+    console.log("結果", atari/trailCount)
+  }
+  // UC true 80 end
   const dispatch = useDispatch();
   const users = useSelector(selectUsers);
   useEffect(() => {
     dispatch(fetchAsyncGet())
   }, [dispatch])
   return (
-    <div>
-      {users.map((user:user) => (
-        <div key={user.id}>{user.email}</div>
-      ))}
-    </div>
+    <>
+      <Button onClick={probability}>ユニコーン</Button>
+      <div>
+        {users.map((user:user) => (
+          <div key={user.id}>{user.email}</div>
+        ))}
+      </div>
+    </>
   )
 }
