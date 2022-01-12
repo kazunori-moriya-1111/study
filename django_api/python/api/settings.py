9b01ffa42b8d8d3dfbe9b58e182fe8fe2c0f5c1b
kazunori_moriya <g.ff.k.96@gmail.com>
2022-01-12 20:01:32 +0900

    settings.py urls.py更新

--- django_api/python/api/settings.py
+++ django_api/python/api/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'task_base_api.apps.TaskBaseApiConfig',
+    'app.apps.AppConfig',
     'corsheaders',
     'djoser',
 ]
--- django_api/python/api/urls.py
+++ django_api/python/api/urls.py
@@ -19,6 +19,6 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include("api.urls")),
+    path('api/', include("app.urls")),
     path('authen/', include("djoser.urls.jwt")),
 ]
