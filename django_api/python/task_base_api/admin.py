9228337ccaccd04da1fc12a01a7f50a4cff3c417
kazunori_moriya <g.ff.k.96@gmail.com>
2022-01-12 19:50:40 +0900

    不要ファイル削除

--- django_api/python/task_base_api/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Task
-# Register your models here.
-
-# プロジェクト名にアンスコを使用しているため、mikemigrationsが動作しない可能性が高い
-admin.site.register(Task)
--- django_api/python/task_base_api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include
-from django.urls import path
-from rest_framework import routers
-
-router = routers.DefaultRouters()
-
-urlpatterns = [
-  
-]
