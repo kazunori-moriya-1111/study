0ad6eb3fb635ea09df4afba70023deb53bfa6938
kazunori_moriya <g.ff.k.96@gmail.com>
2022-01-14 20:18:58 +0900

    views.py urls.py更新

--- django_api/python/app/urls.py
+++ django_api/python/app/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import include
 from django.urls import path
 from rest_framework import routers
+from app.views import TaskViewSet, CreateUserView, MyProfileView
 
 router = routers.DefaultRouter()
+router.register('tasks', TaskViewSet, basename='tasks')
 
 urlpatterns = [
-  path('', include(router.urls))
+  path('myself/', MyProfileView.as_view(), name='myself'),
+  path('register/', CreateUserView.as_view(), name='register'),
+  path('', include(router.urls)),
 ]
