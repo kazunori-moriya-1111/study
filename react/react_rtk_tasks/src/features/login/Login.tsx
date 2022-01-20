e1afee5c5117de01d5e43e19988511765c4bbc2d
kazunori_moriya <g.ff.k.96@gmail.com>
2022-01-20 13:29:51 +0900

    ログインコンポーネント実装

--- react/react_rtk_tasks/src/features/login/Login.tsx
+++ react/react_rtk_tasks/src/features/login/Login.tsx
@@ -1,3 +1,63 @@
+import Button from '@material-ui/core/Button'
+import { useSelector, useDispatch } from 'react-redux'
+import styles from './Login.module.css'
+import { editUsername, editPassword, toggleMode, fetchAsyncLogin, fetchAsyncRegister, selectAuthen, selectIsloginView } from './loginSlice'
+
 export const Login = () => {
-  return <div>Hello login</div>
+  const dispatch = useDispatch()
+  const authen = useSelector(selectAuthen)
+  const isLoginView = useSelector(selectIsloginView)
+  const btnDisabler = authen.username === '' || authen.password === ''
+  
+  const login = async () => {
+    if(isLoginView) {
+      await dispatch(fetchAsyncLogin(authen))
+    }else{
+      const result:any = await dispatch(fetchAsyncRegister(authen))
+      if (fetchAsyncRegister.fulfilled.match(result)){
+        await dispatch(fetchAsyncLogin(authen))
+      }
+    }
+  }
+  return (
+    <div className={styles.containerLogin}>
+      <div className={styles.appLogin}>
+        <h1>{isLoginView ? "Login" : "Register"}</h1>
+        <span>Username</span>
+        <input 
+          type="text"
+          className={styles.inputLog}
+          name="username"
+          placeholder=""
+          onChange={(e) => dispatch(editUsername(e.target.value))}
+          required
+        />
+        <span>Password</span>
+        <input 
+          type="password"
+          className={styles.inputLog}
+          name="password"
+          placeholder=""
+          onChange={(e) => dispatch(editPassword(e.target.value))}
+          required
+        />
+        <div className={styles.switch}>
+          <Button
+            variant="contained"
+            disabled={btnDisabler}
+            color="primary"
+            onClick={login}
+          >
+            {isLoginView ? "Login" : "Create"}
+          </Button>
+        </div>
+        <span
+          className={styles.switchText}
+          onClick={() => dispatch(toggleMode())}
+        >
+          {isLoginView ? "Create Account ?" : "Back to Login"}
+        </span>
+      </div>
+    </div>
+  )
 }
