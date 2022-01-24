a2f2a5ea388c4c25cb95a404bb80e34c0c2051d5
kazunori_moriya <g.ff.k.96@gmail.com>
2022-01-24 20:01:15 +0900

    インデント修正

--- react/react_rtk_tasks/src/App.tsx
+++ react/react_rtk_tasks/src/App.tsx
@@ -7,15 +7,15 @@ function App() {
     localStorage.removeItem("localJWT")
     window.location.href = "/"
   }
-   return (
-    <div className={styles.containerTasks}>
-      <div className={styles.appTasks}>
-        <button onClick={Logout} className={styles.signBtn}>
-          <FaSignInAlt />
-        </button>
-      </div>
-      <div className={styles.appDetails}></div>
+  return (
+  <div className={styles.containerTasks}>
+    <div className={styles.appTasks}>
+      <button onClick={Logout} className={styles.signBtn}>
+        <FaSignInAlt />
+      </button>
     </div>
+    <div className={styles.appDetails}></div>
+  </div>
   );
 }
 
