bb8f8e95820cda1c63056257529798eb5d92299b
kazunori_moriya <g.ff.k.96@gmail.com>
2022-01-28 08:23:23 +0900

    仮のtaskitemコンポーネント実装

---
+++ react/react_rtk_tasks/src/features/task/TaskItem.tsx
@@ -0,0 +1,13 @@
+import styles from "./TaskItem.module.css"
+
+import { BsTrash } from "react-icons/bs"
+import { FaEdit } from "react-icons/fa"
+import { useDispatch } from "react-redux";
+import { fetchAsyncDelete, selectTasks, editTask } from "./taskSlice";
+
+const TaskItem = () => {
+  return <div></div>
+  
+}
+
+export default TaskItem
