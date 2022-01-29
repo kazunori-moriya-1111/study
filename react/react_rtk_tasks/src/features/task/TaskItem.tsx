6dfd870c0ad58ee408eb4384c82fe6814439fd37
kazunori_moriya <g.ff.k.96@gmail.com>
2022-01-29 18:50:43 +0900

    TaskListコンポーネント更新

--- react/react_rtk_tasks/src/features/task/TaskItem.tsx
+++ react/react_rtk_tasks/src/features/task/TaskItem.tsx
@@ -3,10 +3,34 @@ import styles from "./TaskItem.module.css"
 import { BsTrash } from "react-icons/bs"
 import { FaEdit } from "react-icons/fa"
 import { useDispatch } from "react-redux";
-import { fetchAsyncDelete, selectTasks, editTask } from "./taskSlice";
+import { fetchAsyncDelete, editTask, selectTask } from "./taskSlice";
 
-const TaskItem = () => {
-  return <div></div>
+const TaskItem = ({task}:any) => {
+  const dispatch = useDispatch()
+  return (
+    <li className={styles.listItem}>
+      <span
+        className={styles.cursor}
+        onClick={() => dispatch(selectTask(task))}
+      >
+        {task.title}
+      </span>
+      <div>
+        <button
+          onClick={() => dispatch(fetchAsyncDelete(task.id))}
+          className={styles.taskIcon}
+        >
+          <BsTrash />
+        </button>
+        <button
+          onClick={() => dispatch(editTask(task))}
+          className={styles.taskIcon}
+        >
+          <FaEdit />
+        </button>
+      </div>
+    </li>
+  )
   
 }
 
