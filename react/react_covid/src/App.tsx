7a972bcab3607f02cc5baa32468a0309c6c4e466
kazunori_moriya <g.ff.k.96@gmail.com>
2022-02-06 13:35:47 +0900

    棒グラフ実装

--- react/react_covid/src/App.tsx
+++ react/react_covid/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import BarPlot from './features/chart/BarPlot';
 import LinePlot from './features/chart/LinePlot';
 import PiePlot from './features/chart/PiePlot';
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <LinePlot />
       <PiePlot />
+      <BarPlot />
     </div>
   );
 }
