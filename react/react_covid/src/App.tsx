974399c318629a3b55936b8148285e1aaa082177
kazunori_moriya <g.ff.k.96@gmail.com>
2022-02-06 12:45:40 +0900

    円グラフ実装

--- react/react_covid/src/App.tsx
+++ react/react_covid/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import LinePlot from './features/chart/LinePlot';
+import PiePlot from './features/chart/PiePlot';
 
 function App() {
   return (
     <div className="App">
       <LinePlot />
+      <PiePlot />
     </div>
   );
 }
