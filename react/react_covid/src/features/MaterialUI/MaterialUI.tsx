69e7b39bf503fd58141fd4a32468c90e637bb588
kazunori_moriya <g.ff.k.96@gmail.com>
2022-02-07 09:57:59 +0900

    makeStylesによるcss適用

--- react/react_covid/src/features/MaterialUI/MaterialUI.tsx
+++ react/react_covid/src/features/MaterialUI/MaterialUI.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
-import { Button } from '@material-ui/core'
+import { Button, makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles({
+  btnStyle: {
+    backgroundColor: "green",
+    padding: "3px 50px",
+  }
+})
 
 const MaterialUI : React.FC = () => {
-  return <div><Button variant="contained" color="primary">Test Button</Button></div>;
+  const classes = useStyles();
+  return (
+    <div>
+      <Button className={classes.btnStyle} variant="contained" color="primary">
+        Test Button
+      </Button>
+    </div>
+  );
 };
 
 export default MaterialUI;
