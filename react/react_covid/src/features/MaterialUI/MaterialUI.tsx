da32a511dab034d4b5f2ae315f99c930f70555da
kazunori_moriya <g.ff.k.96@gmail.com>
2022-02-10 11:00:51 +0900

    Grid実装

--- react/react_covid/src/features/MaterialUI/MaterialUI.tsx
+++ react/react_covid/src/features/MaterialUI/MaterialUI.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, makeStyles, Typography } from '@material-ui/core'
+import { Button, makeStyles, Typography, Grid, Paper } from '@material-ui/core'
 
 const useStyles = makeStyles({
   btnStyle: {
@@ -8,6 +8,10 @@ const useStyles = makeStyles({
   },
   typoStyle: {
     color: "blue"
+  },
+  paperStyle: {
+    backgroundColor: "orange",
+    height: "50px",
   }
 })
 
@@ -19,6 +23,30 @@ const MaterialUI : React.FC = () => {
       <Typography className={classes.typoStyle} color="secondary" variant="h1" gutterBottom component="h2">
         Hello UI
       </Typography>
+      <Grid container spacing={1}>
+        <Grid item xs={12}>
+          <Paper className={classes.paperStyle}>xs=12</Paper>
+        </Grid>
+        {/* xs mdを設定することでbreak pointの設定ができる */}
+        <Grid item xs={12} md={6}>
+          <Paper className={classes.paperStyle}>xs=6</Paper>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Paper className={classes.paperStyle}>xs=6</Paper>
+        </Grid>
+        <Grid item xs={6} md={3}>
+          <Paper className={classes.paperStyle}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={6} md={3}>
+          <Paper className={classes.paperStyle}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={6} md={3}>
+          <Paper className={classes.paperStyle}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={6} md={3}>
+          <Paper className={classes.paperStyle}>xs=3</Paper>
+        </Grid>
+      </Grid>
       <Button className={classes.btnStyle} variant="contained" color="primary">
         Test Button
       </Button>
