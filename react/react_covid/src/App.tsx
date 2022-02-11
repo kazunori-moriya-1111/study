4ad0836a96eae4f8a60c3de9f99155dced66727e
kazunori_moriya <g.ff.k.96@gmail.com>
2022-02-11 16:27:20 +0900

    Cardコンポーネント作成

--- react/react_covid/src/App.tsx
+++ react/react_covid/src/App.tsx
@@ -1,20 +1,10 @@
 import './App.css';
-import BarPlot from './features/chart/BarPlot';
-import BubblePlot from './features/chart/BubblePlot';
-import LinePlot from './features/chart/LinePlot';
-import PiePlot from './features/chart/PiePlot';
-import RadarPlot from './features/chart/RadarPlot';
-import MaterialUI from './features/MaterialUI/MaterialUI';
+import Cards from './features/covid/Cards/Cards';
 
 function App() {
   return (
     <div className="App">
-      {/* <LinePlot />
-      <PiePlot />
-      <BarPlot />
-      <RadarPlot />
-      <BubblePlot /> */}
-      <MaterialUI />
+      <Cards />
     </div>
   );
 }
