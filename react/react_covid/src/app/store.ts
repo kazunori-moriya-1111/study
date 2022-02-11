9acc09e295c04fce6aec9e3589d976edfc9f6745
kazunori_moriya <g.ff.k.96@gmail.com>
2022-02-11 15:50:08 +0900

    covidSliceをstoreへ登録

--- react/react_covid/src/app/store.ts
+++ react/react_covid/src/app/store.ts
@@ -1,7 +1,10 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import covidReducer from '../features/covid/covidSlice';
+
 
 export const store = configureStore({
   reducer: {
+    covid: covidReducer
   },
 });
 
