49889802dd364f99ef7329681f261fe2e8d22633
kazunori_moriya <g.ff.k.96@gmail.com>
2022-02-27 12:30:21 +0900

    model修正 - migrate実行

--- django_insta_api/python/src/api/models.py
+++ django_insta_api/python/src/api/models.py
@@ -34,7 +34,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
   email = models.EmailField(max_length=50, unique=True)
   is_active = models.BooleanField(default=True)
-  is_superuser = models.BooleanField(default=False)
+  is_staff = models.BooleanField(default=False)
   # ネストしたクラス定義 Userクラスのインスタンスでobject.create_userの様にUserManagerクラスのメソッドが使用可能になる
   object = UserManager()
 
