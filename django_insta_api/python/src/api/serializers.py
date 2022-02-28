943a7bf3b2237512a0f1ce798b2921b43011bb7a
kazunori_moriya <g.ff.k.96@gmail.com>
2022-02-28 19:09:17 +0900

    serializers実装

---
+++ django_insta_api/python/src/api/serializers.py
@@ -0,0 +1,35 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from .models import Profile, Post, Comment
+
+class UserSerializer(serializers.ModelSerializer):
+
+  class Meta:
+    # get_user_model() django標準のuser modelを取得する関数。今回はカスタムしたUserモデルを読んでいる
+    model = get_user_model()
+    fileds = ('id', 'email', 'password')
+    extra_kwargs = {'password': {'write_only': True}}
+  
+  def create(self, validated_data):
+    user = get_user_model().objects.create_user(**validated_data)
+    return user
+
+class ProfileSerializer(serializers.ModelSerializer):
+  created_on = serializers.DateTimeField(format="%Y-%m-%d", read_only=True)
+  class Meta:
+    model = Profile
+    fields = ('id', 'nickName', 'userProfile', 'created_on', 'img')
+    extra_kwargs = {'userProfile': {'read_only': True}}
+
+class PostSerializer(serializers.ModelSerializer):
+  created_on = serializers.DateTimeField(format="%Y-%m-%d", read_only=True)
+  class Meta:
+    model = Post
+    fields = ('id', 'title', 'userPost', 'created_on', 'img', 'liked')
+    extra_kwargs = {'userPost': {'read_only': True}}
+
+class CommentSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Comment
+    fields = ('id', 'text', 'userComment', 'post')
+    extra_kwargs = {'userComment': {'read_only': True}}
