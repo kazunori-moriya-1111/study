4c543f32088f91ed59428cb85d0f34ae6c1b5f47
kazunori_moriya <g.ff.k.96@gmail.com>
2022-03-02 09:36:29 +0900

    アプリケーション側のurls.py実装

---
+++ django_insta_api/python/src/api/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, include
+from . import views
+from rest_framework.routers import DefaultRouter
+
+app_name = 'django'
+
+router = DefaultRouter()
+# router.registerではviewsetsを継承したクラスが登録できる
+router.register('profile', views.ProfileViewSet)
+router.register('post', views.PostViewSet)
+router.register('comment', views.CommentViewSet)
+
+# genericsを継承したクラスを登録
+urlpatterns = [
+  path('register/', views.CreateUserView.as_view(), name='register'),
+  path('myprofile/', views.MyProfileListView.as_view(), name='myprofile'),
+  path('', include(router.urls))
+]
