82b2f317be64b08d9e2c4a135bf1aaf5b0e4621f
kazunori_moriya <g.ff.k.96@gmail.com>
2022-03-11 20:42:34 +0900

    非同期関数を実装

--- react/react_insta/src/features/auth/authSlice.ts
+++ react/react_insta/src/features/auth/authSlice.ts
@@ -5,6 +5,92 @@ import { PROPS_AUTHEN, PROPS_PROFILE, PROPS_NICKNAME } from "../types"
 
 const apiUrl = process.env.REACT_APP_DEV_API_URL;
 
+// jwtトークンを取得する関数
+export const fetchAsyncLogin = createAsyncThunk(
+  "auth/post",
+  async (authen: PROPS_AUTHEN) => {
+    const res = await axios.post(`${apiUrl}authen/jwt/create`, authen, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return res.data
+  }
+);
+
+// 新規アカウント作成する関数
+export const fetchAsyncRegister = createAsyncThunk(
+  "auth/register",
+  async (auth: PROPS_AUTHEN) => {
+    const res = await axios.post(`${apiUrl}api/register`, auth, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return res.data;
+  }
+)
+
+// プロフィールを作成する関数
+export const fetchAsyncCreateProf = createAsyncThunk(
+  "profile/post",
+  async (nickName: PROPS_NICKNAME) => {
+    const res = await axios.post(`${apiUrl}api/profile/`, nickName, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `JWT ${localStorage.localJWT}`,
+      },
+    });
+    return res.data;
+  }
+)
+
+// プロフィールを更新する関数
+export const fetchAsyncUpdateProf = createAsyncThunk(
+  "profile/put",
+  async (profile: PROPS_PROFILE) => {
+    const uploadData = new FormData();
+    uploadData.append('nickName', profile.nickName);
+    profile.img && uploadData.append('img', profile.img, profile.img.name);
+    const res = await axios.put(
+      `${apiUrl}api/profile/${profile.id}`,
+      uploadData,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `JWT ${localStorage.localJWT}`,
+        },
+      }
+    );
+  }
+)
+
+// ログインしているユーザのプロフィールを取得する関数
+export const fetchAsyncGetMyProf = createAsyncThunk(
+  'profile/get',
+  async() => {
+    const res = await axios.get(`${apiUrl}api/myprofile/`, {
+      headers: {
+        Authorization: `JWT ${localStorage.localJWT}`,
+      },
+    });
+    return res.data[0]
+  }
+);
+
+// 全ユーザのプロフィールを取得する関数
+export const fetchAsyncGetProfs = createAsyncThunk(
+  'profiles/get',
+  async() => {
+    const res = await axios.get(`${apiUrl}api/profile/`, {
+      headers: {
+        Authorization: `JWT ${localStorage.localJWT}`,
+      },
+    });
+    return res.data
+  }
+);
+
 export const authSlice = createSlice({
   name: 'auth',
   initialState: {
