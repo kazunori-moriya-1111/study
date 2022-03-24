785f7c83fa860dba4a5fc7c672b3781878ad28d6
kazunori_moriya <g.ff.k.96@gmail.com>
2022-03-24 09:02:27 +0900

    postSliceの非同期関数とinitial stateを定義

---
+++ react/react_insta/src/features/post/postSlice.ts
@@ -0,0 +1,156 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { RootState } from '../../app/store';
+import axios from "axios"
+import { PROPS_NEWPOST, PROPS_LIKED, PROPS_COMMENT } from "../types"
+
+const apiUrlPost = `${process.env.REACT_APP_DEV_API_URL}api/post/`;
+const apiUrlComment = `${process.env.REACT_APP_DEV_API_URL}api/comment/`
+
+// 投稿の一覧を取得する関数
+export const fetchAsyncGetPosts = createAsyncThunk("post/get", async () => {
+  const res = await axios.get(apiUrlPost, {
+    headers: {
+      Authorization: `JWT ${localStorage.localJWT}`,
+    },
+  });
+  return res.data;
+});
+
+// 新規投稿を作成する関数
+export const fetchAsyncNewPost = createAsyncThunk(
+  "post/post",
+  async (newPost: PROPS_NEWPOST) => {
+    const uploadData = new FormData();
+    uploadData.append("title", newPost.title);
+    // 第3引数はファイル名
+    newPost.img && uploadData.append("img", newPost.img, newPost.img.name);
+    const res = await axios.post(apiUrlPost, uploadData, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `JWT ${localStorage.localJWT}`,
+      },
+    });
+    return res.data;
+  }
+);
+
+// 投稿のいいねを更新する関数
+export const fetchAsyncPatchLiked = createAsyncThunk(
+  "post/patch",
+  async (liked: PROPS_LIKED) => {
+    const currentLiked = liked.current; //現在のいいねしているユーザID
+    const uploadData = new FormData();
+
+    let isOverlapped = false;
+    currentLiked.forEach((current) => {
+      if (current === liked.new) {
+        isOverlapped = true;
+      } else {
+        uploadData.append("liked", String(current));
+      }
+    });
+
+    if (!isOverlapped) {
+      uploadData.append("liked", String(liked.new));
+    } else if (currentLiked.length === 1) {
+      // いいねした人をブランクにしたい場合
+      uploadData.append("title", liked.titile);
+      const res = await  axios.put(`${apiUrlPost}${liked.id}/`, uploadData, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `JWT ${localStorage.localJWT}`,
+        },
+      });
+      return res.data;
+    }
+    const res = await axios.patch(`${apiUrlPost}${liked.id}/`, uploadData, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `JWT ${localStorage.localJWT}`,
+      },
+    });
+    return res.data;
+  }
+);
+
+// コメントの一覧を取得する関数
+export const fetchAsyncGetComments = createAsyncThunk(
+  "comment/get",
+  async () => {
+    const res = await axios.get(apiUrlComment, {
+      headers: {
+        Authorization: `JWT ${localStorage.localJWT}`,
+      },
+    });
+    return res.data;
+  }
+);
+
+// コメントを新規で作成する関数
+export const fetchAsyncPostComment = createAsyncThunk(
+  "comment/post",
+  async (comment: PROPS_COMMENT) => {
+    const res = await axios.post(apiUrlComment, comment, {
+      headers: {
+        Authorization: `JWT ${localStorage.localJWT}`,
+      },
+    });
+    return res.data;
+  }
+);
+
+export const postSlice = createSlice({
+  name: 'post',
+  initialState: {
+    isLoadingPost: false,
+    openNewPost: false,
+    posts: [
+      {
+        id: 0,
+        title: "",
+        userPost: 0,
+        created_on: "",
+        img: "",
+        liked: [0],
+      },
+    ],
+    comments: [
+      {
+        id: 0,
+        text: "",
+        userComment: 0,
+        post: 0
+      }
+    ],
+  },
+  reducers: {
+    fetchPostStart(state) {
+      state.isLoadingPost = true;
+    },
+    fetchPostEnd(state) {
+      state.isLoadingPost = false;
+    },
+    setOpenNewPost(state) {
+      state.openNewPost = true;
+    },
+    resetOpenNewPost(state) {
+      state.openNewPost = false; 
+    }
+  },
+  extraReducers: (builder) => {
+    
+  },
+});
+
+export const {
+  
+} = postSlice.actions;
+
+export const selectIsLoadingAuth = (state: RootState) => state.auth.isLoadingAuth
+export const selectOpenSignIn = (state: RootState) => state.auth.openSignIn;
+export const selectOpenSignUp = (state: RootState) => state.auth.openSignUp;
+export const selectOpenProfile = (state: RootState) => state.auth.openProfile;
+export const selectProfile = (state: RootState) => state.auth.myprofile;
+export const selectProfiles = (state: RootState) => state.auth.profiles;
+
+export default postSlice.reducer;
