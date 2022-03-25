627d1f9f2558be2f7b1f920b5d4d359504f8545d
kazunori_moriya <g.ff.k.96@gmail.com>
2022-03-25 10:56:17 +0900

    extraReducers、useSelector用変数実装

--- react/react_insta/src/features/post/postSlice.ts
+++ react/react_insta/src/features/post/postSlice.ts
@@ -138,19 +138,51 @@ export const postSlice = createSlice({
     }
   },
   extraReducers: (builder) => {
-    
+    builder.addCase(fetchAsyncGetPosts.fulfilled, (state, action) => {
+      return {
+        ...state,
+        posts: action.payload,
+      };
+    });
+    builder.addCase(fetchAsyncNewPost.fulfilled, (state, action) => {
+      return {
+        ...state,
+        posts: [...state.posts, action.payload],
+      };
+    });
+    builder.addCase(fetchAsyncGetComments.fulfilled, (state, action) => {
+      return {
+        ...state,
+        components: action.payload
+      };
+    });
+    builder.addCase(fetchAsyncPostComment.fulfilled, (state, action) => {
+      return {
+        ...state,
+        comments: [...state.comments, action.payload],
+      };
+    });
+    builder.addCase(fetchAsyncPatchLiked.fulfilled, (state, action) => {
+      return {
+        ...state,
+        posts: state.posts.map((post) => 
+          post.id === action.payload.id ? action.payload : post
+        ),
+      };
+    });
   },
 });
 
 export const {
-  
+  fetchPostStart,
+  fetchPostEnd,
+  setOpenNewPost,
+  resetOpenNewPost,
 } = postSlice.actions;
 
-export const selectIsLoadingAuth = (state: RootState) => state.auth.isLoadingAuth
-export const selectOpenSignIn = (state: RootState) => state.auth.openSignIn;
-export const selectOpenSignUp = (state: RootState) => state.auth.openSignUp;
-export const selectOpenProfile = (state: RootState) => state.auth.openProfile;
-export const selectProfile = (state: RootState) => state.auth.myprofile;
-export const selectProfiles = (state: RootState) => state.auth.profiles;
+export const selectIsLoadingPost = (state: RootState) => state.post.isLoadingPost
+export const selectOpenNewPost = (state: RootState) => state.post.openNewPost
+export const selectPosts = (state: RootState) => state.post.posts
+export const selectComments = (state: RootState) => state.post.comments
 
 export default postSlice.reducer;
