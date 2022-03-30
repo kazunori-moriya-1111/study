04a9084cba5abaca5860d773f201edd1c40952ba
kazunori_moriya <g.ff.k.96@gmail.com>
2022-03-30 09:45:31 +0900

    ログアウト機能修正

--- react/react_insta/src/features/core/Core.tsx
+++ react/react_insta/src/features/core/Core.tsx
@@ -102,99 +102,37 @@ const Core: React.FC = () => {
   return (
     <div>
       <Auth />
-      <EditProfile />
-      <NewPost />
       <div className={styles.core_header}>
         <h1 className={styles.core_title}>SNS clone</h1>
+        {/* profile?はprofileが存在する時に式の評価を行う */}
         {profile?.nickName ? (
+          // ログインしている時
           <>
             <button
               className={styles.core_btnModal}
               onClick={() => {
-                dispatch(setOpenNewPost());
-                dispatch(resetOpenProfile());
+                dispatch(setOpenNewPost())
+                dispatch(resetOpenProfile())
               }}
             >
               <MdAddAPhoto />
             </button>
             <div className={styles.core_logout}>
-              {(isLoadingPost || isLoadingAuth) && <CircularProgress />}
               <Button
                 onClick={() => {
-                  localStorage.removeItem("localJWT");
-                  dispatch(editNickname(""));
+                  localStorage.removeItem('localJWT')
+                  dispatch(editNickname(''))
                   dispatch(resetOpenProfile());
                   dispatch(resetOpenNewPost());
-                  dispatch(setOpenSignIn());
+                  dispatch(setOpenSignIn())
                 }}
               >
                 Logout
               </Button>
-              <button
-                className={styles.core_btnModal}
-                onClick={() => {
-                  dispatch(setOpenProfile());
-                  dispatch(resetOpenNewPost());
-                }}
-              >
-                <StyledBadge
-                  overlap="circle"
-                  anchorOrigin={{
-                    vertical: "bottom",
-                    horizontal: "right",
-                  }}
-                  variant="dot"
-                >
-                  <Avatar alt="who?" src={profile.img} />{" "}
-                </StyledBadge>
-              </button>
             </div>
           </>
-        ) : (
-          <div>
-            <Button
-              onClick={() => {
-                dispatch(setOpenSignIn());
-                dispatch(resetOpenSignUp());
-              }}
-            >
-              LogIn
-            </Button>
-            <Button
-              onClick={() => {
-                dispatch(setOpenSignUp());
-                dispatch(resetOpenSignIn());
-              }}
-            >
-              SignUp
-            </Button>
-          </div>
-        )}
+        ) : (<div></div>)}
       </div>
-
-      {profile?.nickName && (
-        <>
-          <div className={styles.core_posts}>
-            <Grid container spacing={4}>
-              {/* {posts
-                .slice(0)
-                .reverse()
-                .map((post) => (
-                  <Grid key={post.id} item xs={12} md={4}>
-                    <Post
-                      postId={post.id}
-                      title={post.title}
-                      loginId={profile.userProfile}
-                      userPost={post.userPost}
-                      imageUrl={post.img}
-                      liked={post.liked}
-                    />
-                  </Grid>
-                ))} */}
-            </Grid>
-          </div>
-        </>
-      )}
     </div>
   )
 }
