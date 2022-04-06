efae2d8610260375d435705b014b168789072ff4
kazunori_moriya <g.ff.k.96@gmail.com>
2022-04-06 22:37:33 +0900

    EditProfileコンポーネントimport処理追記

--- react/react_insta/src/features/core/EditProfile.tsx
+++ react/react_insta/src/features/core/EditProfile.tsx
@@ -1,6 +1,39 @@
-import React from 'react'
+import React, { useState }from 'react'
+import Modal from "react-modal";
+import styles from "./Core.module.css"
 
-const EditProfile = () => {
+import { useSelector, useDispatch } from 'react-redux';
+import { AppDispatch } from '../../app/store';
+
+import { File } from "../types"
+
+import {
+  editNickname,
+  selectProfile,
+  selectOpenProfile,
+  resetOpenProfile,
+  fetchCredStart,
+  fetchCredEnd,
+  fetchAsyncUpdateProf,
+} from "../auth/authSlice";
+
+import { Button, TextField, IconButton } from "@material-ui/core";
+import { MdAddAPhoto } from "react-icons/md";
+
+const customStyles = {
+  content: {
+    top: "55%",
+    left: "50%",
+
+    width: 280,
+    height: 220,
+    padding: "50px",
+
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+const EditProfile:React.FC = () => {
   return (
     <div>EditProfile</div>
   )
