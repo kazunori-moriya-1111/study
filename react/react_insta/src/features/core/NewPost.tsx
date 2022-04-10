84a5551435a45b65b8d12b7ac3ccf273c2c6324d
kazunori_moriya <g.ff.k.96@gmail.com>
2022-04-10 19:48:00 +0900

    NewPostコンポーネントのインポート処理実装

--- react/react_insta/src/features/core/NewPost.tsx
+++ react/react_insta/src/features/core/NewPost.tsx
@@ -1,4 +1,35 @@
-import React from 'react'
+import React, { useState } from "react";
+import Modal from "react-modal";
+import { useSelector, useDispatch } from "react-redux";
+import { AppDispatch } from "../../app/store";
+
+import styles from "./Core.module.css";
+
+import { File } from "../types";
+
+import {
+  selectOpenNewPost,
+  resetOpenNewPost,
+  fetchPostStart,
+  fetchPostEnd,
+  fetchAsyncNewPost,
+} from "../post/postSlice";
+
+import { Button, TextField, IconButton } from "@material-ui/core";
+import { MdAddAPhoto } from "react-icons/md";
+
+const customStyles = {
+  content: {
+    top: "55%",
+    left: "50%",
+
+    width: 280,
+    height: 220,
+    padding: "50px",
+
+    transform: "translate(-50%, -50%)",
+  },
+};
 
 const NewPost = () => {
   return (
