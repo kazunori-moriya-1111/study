09f14a3539b52a2315c64b897fb581d5e3f6186e
kazunori_moriya <g.ff.k.96@gmail.com>
2022-05-04 16:13:39 +0900

    view.py実装

--- django_nextjs_api/python/src/api/views.py
+++ django_nextjs_api/python/src/api/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
-
+from rest_framework.permissions import AllowAny
+from rest_framework import generics
+from rest_framework import viewsets
+from .seializers import TaskSerializer, UserSerializer, PostSerializer
+from .models import Task, Post
 # Create your views here.
+
+class CreateUserView(generic.CreateAPIView):
+  serializer_class = UserSerializer
+  permission_classes = (AllowAny,)
+
+class PosrListView(generic.ListAPIView):
+  queryset = Post.object.all()
+  serializer_class = PostsList
+  permisson_classes = (AllowAny,)
+
+class PostRetrieveView(generic.RetrieveAPIView):
+  queryset = Post.object.all()
+  serializer_class = PostSerializer
+  permission_classes = (AllowAny,)
+
+class TaskListView(generic.ListAPIView):
+  queryset = Task.objects.all()
+  serializer_class = TaskSerializer
+  permisson_classes = (AllowAny,)
+
+class TaskRetrieveView(generic.RetrieveAPIView):
+  queryset = Task.objects.all()
+  serializer_class = TaskSerializer
+  permisson_classes = (AllowAny,)
+
+class TaskViewSet(viewsets.ModelViewSet):
+  queryset = Task.objects.all()
+  serializer_class = TaskSerializer
