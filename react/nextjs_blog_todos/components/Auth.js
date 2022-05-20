966e8d0d8268d19db04b48dee7777a280e41ffab
kazunori_moriya <g.ff.k.96@gmail.com>
2022-05-20 09:11:37 +0900

    ログイン処理関数実装

--- react/nextjs_blog_todos/components/Auth.js
+++ react/nextjs_blog_todos/components/Auth.js
@@ -6,6 +6,39 @@ import Cookie from "universal-cookie";
 const cookie = new Cookie();
 
 export default function Auth() {
+  const router = useRouter();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLogin, setLogin] = useState(true);
+
+  const login = async () => {
+    try{
+      await fetch(
+        `S{process.env.NEXT_PUBLIC_RESTAPI_URL}api/auth/jwt/create/`,
+        {
+          method: "POST",
+          body: JSON.stringify({ username: username, password: password}),
+          headers:{
+            "Content-Type": "application/json",
+          },
+        }
+      ).then((res) => {
+        if(res.status === 400){
+          throw "authentication failed";
+        }else if(res.ok){
+          return res.json();
+        }
+      })
+      .then((data) => {
+        const options = { path: "/" };
+        cookie.set("access_token", data.access, options);
+      });
+      router.push("/main-page")
+    } catch (err) {
+      alert(err)
+    }
+  }
+
   return (
     <>
       <div className="max-w-md w-full space-y-8">
