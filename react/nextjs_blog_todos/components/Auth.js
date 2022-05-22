91bd5109db44150f60ce6512c04f0c294193d986
kazunori_moriya <g.ff.k.96@gmail.com>
2022-05-22 11:50:17 +0900

    ユーザ認証処理実装

--- react/nextjs_blog_todos/components/Auth.js
+++ react/nextjs_blog_todos/components/Auth.js
@@ -40,6 +40,30 @@ export default function Auth() {
     }
   }
 
+  // ユーザ認証処理
+  const authUser = async (e) => {
+    e.preventDefault();
+    if (isLogin) {
+      login()
+    } else {
+      try{
+        await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/register/`, {
+          method: "POST",
+          body: JSON.stringify({ username: username, password: password }),
+          headers: {
+            'Content-Type': 'application/json'
+          },
+        }).then((res) => {
+          if (res.status === 400) {
+            throw "authentication failed";
+          }
+        });
+        login()
+      } catch (err){
+        alert(err)
+      }
+    }
+  }
   return (
     <>
       <div className="max-w-md w-full space-y-8">
