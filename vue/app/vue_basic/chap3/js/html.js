487470e1289eea6db12b3248ffc491b65614951f
kazunori_moriya <g.ff.k.96@gmail.com>
2022-07-04 08:58:03 +0900

    v-htmlディレクティブの使い方

---
+++ vue/app/vue_basic/chap3/js/html.js
@@ -0,0 +1,8 @@
+Vue.createApp({
+  data() {
+    return {
+      message: `<h3>WINGSプロジェクト</h3>
+      <img src="http://www.web-deli.com/image/linkbanner_l.gif" alt="ロゴ">`
+    }
+  }
+}).mount('#app');
