6718409e5b7bf2a480a4b98c5b16df6318a4bc8f
kazunori_moriya <g.ff.k.96@gmail.com>
2022-07-19 09:40:05 +0900

    propsを制限して堅牢性を向上する

--- vue/app/vue_basic/chap4/js/prop_basic.js
+++ vue/app/vue_basic/chap4/js/prop_basic.js
@@ -1,6 +1,12 @@
 Vue.createApp({})
   .component('my-hello',{
-    props: ['yourName'],
+    // props: [ 'yourName' ],
+    props: {
+      yourName:{
+        type: String,
+        required: true
+      }
+    },
     template: `<div>こんにちは、{{ yourName }}</div>`,
   })
   .mount('#app');
