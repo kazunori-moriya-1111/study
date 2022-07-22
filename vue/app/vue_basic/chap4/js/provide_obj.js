d2b0aebe0fd4c2f572017964826f5db105a26d4b
kazunori_moriya <g.ff.k.96@gmail.com>
2022-07-22 15:09:35 +0900

    provideでデータオブジェクトを引き渡し

---
+++ vue/app/vue_basic/chap4/js/provide_obj.js
@@ -0,0 +1,28 @@
+Vue.createApp({})
+  .component('my-parent',{
+    data(){
+      return{
+        title: 'Vue3 実践入門'
+      }
+    },
+    provide(){
+      return{
+        title: this.title
+      }
+    },
+    template: `<div id="parent">
+                <my-my />
+              </div>`,
+  })
+  .component('my-my',{
+    template: `<div id="my">
+                <my-child />
+              </div>`,
+  })
+  .component('my-child',{
+    inject: ['title'],
+    template: `<div id="child">
+                {{ title }}
+              </div>`,
+  })
+  .mount('#app');
