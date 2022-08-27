6d517895af4c58290ce11477be2db131ecb8f1ff
kazunori_moriya <g.ff.k.96@gmail.com>
2022-08-27 17:30:43 +0900

    テーブルへ表示するデータ計算処理をざっくり

--- vue/app/simulation/js/index.js
+++ vue/app/simulation/js/index.js
@@ -4,12 +4,27 @@ Vue.createApp({
       remaining: '',
       rate:'',
       isShow:false,
+      data:[
+        {
+          ym: '2022/09',
+          paid: 30000,
+          interest: 3000,
+          remaining: 50000,
+        },
+        {
+          ym: '2022/10',
+          paid: 40000,
+          interest: 4000,
+          remaining: 40000,
+        }
+      ]
     };
   },
   methods: {
     // クリック時に関数をまとめて動作
     onclick(){
       this.toInt()
+      this.createTable()
       // 残金と利率が正常に数値変化できていれば後続処理を実施する
       if(!this.remaining || !this.rate){ return; }
       console.log("後続処理実施")
@@ -28,7 +43,27 @@ Vue.createApp({
         this.rate = ''
       }
     },
-    // table作成
+    // tableで表示するデータ作成
+    createTable(){
+      this.isShow = true
+      const date = new Date()
+      const startMonth = date.getMonth() + 2
+      const startYear = date.getFullYear()
+      var remainMoney = this.remaining
+      var PaidMoney = this.getPaidMoney(remainMoney)
+      var interestMoney = this.getInterestMoney(remainMoney)
+      // 残金を更新
+      for (let i = 1; i < 10; i++){
+        remainMoney += interestMoney
+        remainMoney -= PaidMoney
+        console.log("残金",remainMoney)
+        console.log("利息",interestMoney)
+        console.log("支払い金額",PaidMoney)
+        // 利息と支払い金額を再定義
+        PaidMoney = this.getPaidMoney(remainMoney)
+        interestMoney = this.getInterestMoney(remainMoney)
+      }
+    },
     // 残金から支払い金額を返却する
     getPaidMoney(remainMoney){
       if(remainMoney >= 400001){
