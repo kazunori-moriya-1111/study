264c24632e0ef276139327e168e4c939d092f772
kazunori_moriya <g.ff.k.96@gmail.com>
2022-09-05 17:05:19 +0900

    scoped cssでスタイルを設定

--- vue/app/vue_basic/chap7/src/App.vue
+++ vue/app/vue_basic/chap7/src/App.vue
@@ -2,6 +2,7 @@
   <img alt="Vue logo" src="./assets/logo.png">
   <!-- eslint-disable-next-line -->
   <MyCounter init='0'/>
+  <p>こんにちは、vue.js！</p>
 </template>
 
 <script>
@@ -16,7 +17,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +25,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+p {
+  border: 1px solid red;
+  background-color: yellow;
 }
 </style>
