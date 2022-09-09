4203f4e1c22712833e30fd050c8421423fe5f669
kazunori_moriya <g.ff.k.96@gmail.com>
2022-09-09 10:49:13 +0900

    画面遷移でパラメータを使用

--- vue/app/vue_basic/route-basic/src/App.vue
+++ vue/app/vue_basic/route-basic/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/article/108">記事NO.108488</router-link>
   </div>
   <router-view/>
 </template>
