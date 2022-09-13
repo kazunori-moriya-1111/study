b39804f185b601b973af5755b68f0abfe6beb170
kazunori_moriya <g.ff.k.96@gmail.com>
2022-09-13 09:20:19 +0900

    vue routerを用いた入れ子ページの設定

--- vue/app/vue_basic/route-nest/src/router/index.js
+++ vue/app/vue_basic/route-nest/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Article from '../views/Article.vue'
+import Page from '../views/Page.vue'
 
 const routes = [
   {
@@ -14,6 +16,26 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/article/:aid',
+    name: 'Article',
+    component: Article,
+    props: true,
+    children:[
+      {
+        path: 'pages/:page_num',
+        name: 'Page',
+        component: Page,
+        props: true
+      },
+      {
+        path: '',
+        name: 'PageOne',
+        component: Page,
+        props: { page_num: 1 }
+      }
+    ]
   }
 ]
 
---
+++ vue/app/vue_basic/route-nest/src/views/Page.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>{{ page_num }}</div>
+</template>
+
+<script>
+  export default {
+    name: 'PageView',
+    props: ['page_num']
+  }
+</script>
