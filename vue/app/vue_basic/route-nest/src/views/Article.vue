6c197ffa7f76687b95fb52b67117fad65a5d3066
kazunori_moriya <g.ff.k.96@gmail.com>
2022-09-14 10:39:15 +0900

    ナビゲーションガード実装

--- vue/app/vue_basic/route-nest/src/views/Article.vue
+++ vue/app/vue_basic/route-nest/src/views/Article.vue
@@ -1,16 +1,34 @@
 <template>
-  <div>
+  <div class="about">
     <div>記事コード：{{ aid }}</div>
-      <span><router-link v-bind:to="`/article/${aid}/pages/1`">Page:1</router-link></span>
-      <span><router-link v-bind:to="`/article/${aid}/pages/2`">Page:2</router-link></span>
-      <hr />
-      <router-view />
   </div>
 </template>
 
 <script>
+const timeGuard = function(to){
+  // 有効期限を設定
+  const data = {
+    13: new Date(2021, 10, 30),
+    108:  new Date(2022, 10, 30)
+  }
+
+  // 移動先のaid値から有効期限を取得
+  const limit = data[to.params.aid] ? data[to.params.aid] : new Data(2999, 11, 31)
+
+  // 現在時刻
+  const current = new Date()
+
+  // 有効期限が切れている場合、移動をキャンセル
+  if (limit && limit.getTime() < current.getTime()){
+    window.alert('記事の有効期限が過ぎています')
+    return false
+  }
+}
   export default{
     name: 'ArticleView',
+    // ナビゲーションガードを紐付け
+    beforeRouteEnter: timeGuard,
+    beforeRouteUpdate: timeGuard,
     props: ['aid']
   }
 </script>
