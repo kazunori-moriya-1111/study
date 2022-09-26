64803cc590365470a9d323d625b3dc7f5dd72de5
kazunori_moriya <g.ff.k.96@gmail.com>
2022-09-26 09:19:42 +0900

    setProps使い方

--- vue/app/vue_basic/test-unit/tests/unit/example.spec.js
+++ vue/app/vue_basic/test-unit/tests/unit/example.spec.js
@@ -2,11 +2,15 @@ import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
+  it('renders props.msg when passed', async () => {
     const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
       props: { msg }
     })
-    expect(wrapper.text()).toMatch(msg)
+
+    // msgプロパティを変更して、その結果を検証
+    const new_msg = 'こんにちは、Vue'
+    await wrapper.setProps({ msg: new_msg })
+    expect(wrapper.find('h1').text()).toBe(new_msg)
   })
 })
