b8f8776ac89dbc8ecae9b4eeaa69b4c79d0367bc
kazunori_moriya <g.ff.k.96@gmail.com>
2022-09-27 09:43:38 +0900

    view-routerを削除した挙動へ修正

--- vue/app/vue_basic/test-unit/src/App.vue
+++ vue/app/vue_basic/test-unit/src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
 
 <style>
 #app {
