1b7f8027f1a0d29782f69c680919c24365660280
kazunori_moriya <g.ff.k.96@gmail.com>
2022-09-28 09:35:55 +0900

    スタブをテストに反映する

--- vue/app/vue_basic/test-unit/tests/unit/mount.spec.js
+++ vue/app/vue_basic/test-unit/tests/unit/mount.spec.js
@@ -1,4 +1,5 @@
 import { shallowMount, mount } from '@vue/test-utils'
+import HelloStub from './HelloStub.vue'
 import App from '@/App.vue'
 
 describe('mountの基本', () => {
@@ -9,5 +10,16 @@ describe('mountの基本', () => {
     // それぞれのmount結果を表示
     console.log(shallow.html())
     console.log(deep.html())
+  })
+  it('CustomStub', () => {
+    const shallow = shallowMount(App, {
+      global: {
+        stubs: {
+          HelloWorld: HelloStub
+        }
+      }
+    })
+    // コンポーネントの処理結果を出力
+    console.log(shallow.html())
   })
 })
