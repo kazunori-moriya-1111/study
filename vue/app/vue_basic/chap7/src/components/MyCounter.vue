f25868deb2d0bc2d5ab815906450dfe946868eee
kazunori_moriya <g.ff.k.96@gmail.com>
2022-10-15 09:20:08 +0900

    expressから取得したデータを表示

--- vue/app/vue_basic/chap7/src/components/MyCounter.vue
+++ vue/app/vue_basic/chap7/src/components/MyCounter.vue
@@ -1,11 +1,14 @@
 <template>
   <div>現在値は{{ current }}です
     <input type="button" v-on:click="onclick" value="増やす" />
+  <!-- <input type="button" v-on:click="cdOnclick" value="せつぞく" />
+  せつぞくしたレスポンス{{ text }} -->
   </div>
 </template>
 
-<!-- <script>
+<script>
 import useCounter from '../composables/useCounter'
+// import conDatabase from '../composables/conDatabase'
 
 export default {
   name: 'MyCounter',
@@ -13,19 +16,27 @@ export default {
   setup(props) {
     // コンポジション関数からカウンター機能を取得
     const{ current, onclick } = useCounter(props.init)
+    // const{ text, cdOnclick } = conDatabase()
     return{
       current,
-      onclick
+      onclick,
+      // text,
+      // cdOnclick
     }
     
   },
 }
-</script> -->
-// setup属性を使用した記述
+</script>
+
+<!--// setup属性を使用した記述
 <script setup>
 import {defineProps} from 'vue'
 import useCounter from '../composables/useCounter'
+import conDatabase from '../composables/conDatabase'
 
+var aa = ""
 const props = defineProps(['init'])
 const { current, onclick } = useCounter(props.init)
-</script>
+const { text, cdOnclick } = conDatabase()
+aa = text
+</script> -->
