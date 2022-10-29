aa6d4f8751bb36e9b95f89e9fa19ab9192b6f8e1
kazunori_moriya <g.ff.k.96@gmail.com>
2022-10-29 15:22:06 +0900

    protected修飾子を用いたクラス継承

--- typescript/app/class.ts
+++ typescript/app/class.ts
@@ -1,5 +1,5 @@
 class Person {
-  constructor(public readonly name: string, private age: number) {
+  constructor(public readonly name: string, protected age: number) {
   }
   incrementAge() {
     this.age += 1
@@ -12,3 +12,14 @@ class Person {
 const xxx = new Person('xxx', 33)
 xxx.incrementAge()
 xxx.greeting()
+
+class Teacher extends Person {
+  constructor(name: string, age: number, public subject: string) {
+    super(name, age)
+  }
+  greeting() {
+    console.log(`hello My name is ${this.name}. I am ${this.age} years old. I teach ${this.subject}`)
+  }
+}
+const teacher = new Teacher('abc', 35, 'math')
+teacher.greeting()
--- typescript/app/dist/class.js
+++ typescript/app/dist/class.js
@@ -13,3 +13,14 @@ class Person {
 const xxx = new Person('xxx', 33);
 xxx.incrementAge();
 xxx.greeting();
+class Teacher extends Person {
+    constructor(name, age, subject) {
+        super(name, age);
+        this.subject = subject;
+    }
+    greeting() {
+        console.log(`hello My name is ${this.name}. I am ${this.age} years old. I teach ${this.subject}`);
+    }
+}
+const teacher = new Teacher('abc', 35, 'math');
+teacher.greeting();
