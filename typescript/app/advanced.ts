7bdeb34ffd5ce20ac47c4b4a2462349044c26ae8
kazunori_moriya <g.ff.k.96@gmail.com>
2022-11-08 10:41:13 +0900

    type guard記法

--- typescript/app/advanced.ts
+++ typescript/app/advanced.ts
@@ -13,3 +13,42 @@ const men: EngineerBloger = {
   role: 'front-end',
   follower: 10
 }
+// type guard typeof演算子
+function toUpperCase(x: string | number) {
+  if (typeof x === 'string') {
+    return x.toUpperCase()
+  }
+  return '';
+}
+// type guard in演算子
+type NomadWorker = Engineer | Bloger;
+function describeProfile(nomadWorker: NomadWorker) {
+  console.log(nomadWorker.name)
+  if ('role' in nomadWorker) {
+    console.log(nomadWorker.role)
+  }
+  if ('follower' in nomadWorker) {
+    console.log(nomadWorker.follower)
+  }
+}
+// type guard instanceof演算子
+class Dog {
+  speak() {
+    console.log('bow-bow')
+  }
+}
+class Bird {
+  speak() {
+    console.log('tweet-tweet')
+  }
+  fly() {
+    console.log('flutter')
+  }
+}
+type Pet = Dog | Bird
+function havePet(pet: Pet) {
+  pet.speak()
+  if (pet instanceof Bird) {
+    pet.fly()
+  }
+}
--- typescript/app/dist/advanced.js
+++ typescript/app/dist/advanced.js
@@ -3,3 +3,39 @@ const men = {
     role: 'front-end',
     follower: 10
 };
+// type guard typeof演算子
+function toUpperCase(x) {
+    if (typeof x === 'string') {
+        return x.toUpperCase();
+    }
+    return '';
+}
+function describeProfile(nomadWorker) {
+    console.log(nomadWorker.name);
+    if ('role' in nomadWorker) {
+        console.log(nomadWorker.role);
+    }
+    if ('follower' in nomadWorker) {
+        console.log(nomadWorker.follower);
+    }
+}
+// type guard instanceof演算子
+class Dog {
+    speak() {
+        console.log('bow-bow');
+    }
+}
+class Bird {
+    speak() {
+        console.log('tweet-tweet');
+    }
+    fly() {
+        console.log('flutter');
+    }
+}
+function havePet(pet) {
+    pet.speak();
+    if (pet instanceof Bird) {
+        pet.fly();
+    }
+}
