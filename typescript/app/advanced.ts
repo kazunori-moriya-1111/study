27d97bd9b0da837aacc22c6d735c50350c4aff1f
kazunori_moriya <g.ff.k.96@gmail.com>
2022-11-21 17:42:49 +0900

    関数のユニオン型の扱い方

--- typescript/app/advanced.ts
+++ typescript/app/advanced.ts
@@ -28,15 +28,24 @@ interface TmpFunc {
   (x: number): number
 }
 const upperHello: TmpFunc = function (x: string | number) { return 0 }
+// interface FuncA {
+//   (a: number, b: string): number;
+//   (a: string, b: number): number;
+// }
+// interface FuncB {
+//   (a: string): number;
+// }
+// let intersectionFunc: FuncA & FuncB;
+// intersectionFunc = function (a: number | string, b?: number | string) { return 0 }
 interface FuncA {
-  (a: number, b: string): number;
-  (a: string, b: number): number;
+  (a: number): number;
 }
 interface FuncB {
-  (a: string): number;
+  (a: string): string;
 }
-let intersectionFunc: FuncA & FuncB;
-intersectionFunc = function (a: number | string, b?: number | string) { return 0 }
+let unionFunc: FuncA | FuncB;
+unionFunc = function (a: string) { return 'hi' }
+
 // type guard in演算子
 type NomadWorker = Engineer | Bloger;
 function describeProfile(nomadWorker: NomadWorker) {
