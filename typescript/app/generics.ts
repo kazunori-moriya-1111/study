e596808d2fc505e2238c1bb63488c7612c45bd09
kazunori_moriya <g.ff.k.96@gmail.com>
2022-12-27 12:16:29 +0900

    クラスにジェネリクスを用いる記法

--- typescript/app/generics.ts
+++ typescript/app/generics.ts
@@ -5,3 +5,22 @@ function copy<T extends { name: string }, U extends keyof T>(value: T, key: U): 
 }
 console.log(copy({ name: 'abc', age: 33 }, 'age'))
 type K = keyof { name: string; age: number }
+
+class LightDatabase<T extends string | number | boolean> {
+  private data: T[] = []
+  add(item: T) {
+    this.data.push(item)
+  }
+  remove(item: T) {
+    this.data.splice(this.data.indexOf(item), 1)
+  }
+  get() {
+    return this.data
+  }
+}
+const stringLightDatabase = new LightDatabase()
+stringLightDatabase.add('Apple')
+stringLightDatabase.add('Banana')
+stringLightDatabase.add('Grape')
+stringLightDatabase.remove('Banana')
+console.log(stringLightDatabase.get())
