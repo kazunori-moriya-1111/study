32f7379fc77eb30f30f50ea2380cc95d8d1a7a7e
kazunori_moriya <g.ff.k.96@gmail.com>
2023-01-09 13:45:35 +0900

    サンプルデータインサートSQL作成

---
+++ br/postgresql/insert_sample_data.sql
@@ -0,0 +1,14 @@
+insert into result values
+('20230103', '戸田', 1, '一般', NULL, '初日', NULL, 3, 6, 1, 4550, 18, 'https://www.boatrace.jp/owpc/pc/race/raceresult?rno=1&jcd=02&hd=20230103'),
+('20230103', '戸田', 2, '一般', NULL, '初日', NULL, 2, 1, 5, 4830, 25, 'https://www.boatrace.jp/owpc/pc/race/raceresult?rno=2&jcd=02&hd=20230103'),
+('20230103', '戸田', 3, '一般', NULL, '初日', NULL, 2, 3, 4, 2220, 3, 'https://www.boatrace.jp/owpc/pc/race/raceresult?rno=3&jcd=02&hd=20230103'),
+('20230103', '戸田', 4, '一般', NULL, '初日', NULL, 1, 3, 2, 2200, 4, 'https://www.boatrace.jp/owpc/pc/race/raceresult?rno=4&jcd=02&hd=20230103'),
+('20230103', '戸田', 5, '一般', NULL, '初日', NULL, 4, 5, 3, 7150, 24, 'https://www.boatrace.jp/owpc/pc/race/raceresult?rno=5&jcd=02&hd=20230103'),
+('20230103', '戸田', 6, '一般', NULL, '初日', NULL, 2, 4, 6, 1840, 1, 'https://www.boatrace.jp/owpc/pc/race/raceresult?rno=6&jcd=02&hd=20230103'),
+('20230103', '戸田', 7, '一般', NULL, '初日', NULL, 1, 2, 3, 440, 1, 'https://www.boatrace.jp/owpc/pc/race/raceresult?rno=7&jcd=02&hd=20230103'),
+('20230103', '戸田', 8, '一般', NULL, '初日', NULL, 2, 1, 3, 1580, 8, 'https://www.boatrace.jp/owpc/pc/race/raceresult?rno=8&jcd=02&hd=20230103'),
+('20230103', '戸田', 9, '一般', NULL, '初日', NULL, 1, 4, 3, 4200, 13, 'https://www.boatrace.jp/owpc/pc/race/raceresult?rno=9&jcd=02&hd=20230103'),
+('20230103', '戸田', 10, '一般', NULL, '初日', NULL, 1, 4, 3, 1930, 6, 'https://www.boatrace.jp/owpc/pc/race/raceresult?rno=10&jcd=02&hd=20230103'),
+('20230103', '戸田', 11, '一般', NULL, '初日', NULL, 1, 2, 6, 960, 2, 'https://www.boatrace.jp/owpc/pc/race/raceresult?rno=11&jcd=02&hd=20230103'),
+('20230103', '戸田', 12, '一般', NULL, '初日', NULL, 1, 5, 4, 1430, 3, 'https://www.boatrace.jp/owpc/pc/race/raceresult?rno=12&jcd=02&hd=20230103')
+;
