d2ee75587c2cf799fc3fc0bc9b41771828886fbc
kazunori_moriya <g.ff.k.96@gmail.com>
2023-01-10 10:50:57 +0900

    docker環境のpostgresqlへ接続

--- br/test-app/src/connect_db.py
+++ br/test-app/src/connect_db.py
@@ -1,9 +1,19 @@
 import psycopg2
 
 connector =  psycopg2.connect('postgresql://{user}:{password}@{host}:{port}/{dbname}'.format( 
-                user="postgres",        #ユーザ
+                user="postgres",      #ユーザ
                 password="passw0rd",  #パスワード
-                host="postgres_br",       #ホスト名
-                port="5432",            #ポート
-                dbname="postgres"))    #データベース名
+                host="postgresql_br", #ホスト名
+                port="5432",          #ポート
+                dbname="postgres"))   #データベース名
 print(connector)
+
+# SQL実行
+cursor = connector.cursor()
+cursor.execute('SELECT * FROM result')
+rows = cursor.fetchall()
+cursor.close()
+connector.close()
+
+# SQL結果参照
+print(rows)
