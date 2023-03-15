458089d332ff1077f6ae0d137a5a10927b8091b0
kazunori_moriya <g.ff.k.96@gmail.com>
2023-03-15 12:09:40 +0900

    アロー関数の使い方

--- nodejs/app/01_js_basic/020_arrow_function/start/main.js
+++ nodejs/app/01_js_basic/020_arrow_function/start/main.js
@@ -1,3 +1,12 @@
 function fn(number) {
   return number * 2;
 }
+console.log(fn(3))
+
+const fnAroow = number => {
+  console.log(number)
+  return number * 2
+}
+
+const fnArrowObj = number => ({ result: number * 2 })
+console.log(fnArrowObj(3))
