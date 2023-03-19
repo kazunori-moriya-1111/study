94e68281443af499ab244b169ee7405b5bdf86d6
kazunori_moriya <g.ff.k.96@gmail.com>
2023-03-19 19:57:12 +0900

    スプレッド演算子の記述

--- nodejs/app/01_js_basic/060_spread_rest/start/main.js
+++ nodejs/app/01_js_basic/060_spread_rest/start/main.js
@@ -1,20 +1,24 @@
 const nums = [3, 1, 4, 1, 5, 10, 2, 6];
 
-const result = Math.max(3, 1, 4, 1, 5, 10, 2, 6);
+const result = Math.max(...nums);
 console.log(result);
 
 
 let arr1 = [1, 2, 3];
 let arr2 = [4, 5, 6];
-let newArr = [...arr1];
+let newArr = [...arr1, ...arr2];
 
-console.log(newArr2);
+console.log(newArr);
 
 const obj = {
   name: "Tom",
   age: 22,
 };
 const newObj = { ...obj };
+
+console.log(newObj);
+
+newObj.name = 'Jon'
 
 console.log(newObj);
 
