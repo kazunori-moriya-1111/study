af29e62c14c67d203d01a20d6f5d1b99488f431d
kazunori_moriya <g.ff.k.96@gmail.com>
2023-03-22 13:24:42 +0900

    promiseの使い方

---
+++ nodejs/app/01_js_basic/090_promise/start/main.js
@@ -0,0 +1,13 @@
+let a = 0
+console.log(a)
+
+new Promise((resolve, reject) => {
+  setTimeout(() => {
+    a = 1
+    reject(a)
+  }, 2000)
+}).then((b) => {
+  console.log(b)
+}).catch((c) => {
+  console.log('catchが実行', c)
+})
