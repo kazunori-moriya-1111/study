23d256a0230a520a9e55b48afcd2fdba9c41daa1
kazunori_moriya <g.ff.k.96@gmail.com>
2023-03-28 11:28:19 +0900

    commonJSのモジュール管理

--- nodejs/app/02_node_basic/040_module_system/start/example.js
+++ nodejs/app/02_node_basic/040_module_system/start/example.js
@@ -1,5 +1,7 @@
-function plus(a, b) {
-  return a + b;
-}
-const result = plus(1, 2);
+const calc = require('./calc')
+// const { plus, minus } = require('./calc') でもいける
+console.log(calc)
+const result = calc.plus(1, 2);
 console.log(result);
+const result1 = calc.minus(1, 2)
+console.log(result1)
