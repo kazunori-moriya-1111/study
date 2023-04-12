1f16a7b9b272c04961e4a4c029e07f65b3b7a6c2
kazunori_moriya <g.ff.k.96@gmail.com>
2023-04-12 16:52:28 +0900

    playwrightを使ってみる

--- nodejs/app/04_automation/010_playwright/030_what_is_playwright/start/Example.mjs
+++ nodejs/app/04_automation/010_playwright/030_what_is_playwright/start/Example.mjs
@@ -1,2 +1,12 @@
 import { chromium } from "@playwright/test";
 
+(async () => {
+  const browser = await chromium.launch()
+  const page = await browser.newPage()
+  await page.goto("http://localhost:3000")
+  const htmlStr = await page.content()
+
+  console.log(htmlStr)
+
+  await browser.close()
+})()
