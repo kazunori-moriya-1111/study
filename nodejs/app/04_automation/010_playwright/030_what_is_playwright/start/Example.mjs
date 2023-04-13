0cefd32707d2ea04a28d73a9939ede0833013b82
kazunori_moriya <g.ff.k.96@gmail.com>
2023-04-13 09:47:40 +0900

    locatorの使い方

--- nodejs/app/04_automation/010_playwright/030_what_is_playwright/start/Example.mjs
+++ nodejs/app/04_automation/010_playwright/030_what_is_playwright/start/Example.mjs
@@ -4,9 +4,19 @@ import { chromium } from "@playwright/test";
   const browser = await chromium.launch()
   const page = await browser.newPage()
   await page.goto("http://localhost:3000")
-  const htmlStr = await page.content()
+  // CSSセレクターで要素を取得
+  const pageTitleLocator = await page.locator('.navbar-brand')
+  const pageTitle = await pageTitleLocator.innerText()
+  // console.log(pageTitle)
 
-  console.log(htmlStr)
+  //文字列で要素を取得
+  const textLocator = await page.locator('text=名刺管理アプリ')
+  const pageText = await textLocator.innerText()
+  // console.log(pageText)
 
+  //xpathで要素を取得
+  const xpathLocator = await page.locator('xpath=//*[@id="__next"]/nav/div/a')
+  const xpathText = await xpathLocator.innerText()
+  console.log(xpathText)
   await browser.close()
 })()
