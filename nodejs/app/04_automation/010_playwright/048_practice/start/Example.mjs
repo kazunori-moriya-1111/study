c1cb5d214794283666945020d3924e3518763242
kazunori_moriya <g.ff.k.96@gmail.com>
2023-04-17 10:52:58 +0900

    playwriteの本格的な実装

--- nodejs/app/04_automation/010_playwright/048_practice/start/Example.mjs
+++ nodejs/app/04_automation/010_playwright/048_practice/start/Example.mjs
@@ -5,10 +5,22 @@ import { chromium } from "@playwright/test";
  * [佐藤]で検索して、一番最後に出てくる人物の名前を取得してください。
  */
 (async () => {
-  const browser = await chromium.launch({ headless: false, slowMo: 500 });
+  const browser = await chromium.launch();
   const page = await browser.newPage();
   await page.goto("http://localhost:3000");
 
+  const inputLocator = page.locator('//*[@id="__next"]/div/div[1]/label/input')
+  await inputLocator.type('佐藤')
+
+  const pagerLocator = page.locator('.page-link.page-number')
+  if (await pagerLocator.count() > 1) {
+    const lastpagerLocator = pagerLocator.locator('nth=-1')
+    await lastpagerLocator.click()
+  }
+
+  const lastCardLocator = page.locator('.cards.list-group-item >> nth=-1')
+  const lastCardName = await lastCardLocator.textContent()
+  console.log(lastCardName)
   await browser.close();
 
 })();
