bb8dd1df89eda4f75a80899a2f8ef4b5da9b8c06
kazunori_moriya <g.ff.k.96@gmail.com>
2023-04-21 21:31:22 +0900

    例外処理の記述

--- nodejs/app/04_automation/010_playwright/070_error_handling/start/Example.mjs
+++ nodejs/app/04_automation/010_playwright/070_error_handling/start/Example.mjs
@@ -1,14 +1,18 @@
 import { chromium } from "@playwright/test";
 
 (async () => {
-  const browser = await chromium.launch({ headless: false, slowMo: 500 });
+  const browser = await chromium.launch();
   const page = await browser.newPage();
   await page.goto("http://localhost:3000");
 
   await page.waitForTimeout(2000);
 
-  const inputLocator = page.locator("//*[@id=\"__next\"]/div/div[1]/label/input");
-  await inputLocator.type('美');
+  try {
+    const inputLocator = page.locator("//*[@id=\"__next\"]/div[1]/label/input");
+    await inputLocator.type('美', { timeout: 1000 });
+  } catch (e) {
+    console.error('インプットの入力処理で例外が発生しました', e)
+  }
 
   const pager3Locator = page.locator(".page-link.page-number >> nth=-1");
   await pager3Locator.click();
@@ -18,6 +22,6 @@ import { chromium } from "@playwright/test";
   const cardCount = await cardLocator.count();
   console.log(cardCount);
 
-  // await browser.close();
+  await browser.close();
 
 })();
