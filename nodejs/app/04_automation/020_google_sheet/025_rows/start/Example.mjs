38c62192b0afd0a2c055ff8579c424590680fb39
kazunori_moriya <g.ff.k.96@gmail.com>
2023-04-27 09:32:58 +0900

    行の追加

--- nodejs/app/04_automation/020_google_sheet/025_rows/start/Example.mjs
+++ nodejs/app/04_automation/020_google_sheet/025_rows/start/Example.mjs
@@ -1,9 +1,9 @@
 import { GoogleSpreadsheet } from "google-spreadsheet";
 import env from 'dotenv';
-env.config();
+env.config({ path: '../../../.env' });
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
-const secrets = require('../../../google_secrets.json');
+const secrets = require('../../udemy-spread-sheet-f5eada302ada.json');
 
 (async () => {
     const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
@@ -12,7 +12,29 @@ const secrets = require('../../../google_secrets.json');
         client_email: secrets.client_email,
         private_key: secrets.private_key
     });
-    
+
     await doc.loadInfo();
-    
+
+    // await doc.addSheet({ title: 'persons', headerValues: ['name', 'age', 'gender'] })
+
+    const personSheet = doc.sheetsByTitle['persons'];
+    const rows = await personSheet.addRows([
+        {
+            name: 'Tom',
+            age: 18,
+            gender: 'M'
+        },
+        {
+            name: 'Hanako',
+            age: 20,
+            gender: 'F'
+        },
+        {
+            name: 'John',
+            age: 25,
+            gender: 'M'
+        }
+    ]);
+
+    rows.forEach(row => async () => await row.save())
 })();
