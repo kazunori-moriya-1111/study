7657b8e942633d0054e7b3df842f0014b428a58f
kazunori_moriya <g.ff.k.96@gmail.com>
2023-04-30 13:52:05 +0900

    webpackを用いたesモジュールのブラウザ利用

--- nodejs/app/05_frontend/010_browser_module/start/main.js
+++ nodejs/app/05_frontend/010_browser_module/start/main.js
@@ -1,3 +1,7 @@
+import isOdd from "is-odd";
 import { helloFromSub } from "./sub.js";
+
 // const myModule = require('./sub.js')
-helloFromSub()
+helloFromSub();
+const oddy = isOdd(3);
+console.log(oddy);
