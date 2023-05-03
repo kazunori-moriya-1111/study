d21a83fabdb20f9d6de9b20c5b3f92188697d0a4
kazunori_moriya <g.ff.k.96@gmail.com>
2023-05-03 14:06:28 +0900

    redirect処理実装

--- nodejs/app/06_backend/050_http_status/start/app.mjs
+++ nodejs/app/06_backend/050_http_status/start/app.mjs
@@ -1,13 +1,15 @@
 import * as http from "http";
 
-const server = http.createServer(function(req, res) {
-    console.log(req.url);
-    res.writeHead(200, {'content-type': 'text/html; charset=UTF-8'});
-    if(req.url === '/hello') {
-        res.end(`<h1>こんにちは</h1>`)
-    } else if(req.url === '/bye') {
-        res.end('bye');
-    }
+const server = http.createServer(function (req, res) {
+  console.log(req.url);
+  if (req.url === "/hello") {
+    res.writeHead(302, { location: "/redirected" });
+    res.end(`<h1>こんにちは</h1>`);
+  } else if (req.url === "/bye") {
+    res.end("bye");
+  } else {
+    res.end(req.url);
+  }
 });
 
 server.listen(8080);
