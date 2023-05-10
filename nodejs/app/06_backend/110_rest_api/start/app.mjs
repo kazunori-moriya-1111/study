54a355aee0e45941b6cf755eab41605454d96aa5
kazunori_moriya <g.ff.k.96@gmail.com>
2023-05-10 20:28:05 +0900

    API作成

--- nodejs/app/06_backend/110_rest_api/start/app.mjs
+++ nodejs/app/06_backend/110_rest_api/start/app.mjs
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from "express";
 
 const PORT = 8080;
 const app = express();
@@ -11,8 +11,36 @@ const products = [
   { name: "clock", price: 700 },
 ];
 
-app.get('/products', function (req, res) {
+app.get("/products", function (req, res) {
   res.json(products);
+});
+
+app.post("/create-product", function (req, res) {
+  const newProduct = req.body;
+  console.log(newProduct);
+  products.push(newProduct);
+  console.log(products);
+  res.json(newProduct);
+});
+
+app.post("/delete-product", function (req, res) {
+  const deleteId = req.body.id;
+  products.splice(deleteId, 1);
+  console.log(products);
+  res.json({ deleteId });
+});
+
+app.post("/update-product", function (req, res) {
+  const updateId = req.body.id;
+  const targetProduct = products[updateId];
+  if (req.body.hasOwnProperty("price")) {
+    targetProduct.price = req.body.price;
+  }
+  if (req.body.hasOwnProperty("name")) {
+    targetProduct.price = req.body.name;
+  }
+  console.log(products);
+  res.json(targetProduct);
 });
 
 app.listen(PORT, function () {
