936707109193b619a50dfa3baeddd370b2f96776
kazunori_moriya <g.ff.k.96@gmail.com>
2023-05-14 09:09:55 +0900

    mongoDBデータ検索

--- nodejs/app/07_mongodb/020_find/start/Example.mjs
+++ nodejs/app/07_mongodb/020_find/start/Example.mjs
@@ -1,16 +1,20 @@
-import env from 'dotenv';
-env.config();
+import env from "dotenv";
+env.config({ path: "../../.env" });
 
-import { MongoClient, ServerApiVersion, ObjectId } from 'mongodb';
-const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+import { MongoClient, ServerApiVersion, ObjectId } from "mongodb";
+const client = new MongoClient(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function getCollection() {
   try {
     await client.connect();
-    const db = client.db('bookshelf');
-    return db.collection('books');
+    const db = client.db("bookshelf");
+    return db.collection("books");
   } catch {
-    // await client.close();
+    await client.close();
   }
 }
 // クエリ
@@ -20,10 +24,17 @@ async function getCollection() {
 getAllBooks();
 async function getAllBooks() {
   const col = await getCollection();
-  debugger;
-  let cursor = col.find();
+  // let cursor = col.find({
+  //   $or: [{ title: "バックエンド開発" }, { rating: 3 }],
+  // });
+  // let cursor = col.find({
+  //   title: { $in: ["ごんぎつね4", "バックエンド開発"] },
+  // });
+  // let cursor = col.find({ rating: { $gte: 2, $lte: 4 } }).sort({ rating: -1 });
+  // let cursor = col.find({ _id: new ObjectId("6334634881d8ae4ba04fbc8e") });
+  let cursor = col.find({ title: { $regex: /^潮/ } });
   const result = await cursor.toArray();
   console.log(result);
 
-//   await client.close();
+  await client.close();
 }
