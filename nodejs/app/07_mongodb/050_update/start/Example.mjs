e150e51fe8c21deb45a9243b8c9df8fbbee67fa8
kazunori_moriya <g.ff.k.96@gmail.com>
2023-05-16 19:17:46 +0900

    mongoDBの更新

--- nodejs/app/07_mongodb/050_update/start/Example.mjs
+++ nodejs/app/07_mongodb/050_update/start/Example.mjs
@@ -1,14 +1,18 @@
-import env from 'dotenv';
-env.config();
+import env from "dotenv";
+env.config({ path: "../../.env" });
 
-import { MongoClient, ServerApiVersion, ObjectId } from 'mongodb';
-const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+import { MongoClient, ServerApiVersion, ObjectId } from "mongodb";
+const client = new MongoClient(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function getCollection() {
   try {
     await client.connect();
-    const db = client.db('bookshelf');
-    return db.collection('books');
+    const db = client.db("bookshelf");
+    return db.collection("books");
   } catch {
     await client.close();
   }
@@ -17,6 +21,7 @@ async function getCollection() {
 updateBook();
 async function updateBook() {
   const col = await getCollection();
-
+  await col.updateOne({ title: "バックエンド開発" }, { $set: { rating: 3 } });
+  await col.updateMany({ description: "三島由紀夫" }, { $set: { rating: 3 } });
   await client.close();
 }
