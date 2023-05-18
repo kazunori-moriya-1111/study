87b42c0717bf187420a2e942171d61caa6122271
kazunori_moriya <g.ff.k.96@gmail.com>
2023-05-19 00:20:04 +0900

    スキーマ定義のオプション

--- nodejs/app/07_mongodb/070_model_schema/start/Example.mjs
+++ nodejs/app/07_mongodb/070_model_schema/start/Example.mjs
@@ -4,15 +4,26 @@ env.config({ path: "../../.env" });
 
 connect(process.env.MONGO_URI);
 const catSchema = new Schema({
-  name: String,
-  size: Number,
-  bool: Boolean,
-  dt: Date,
-  arry: [],
+  name: { type: String, require: true },
+  size: { type: Number, require: true, enum: [0, 1] },
+  bool: { type: Boolean, default: false },
+  dt: {
+    type: Date,
+    set: function (newVal) {
+      return new Date(newVal);
+    },
+    get: function (val) {
+      return val instanceof Date ? val : new Date(val);
+    },
+  },
+  arry: [String],
   anything: Mixed,
 });
 const Cat = model("Cat", catSchema);
 
 const kitty = new Cat();
 kitty.name = "Zildjian";
-kitty.save().then(() => console.log("meow"));
+kitty.size = 10;
+kitty.dt = "2017/12/21";
+kitty.arry = [0, 1];
+kitty.save().then((doc) => console.log(doc.dt));
