2f126517d0eb7e3fdf98184a73970fc39ab09f6f
kazunori_moriya <g.ff.k.96@gmail.com>
2023-05-19 17:57:32 +0900

    bookshelfのスキーマ定義

--- nodejs/app/07_mongodb/080_model_bookshelf/start/Example.mjs
+++ nodejs/app/07_mongodb/080_model_bookshelf/start/Example.mjs
@@ -1,6 +1,6 @@
-import { connect, Schema, model, Mixed } from 'mongoose';
-import env from 'dotenv';
-env.config();
+import mongoose, { connect, Schema, model } from "mongoose";
+import env from "dotenv";
+env.config({ path: "../../.env" });
 
 /**
 String: 文字列
@@ -17,28 +17,62 @@ Schema: 他のスキーマ
  */
 connect(process.env.MONGO_URI);
 
-const catSchema = new Schema({
-  name: { type: String, required: true },
-  size: { type: Number, required: true, enum: [0,1] },
-  bool: { type: Boolean, default: false, alias: 'b' },
-  dt: {
-    type: Date,
-    set: function (newVal) {
-      return new Date(newVal);
+// const catSchema = new Schema(
+//   {
+//     name: { type: String, required: true },
+//     size: { type: Number, required: true, enum: [0, 1] },
+//     bool: { type: Boolean, default: false, alias: "b" },
+//     dt: {
+//       type: Date,
+//       set: function (newVal) {
+//         return new Date(newVal);
+//       },
+//       get: function (val) {
+//         return val instanceof Date ? val : new Date(val);
+//       },
+//     },
+//     arry: [String],
+//     anything: Mixed,
+//   },
+//   { timestamps: true }
+// );
+const bookSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
     },
-    get: function(val) {
-        return val instanceof Date ? val : new Date(val)
-    }
+    description: {
+      type: String,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      enum: [1, 2, 3, 4, 5],
+      default: 3,
+    },
   },
-  arry: [ String ],
-  anything: Mixed,
+  { timestamps: true }
+);
+const Book = model("Book", bookSchema);
+
+const kitty = new Book({
+  title: "てすとぶっく",
+  description: "これは説明卵",
+  comment: "素晴らしい",
+  rating: 4,
 });
-const Cat = model('Cat', catSchema);
+// kitty.name = "Zildjian";
+// kitty.size = 1;
+// kitty.arry = [0, 1];
+// kitty.dt = "2017/12/21";
 
-const kitty = new Cat();
-kitty.name = 'Zildjian';
-kitty.size = 1;
-kitty.arry = [0,1];
-kitty.dt = "2017/12/21";
-
-kitty.save().then((doc) => console.log(doc.b));
+kitty.save().then((book) => {
+  console.log(book._id);
+  mongoose.connection.close();
+});
