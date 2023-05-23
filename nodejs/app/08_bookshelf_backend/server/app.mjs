2c8bce4fec7787e06262c65af9c8f478667dd8c5
kazunori_moriya <g.ff.k.96@gmail.com>
2023-05-23 21:49:18 +0900

    フォーマット

--- nodejs/app/08_bookshelf_backend/server/app.mjs
+++ nodejs/app/08_bookshelf_backend/server/app.mjs
@@ -1,6 +1,6 @@
 import express from "express";
 import env from "dotenv";
-env.config();
+env.config({ path: "../../.env" });
 
 import apiRoutes from "./api-routes/index.mjs";
 import "./helpers/db.mjs";
@@ -11,19 +11,19 @@ const port = process.env.PORT || 8080;
 app.use(express.json());
 
 // API
-app.use('/api', apiRoutes);
+app.use("/api", apiRoutes);
 
-app.use(function(req, res) {
-    res.status(404).json({ msg: "Page Not Found" });
+app.use(function (req, res) {
+  res.status(404).json({ msg: "Page Not Found" });
 });
 
-app.use(function(err, req, res, next) {
-    if(res.headersSent) {
-        return next(err);
-    }
-    res.status(500).json({ msg: '不正なエラーが発生しました。'});
+app.use(function (err, req, res, next) {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500).json({ msg: "不正なエラーが発生しました。" });
 });
 
 app.listen(port, () => {
-    console.log(`Server Start: http://localhost:${port}`);
+  console.log(`Server Start: http://localhost:${port}`);
 });
