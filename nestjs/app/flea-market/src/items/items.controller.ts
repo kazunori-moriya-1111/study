2be3f8b78c56f82a4dd53767ae9e0aaac5392453
kazunori_moriya <g.ff.k.96@gmail.com>
2023-05-25 12:02:37 +0900

    readメソッド実装

--- nestjs/app/flea-market/src/items/items.controller.ts
+++ nestjs/app/flea-market/src/items/items.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ItemsService } from './items.service';
 import { Item } from './items.model';
 import { ItemStatus } from './item-status.enum';
@@ -7,9 +7,15 @@ import { ItemStatus } from './item-status.enum';
 export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
   @Get()
-  findAll() {
+  findAll(): Item[] {
     return this.itemsService.findAll();
   }
+
+  @Get(':id')
+  findById(@Param('id') id: string): Item {
+    return this.itemsService.findById(id);
+  }
+
   @Post()
   create(
     @Body('id') id: string,
--- nestjs/app/flea-market/src/items/items.service.ts
+++ nestjs/app/flea-market/src/items/items.service.ts
@@ -4,9 +4,14 @@ import { Item } from './items.model';
 @Injectable()
 export class ItemsService {
   private items: Item[] = [];
-  findAll() {
-    return 'This is ItemsService';
+  findAll(): Item[] {
+    return this.items;
   }
+
+  findById(id: string): Item {
+    return this.items.find((item) => item.id === id);
+  }
+
   create(item: Item): Item {
     this.items.push(item);
     return item;
