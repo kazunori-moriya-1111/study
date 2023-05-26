bd5128ce6c3b864d49ccb7e31c8b52263bdf7991
kazunori_moriya <g.ff.k.96@gmail.com>
2023-05-27 02:31:19 +0900

    typeORMでのPostgreSQL接続設定

--- nestjs/app/flea-market/src/app.module.ts
+++ nestjs/app/flea-market/src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ItemsModule } from './items/items.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ItemsModule],
+  imports: [
+    ItemsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgresql_nest',
+      port: 5432,
+      username: 'postgres',
+      password: 'passw0rd',
+      database: 'postgres',
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
