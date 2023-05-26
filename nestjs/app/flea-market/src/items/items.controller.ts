3c18737fb64528bc55e6d9f8f9f21b7ade05c755
kazunori_moriya <g.ff.k.96@gmail.com>
2023-05-27 01:48:27 +0900

    DTOを使用したリファクタリング

--- nestjs/app/flea-market/src/items/items.controller.ts
+++ nestjs/app/flea-market/src/items/items.controller.ts
@@ -9,7 +9,7 @@ import {
 } from '@nestjs/common';
 import { ItemsService } from './items.service';
 import { Item } from './items.model';
-import { ItemStatus } from './item-status.enum';
+import { CreateItemDto } from './dto/create-item.dto';
 
 @Controller('items')
 export class ItemsController {
@@ -25,20 +25,8 @@ export class ItemsController {
   }
 
   @Post()
-  create(
-    @Body('id') id: string,
-    @Body('name') name: string,
-    @Body('price') price: number,
-    @Body('description') description: string,
-  ): Item {
-    const item: Item = {
-      id,
-      name,
-      price,
-      description,
-      status: ItemStatus.ON_SALE,
-    };
-    return this.itemsService.create(item);
+  create(@Body() CreateItemDto: CreateItemDto): Item {
+    return this.itemsService.create(CreateItemDto);
   }
 
   @Patch(':id')
