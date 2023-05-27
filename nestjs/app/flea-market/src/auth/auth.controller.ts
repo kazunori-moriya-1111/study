d0943a5347638dcda85c9f8b89e087a115bb874f
kazunori_moriya <g.ff.k.96@gmail.com>
2023-05-27 18:01:31 +0900

    User作成機能実装

--- nestjs/app/flea-market/src/auth/auth.controller.ts
+++ nestjs/app/flea-market/src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from '../entities/user.entity';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  async signup(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return await this.authService.singUp(createUserDto);
+  }
+}
--- nestjs/app/flea-market/src/auth/auth.service.ts
+++ nestjs/app/flea-market/src/auth/auth.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../entities/user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async singUp(createUserDto: CreateUserDto): Promise<User> {
+    const user: User = {
+      id: uuid(),
+      ...createUserDto,
+    };
+    return await this.userRepository.save(user);
+  }
+}
