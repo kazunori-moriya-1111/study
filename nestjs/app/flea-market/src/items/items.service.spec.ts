fa62bfb0612a504e174ec82cd33c180272bf7f4d
kazunori_moriya <g.ff.k.96@gmail.com>
2023-05-31 20:53:48 +0900

    testモジュール実装

---
+++ nestjs/app/flea-market/src/items/items.service.spec.ts
@@ -0,0 +1,25 @@
+import { Test } from '@nestjs/testing';
+import { ItemsService } from './items.service';
+import { Repository } from 'typeorm';
+import { Item } from 'src/entities/item.entity';
+
+const mockItemRepository = () => ({});
+
+describe('ItemsServiceTest', () => {
+  let itemsService;
+  let itemRepository;
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        ItemsService,
+        {
+          provide: Repository<Item>,
+          useFactory: mockItemRepository,
+        },
+      ],
+    }).compile();
+
+    itemsService = module.get<ItemsService>(ItemsService);
+    itemRepository = module.get<Repository<Item>>(Repository<Item>);
+  });
+});
