e8f60dc1ea9d1355d5a852a5bfd7dff64a07421a
kazunori_moriya <g.ff.k.96@gmail.com>
2023-06-05 16:56:32 +0900

    実装に必要なモジュールをインポート

--- nestjs-nextjs/nestjs/api-lesson/src/auth/auth.service.ts
+++ nestjs-nextjs/nestjs/api-lesson/src/auth/auth.service.ts
@@ -1,4 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ForbiddenException } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import * as bctypt from 'bcrypt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto/auth.dto';
+import { Msg, Jwt } from './interfaces/auth.interface';
 
 @Injectable()
 export class AuthService {}
