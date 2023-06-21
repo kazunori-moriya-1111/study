f6ed24fd698529b7f4bcf0a2affd4989280a4e2c
kazunori_moriya <g.ff.k.96@gmail.com>
2023-06-21 22:44:57 +0900

    スキーマ定義を拡張

--- graphql/server/src/server.js
+++ graphql/server/src/server.js
@@ -1,14 +1,29 @@
 const { ApolloServer, gql } = require('apollo-server');
 
+let links = [
+  {
+    id: 'link-0',
+    description: 'GraphQLを学ぶ',
+    url: 'abc.com',
+  },
+];
 const typeDefs = gql`
   type Query {
     info: String!
+    feed: [Link]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `;
 
 const resolvers = {
   Query: {
     info: () => 'HackerNewsクローン',
+    fedd: () => links,
   },
 };
 
