f1b5f821c1e21301865ecbee6b3c8e287f847db5
kazunori_moriya <g.ff.k.96@gmail.com>
2023-07-21 22:31:58 +0900

    共通のレイアウトコンポーネントを作成

---
+++ nextjs/app/nextjs-microblog/components/Layout.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+
+const name = 'Cafe au lait';
+export const siteTitle = 'Next.js blog';
+function Layout({ children }) {
+  return (
+    <div>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header>
+        <img src="/images/profile.png" />
+        <h1>{name}</h1>
+      </header>
+      <main>{children}</main>
+    </div>
+  );
+}
+export default Layout;
