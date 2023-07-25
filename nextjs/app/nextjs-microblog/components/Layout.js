4c0df21f118f5093f3c972bbaa0151dd383b269a
kazunori_moriya <g.ff.k.96@gmail.com>
2023-07-25 23:57:39 +0900

    css適用

--- nextjs/app/nextjs-microblog/components/Layout.js
+++ nextjs/app/nextjs-microblog/components/Layout.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from './layout.module.css';
+import utilStyles from '../styles/utils.module.css';
 
 const name = 'Cafe au lait';
 export const siteTitle = 'Next.js blog';
@@ -10,8 +11,8 @@ function Layout({ children }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header className={styles.header}>
-        <img src="/images/profile.png" />
-        <h1>{name}</h1>
+        <img src="/images/profile.png" className={utilStyles.borderCircle} />
+        <h1 className={utilStyles.heading2Xl}>{name}</h1>
       </header>
       <main>{children}</main>
     </div>
