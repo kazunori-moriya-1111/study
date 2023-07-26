08342c8100aa611227e610e7cace6cbf13651f01
kazunori_moriya <g.ff.k.96@gmail.com>
2023-07-27 00:32:29 +0900

    記事をトップページへ追加

--- nextjs/app/nextjs-microblog/pages/index.js
+++ nextjs/app/nextjs-microblog/pages/index.js
@@ -1,5 +1,73 @@
+import styles from '../styles/Home.module.css';
 import Layout from '../components/Layout';
 
+import Link from 'next/link';
+import utilStyles from '../styles/utils.module.css';
+
 export default function Home() {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <section className={utilStyles.headingMd}>
+        <p>ここにプロフィールを記載する</p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2>エンジニアのブログ</h2>
+        <div className={styles.grid}>
+          <article>
+            <Link href="/">
+              <img
+                src="/images/thumbnail01.jpg"
+                className={styles.thumbnailImage}
+              />
+            </Link>
+            <Link href="/">
+              <div className={utilStyles.boldText}>記事のタイトル</div>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>July 28, 2023</small>
+          </article>
+          <article>
+            <Link href="/">
+              <img
+                src="/images/thumbnail01.jpg"
+                className={styles.thumbnailImage}
+              />
+            </Link>
+            <Link href="/">
+              <div className={utilStyles.boldText}>記事のタイトル</div>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>July 28, 2023</small>
+          </article>
+          <article>
+            <Link href="/">
+              <img
+                src="/images/thumbnail01.jpg"
+                className={styles.thumbnailImage}
+              />
+            </Link>
+            <Link href="/">
+              <div className={utilStyles.boldText}>記事のタイトル</div>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>July 28, 2023</small>
+          </article>
+          <article>
+            <Link href="/">
+              <img
+                src="/images/thumbnail01.jpg"
+                className={styles.thumbnailImage}
+              />
+            </Link>
+            <Link href="/">
+              <div className={utilStyles.boldText}>記事のタイトル</div>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>July 28, 2023</small>
+          </article>
+        </div>
+      </section>
+    </Layout>
+  );
 }
