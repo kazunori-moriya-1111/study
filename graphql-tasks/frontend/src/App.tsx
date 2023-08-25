f27d703ff01e01211071c872e6ac4efba8c5ab2d
kazunori_moriya <g.ff.k.96@gmail.com>
2023-08-25 13:31:49 +0900

    ページ遷移設定

--- graphql-tasks/frontend/src/App.tsx
+++ graphql-tasks/frontend/src/App.tsx
@@ -3,17 +3,20 @@ import Main from './compunents/Main'
 import NotFound from './compunents/NotFound'
 import SignIn from './compunents/SignIn'
 import SignUp from './compunents/SignUp'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   
 
   return (
-    <div className='App'>
-      <SignIn />
-      <SignUp />
-      <Main />
-      <NotFound />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signin" element={<SignIn />}/>
+        <Route path="/signup" element={<SignUp />}/>
+        <Route path="/" element={<Main />}/>
+        <Route path="*" element={<NotFound />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
