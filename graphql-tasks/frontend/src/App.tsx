57a74209619a2ed03b2434098d0d2ceb69705e88
kazunori_moriya <g.ff.k.96@gmail.com>
2023-08-25 16:30:44 +0900

    apolloクライアントセットアップ

--- graphql-tasks/frontend/src/App.tsx
+++ graphql-tasks/frontend/src/App.tsx
@@ -5,17 +5,27 @@ import NotFound from './compunents/NotFound';
 import SignIn from './compunents/SignIn';
 import SignUp from './compunents/SignUp';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import client from './apolloClient';
+import { ApolloProvider } from '@apollo/client';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/signin" element={<GuestRoute children={<SignIn />} />} />
-        <Route path="/signup" element={<GuestRoute children={<SignUp />} />} />
-        <Route path="/" element={<PrivateRoute children={<Main />} />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/signin"
+            element={<GuestRoute children={<SignIn />} />}
+          />
+          <Route
+            path="/signup"
+            element={<GuestRoute children={<SignUp />} />}
+          />
+          <Route path="/" element={<PrivateRoute children={<Main />} />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ApolloProvider>
   );
 }
 
