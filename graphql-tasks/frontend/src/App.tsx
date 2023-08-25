e84ad11dae53389c850015261d2093e25e7062e8
kazunori_moriya <g.ff.k.96@gmail.com>
2023-08-25 13:50:51 +0900

    prettierによる整形

--- graphql-tasks/frontend/src/App.tsx
+++ graphql-tasks/frontend/src/App.tsx
@@ -1,23 +1,21 @@
-import './App.css'
-import Main from './compunents/Main'
-import NotFound from './compunents/NotFound'
-import SignIn from './compunents/SignIn'
-import SignUp from './compunents/SignUp'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css';
+import Main from './compunents/Main';
+import NotFound from './compunents/NotFound';
+import SignIn from './compunents/SignIn';
+import SignUp from './compunents/SignUp';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-  
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/signin" element={<SignIn />}/>
-        <Route path="/signup" element={<SignUp />}/>
-        <Route path="/" element={<Main />}/>
-        <Route path="*" element={<NotFound />}/>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={<Main />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
