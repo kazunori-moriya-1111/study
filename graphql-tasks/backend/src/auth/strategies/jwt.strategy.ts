5774d9a577c8d0fa36dd4e1650b6120973a73257
kazunori_moriya <g.ff.k.96@gmail.com>
2023-08-25 11:15:06 +0900

    jwt認証実装

---
+++ graphql-tasks/backend/src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UserService } from 'src/user/user.service';
+import { JwtPlayLoad } from '../types/jwt.payload';
+import { User } from '@prisma/client';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly userService: UserService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: JwtPlayLoad): Promise<User | null> {
+    return await this.userService.getUser(payload.email);
+  }
+}
