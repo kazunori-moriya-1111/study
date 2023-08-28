efb9e4f50de271a70485eabb3faac80a5509bc49
kazunori_moriya <g.ff.k.96@gmail.com>
2023-08-28 15:29:19 +0900

    ログアウト機能実装

--- graphql-tasks/frontend/src/compunents/Main.tsx
+++ graphql-tasks/frontend/src/compunents/Main.tsx
@@ -1,6 +1,11 @@
+import Header from './Header';
+
 const Main = () => {
   return (
-    <div>Main</div>
-  )
-}
-export default Main
+    <>
+      <Header />
+      <div>Main</div>
+    </>
+  );
+};
+export default Main;
