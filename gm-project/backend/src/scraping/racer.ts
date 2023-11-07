2295278fd50efc590fc2695a20fc6a5cf340a40b
kazunori_moriya <g.ff.k.96@gmail.com>
2023-11-07 15:51:26 +0900

    スクレイピング処理実装

--- gm-project/backend/src/scraping/racer.ts
+++ gm-project/backend/src/scraping/racer.ts
@@ -1,29 +1,23 @@
-import { Browser, Page, webkit } from 'playwright';
+import { JSDOM } from 'jsdom';
 
 export default class getRacerInfo {
   async scraping(registrationNumber: number): Promise<Object> {
-    // ブラウザの起動
-    const browser: Browser = await webkit.launch({ headless: true });
+    const url = `https://www.boatrace.jp/owpc/pc/data/racersearch/profile?toban=${registrationNumber}`;
 
-    // 新しいページを開く
-    const page: Page = await browser.newPage();
+    // dom取得
+    const dom = await JSDOM.fromURL(url);
+    const document = dom.window.document;
 
-    // 特定のサイトに移動
-    await page.goto(
-      'https://www.boatrace.jp/owpc/pc/data/racersearch/profile?toban=3941',
+    // 要素取得
+    const elementsArray = [...document.querySelectorAll('.list3 dd')].map(
+      (node) => {
+        return node.innerHTML;
+      },
     );
 
-    // タイトルを取得
-    const pageTitle: string = await page.title();
-    console.log('ページのタイトル:', pageTitle);
+    console.log('elementsArray', elementsArray);
 
-    const cardLocator = await page.locator('.list3');
-    const cardCount = await cardLocator.innerText();
-    console.log(cardCount);
-
-    // ブラウザを閉じる
-    await browser.close();
     // オブジェクトの形で返却すれば受け取る側が分割代入で使える
-    return cardCount;
+    return document;
   }
 }
