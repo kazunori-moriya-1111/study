f2c606735a15dba4f29bd51109e1f300f18c85a3
kazunori_moriya <g.ff.k.96@gmail.com>
2023-12-02 16:42:13 +0900

    自己代入

--- php/src/index.php
+++ php/src/index.php
@@ -1,4 +1,16 @@
 <?php
 $name = 'Bob';
-echo "he{$name}llo, $name\n<br>" ;
-echo 'he{$name}llo, $name\n' ;
+echo "he{$name}llo, $name\n<br>";
+echo 'he{$name}llo, $name\n';
+// 自己代入
+$i = 1;
+$j = 2;
+$i = $i + $j;
+
+// 自己代入演算子
+$i += $j;
+
+// インクリメント演算子
+$i++;
+echo $i;
+echo $j;
