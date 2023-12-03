ffb4c48fc495839343fd4bf14e8bae3249ad96a3
kazunori_moriya <g.ff.k.96@gmail.com>
2023-12-03 21:04:53 +0900

    条件分岐の書き方

--- php/src/index.php
+++ php/src/index.php
@@ -28,3 +28,18 @@ echo "<br>";
 var_dump($i === 1);
 
 // 条件分岐の書き方
+$score = 80;
+$kesseki = 4;
+if ($score < 50 || $kesseki >= 5) {
+    echo "不合格";
+} elseif ($score < 70) {
+    echo "合格";
+} else {
+    echo "秀";
+}
+// ==と===の違い
+if (1 === '1') {
+    echo 'true';
+} else {
+    echo 'false';
+}
