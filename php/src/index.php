2705811b5f5e6b3c3dfc8a7e4b2d05ad93ea7fa5
kazunori_moriya <g.ff.k.96@gmail.com>
2023-12-07 18:22:59 +0900

    phpdocの書き方

--- php/src/index.php
+++ php/src/index.php
@@ -132,7 +132,22 @@ $result =  sum($numbers);
 echo "合計：{$result}<br>";
 sum($numbers2);
 
+/** DocComment
+ * 税率計算のための関数を記述するためのファイル
+ * 
+ * @author moriya
+ * @since 1.0.0
+ */
 $price = 1000;
+
+/**
+ * 税込金額を取得する関数
+ * 
+ * @param int $base_price 価格
+ * @param float $tax_rate 税率
+ * 
+ * @return int 税込金額
+ */
 function with_tax($base_price, $tax_rate = 0.1)
 {
     $sum_price = $base_price + ($base_price * $tax_rate);
