2bb11559a604e1605ccbe9903587f80b8da4b444
kazunori_moriya <g.ff.k.96@gmail.com>
2023-12-23 17:04:59 +0900

    データ更新クエリ

--- php/src/db/index.php
+++ php/src/db/index.php
@@ -7,8 +7,7 @@ $dbName = 'test_phpdb';
 $dsn = "mysql:host={$host};port=3306;dbname={$dbName};";
 $conn = new PDO($dsn, $user, $pwd);
 $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-$pst = $conn->query('select * from mst_shops');
-$result = $pst->fetchAll();
+$result = $conn->exec('update mst_prefs set name = "福島" where id = 5');
 
 echo '<pre>';
 print_r($result);
