3c9fea5dfa4103616dab08006f2df0373b28b99f
kazunori_moriya <g.ff.k.96@gmail.com>
2023-12-24 12:38:55 +0900

    PDOエラー設定

--- php/src/db/index.php
+++ php/src/db/index.php
@@ -7,6 +7,7 @@ $dbName = 'test_phpdb';
 $dsn = "mysql:host={$host};port=3306;dbname={$dbName};";
 $conn = new PDO($dsn, $user, $pwd);
 $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $result = $conn->exec('update mst_prefs set name = "福島" where id = 5');
 
 echo '<pre>';
