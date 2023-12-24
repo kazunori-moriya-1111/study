34c80c43d83a0e885668f0b934e04f65ceeddf30
kazunori_moriya <g.ff.k.96@gmail.com>
2023-12-24 16:14:38 +0900

    DB操作クラス作成

--- php/src/db/index.php
+++ php/src/db/index.php
@@ -1,17 +1,14 @@
 <?php
+require_once 'datasource.php';
 
-$user = 'test_user';
-$pwd = 'pwd';
-$host = "mysql5.7-trial2";
-$dbName = 'test_phpdb';
-$dsn = "mysql:host={$host};port=3306;dbname={$dbName};";
-$conn = new PDO($dsn, $user, $pwd);
-$conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$result = $conn->exec('update mst_prefs set name = "福島" where id = 5');
+use db\DataSource;
 
-echo '<pre>';
-print_r($result);
-echo '<pre>';
-
-$conn = null;
+try {
+    $db = new DataSource();
+    $result = $db->selectOne("select * from test_phpdb.mst_shops where id = :id", [
+        ':id' => 1
+    ]);
+    var_dump($result);
+} catch (Error $e) {
+    echo $e;
+}
