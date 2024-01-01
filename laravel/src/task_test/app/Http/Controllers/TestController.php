315c15e9bb94bbb645a4af7a3416ea0c14f814d2
kazunori_moriya <g.ff.k.96@gmail.com>
2024-01-01 21:46:22 +0900

    クエリビルダによるDB更新

--- laravel/src/task_test/app/Http/Controllers/TestController.php
+++ laravel/src/task_test/app/Http/Controllers/TestController.php
@@ -4,13 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Models\Test;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller
 {
     public function index()
     {
+        // エロクアント（クエリ分割、リレーション機能が使いやすい）
         $values = Test::all();
         // dd($values);
+
+        // クエリビルダ（エロクアントと比較して速い）
+        $queryBuilder = DB::table('tests')->where('text', '=', 'aaa')->select('id', 'text')->get();
+        dd($queryBuilder);
         return view('tests.test', compact('values'));
     }
 }
