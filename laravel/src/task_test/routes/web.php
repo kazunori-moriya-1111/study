51dd48879ef2a2951b6ffde2689e514011e8b960
kazunori_moriya <g.ff.k.96@gmail.com>
2024-01-11 17:02:34 +0900

    認証、グループを用いたルーティング

--- laravel/src/task_test/routes/web.php
+++ laravel/src/task_test/routes/web.php
@@ -19,6 +19,16 @@ use App\Http\Controllers\ContactFormController;
 Route::get('tests/test', [TestController::class, 'index']);
 
 // Route::resource('contacts', ContactFormController::class);
+
+
+Route::prefix('contacts')
+    ->middleware(['auth'])
+    ->controller(ContactFormController::class)
+    ->name('contacts.')
+    ->group(function () {
+        Route::get('/', 'index')->name('index');
+    });
+
 Route::get('/', function () {
     return view('welcome');
 });
