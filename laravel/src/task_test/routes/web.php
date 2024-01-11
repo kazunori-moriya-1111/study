7e70b471a188b1614cab049f480432fc1ec35926
kazunori_moriya <g.ff.k.96@gmail.com>
2024-01-11 16:05:02 +0900

    コードの復元

--- laravel/src/task_test/routes/web.php
+++ laravel/src/task_test/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('tests/test', [TestController::class, 'index']);
 Route::get('/', function () {
     return view('welcome');
 });
@@ -28,4 +30,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
