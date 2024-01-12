3c34a060cdc44de4dbc341cee8d17c45aa394ef9
kazunori_moriya <g.ff.k.96@gmail.com>
2024-01-12 21:10:02 +0900

    css,jsの読み込み

--- php/src/app/config.php
+++ php/src/app/config.php
@@ -1,5 +1,7 @@
 <?php
 $url = $_SERVER['REQUEST_URI'];
 echo $url;
-define('BASE_CONTEXT_PATH', $url);
+define('BASE_CONTEXT_PATH', '/app/');
 define('BASE_IMAGE_PATH', BASE_CONTEXT_PATH . 'images/');
+define('BASE_JS_PATH', BASE_CONTEXT_PATH . 'js/');
+define('BASE_CSS_PATH', BASE_CONTEXT_PATH . 'css/');
--- php/src/app/index.php
+++ php/src/app/index.php
@@ -8,10 +8,12 @@ require_once 'config.php'
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="<?php echo BASE_CSS_PATH ?>sample.css">
 </head>
 
 <body>
     <img src="<?php echo BASE_IMAGE_PATH ?>logo.svg" alt="">
+    <script src="<?php echo BASE_JS_PATH ?>sample.js"></script>
 </body>
 
 </html>
