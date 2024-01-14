574241cd7c4967556a6c9f034c8cafce3937468e
kazunori_moriya <g.ff.k.96@gmail.com>
2024-01-14 12:00:45 +0900

    編集画面作成

--- laravel/src/task_test/app/Http/Controllers/ContactFormController.php
+++ laravel/src/task_test/app/Http/Controllers/ContactFormController.php
@@ -95,7 +95,8 @@ class ContactFormController extends Controller
      */
     public function edit($id)
     {
-        //
+        $contact = ContactForm::find($id);
+        return view('contacts.edit', compact('contact'));
     }
 
     /**
