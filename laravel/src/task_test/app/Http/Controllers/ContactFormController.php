11c9cc767afc128a1afb07950ae760f62eb32665
kazunori_moriya <g.ff.k.96@gmail.com>
2024-01-14 22:06:15 +0900

    検索フォーム作成

--- laravel/src/task_test/app/Http/Controllers/ContactFormController.php
+++ laravel/src/task_test/app/Http/Controllers/ContactFormController.php
@@ -14,12 +14,17 @@ class ContactFormController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         // $contacts = ContactForm::select('id', 'name', 'title', 'created_at')->get();
 
         // ページネーション対応
-        $contacts = ContactForm::select('id', 'name', 'title', 'created_at')->paginate(20);
+        // $contacts = ContactForm::select('id', 'name', 'title', 'created_at')->paginate(20);
+        // return view('contacts.index', compact('contacts'));
+        $search = $request->search;
+        $query = ContactForm::search($search);
+        // 検索対応
+        $contacts = $query->select('id', 'name', 'title', 'created_at')->paginate(20);
         return view('contacts.index', compact('contacts'));
     }
 
--- laravel/src/task_test/resources/views/contacts/index.blade.php
+++ laravel/src/task_test/resources/views/contacts/index.blade.php
@@ -9,7 +9,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <a href="{{ route('contacts.create') }}" class="text-blue-500 underline">新規登録</a>
+                    <a href="{{ route('contacts.create') }}" class="text-blue-500 underline">新規登録</a><br>
+                    <form class="mb-8" method="get" action="{{ route('contacts.index') }}">
+                        <input type="text" name="search" placeholder="検索">
+                        <button class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">検索する</button>
+                    </form>
                     <div class="lg:w-2/3 w-full mx-auto overflow-auto">
                         <table class="table-auto w-full text-left whitespace-no-wrap">
                             <thead>
