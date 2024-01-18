26534d236428efeccd60680af05eccc0a56202a1
kazunori_moriya <g.ff.k.96@gmail.com>
2024-01-18 23:15:35 +0900

    トップページのデザイン修正

--- portfolio/src/resources/views/home.blade.php
+++ portfolio/src/resources/views/home.blade.php
@@ -10,6 +10,25 @@
 
 <body>
     <section class="text-gray-600 body-font">
+        <div class="container px-5 py-24 mx-auto flex flex-col">
+            <div class="lg:w-4/6 mx-auto">
+                <div class="flex flex-col sm:flex-row mt-10">
+                    <div class="sm:w-1/3 text-center sm:pr-8 sm:py-8">
+                        <img src=" {{ asset('img/profile.png') }}" class="w-30 h-30" />
+                        <div class=" flex flex-col items-center text-center justify-center">
+                            <h2 class="font-medium title-font mt-4 text-gray-900 text-lg">森谷和徳</h2>
+                            <div class="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>
+                            <p class="text-base">moriya kazunori</p>
+                        </div>
+                    </div>
+                    <div class="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
+                        <p class="leading-relaxed text-lg mb-4">このサイトでは〜〜</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="text-gray-600 body-font">
         <div class="container px-5 py-24 mx-auto">
             <div class="flex flex-col text-center w-full mb-20">
                 <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">森谷和徳のポートフォリオ</h1>
@@ -53,7 +72,6 @@
                     </a>
                 </div>
             </div>
-            <button class="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
         </div>
     </section>
 </body>
