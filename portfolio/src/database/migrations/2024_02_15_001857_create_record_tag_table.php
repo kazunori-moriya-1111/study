1c14e875064f2ec95e573e8e8f89c949fb823bd5
kazunori_moriya <g.ff.k.96@gmail.com>
2024-02-15 00:42:19 +0900

    収支管理機能に必要なテーブル作成

---
+++ portfolio/src/database/migrations/2024_02_15_001857_create_record_tag_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('record_tag', function (Blueprint $table) {
+            $table->foreignId('record_id')->constrained();
+            $table->foreignId('tag_id')->constrained();
+            $table->timestamps();
+            $table->primary(['record_id', 'tag_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('record_tag');
+    }
+};
