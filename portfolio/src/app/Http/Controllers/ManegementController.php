b63100e218a2e039d44f0574357d2a67b153e921
kazunori_moriya <g.ff.k.96@gmail.com>
2024-02-18 14:15:45 +0900

    ルーティング修正

--- portfolio/src/app/Http/Controllers/ManegementController.php
+++ portfolio/src/app/Http/Controllers/ManegementController.php
@@ -10,6 +10,6 @@ class ManegementController extends Controller
     public function index()
     {
         $data = [1, 2, 3, 4, 5, 6];
-        return view('chartjs', compact('data'));
+        return view('manegement', compact('data'));
     }
 }
