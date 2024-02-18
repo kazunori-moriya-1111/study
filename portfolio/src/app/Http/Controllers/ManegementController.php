b5b3f5d28bf071e93616318227d82d189570db41
kazunori_moriya <g.ff.k.96@gmail.com>
2024-02-18 20:46:11 +0900

    DBから取得した値をbladeに渡す

--- portfolio/src/app/Http/Controllers/ManegementController.php
+++ portfolio/src/app/Http/Controllers/ManegementController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Record;
 
 class ManegementController extends Controller
 {
     //
     public function index(Request $request)
     {
-        $data = [1, 2, 3, 4, 5, 6];
+        $collection = Record::select('bet')->where('user_id', 1)->limit(6)->get();
+        $data = $collection->pluck('bet')->all();
         return view('manegement.index', compact('data'));
     }
     public function create()
