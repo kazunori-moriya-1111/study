a511d62736bfc73fed84b528f8d60c16b86b0faf
kazunori_moriya <g.ff.k.96@gmail.com>
2024-02-29 22:34:15 +0900

    詳細画面blade実装

--- portfolio/src/resources/views/manegement/show.blade.php
+++ portfolio/src/resources/views/manegement/show.blade.php
@@ -11,7 +11,21 @@
 <body>
     <div>ボートレース収支管理アプリ_show</div>
     <a href="{{ route('manegement.index') }}" class="text-blue-500 underline">TOPへ</a><br>
-    <p>{{ $record->date }} {{ $record->bet }} {{ $record->payout }} {{ $record->memo }}</p>
+    <label for="date">日付</label>
+    <div>{{ $record->date }}</div>
+    <br>
+    <label for="bet">掛け金</label>
+    <div>{{ $record->bet }}</div>
+    <br>
+    <label for="payout">払戻金</label>
+    <div>{{ $record->payout }}</div>
+    <br>
+    @if($record->memo)
+    <label for="memo">メモ</label>
+    <div>{{ $record->memo }}</div>
+    <br>
+    @endif
+
 </body>
 
 </html>
