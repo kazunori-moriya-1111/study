a1e92bd4855fd196e07cadd47a8eaa5eaa09ba56
kazunori_moriya <g.ff.k.96@gmail.com>
2024-03-04 20:08:51 +0900

    fullcalendar動作確認

--- portfolio/src/resources/views/manegement/index.blade.php
+++ portfolio/src/resources/views/manegement/index.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/chartjs.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/chartjs.js', 'resources/js/calendar.js'])
     <title>Laravel</title>
 </head>
 
@@ -15,6 +15,7 @@
     <div class="w-80">
         <canvas id="myChart"></canvas>
     </div>
+    <div id="calendar" class="w-80"></div>
     <!-- データ一覧表示 -->
     @foreach($data as $row)
     <p>日付:{{ $row->date }} 掛け金:{{ $row->bet }} 払い戻し金:{{ $row->payout }}</p>
