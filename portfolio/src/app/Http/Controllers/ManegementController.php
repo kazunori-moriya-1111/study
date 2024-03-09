7367968987a6fca39b8508a041ec8dddf58f28fa
kazunori_moriya <g.ff.k.96@gmail.com>
2024-03-09 21:14:21 +0900

    日次、週次の集計ロジック実装

--- portfolio/src/app/Http/Controllers/ManegementController.php
+++ portfolio/src/app/Http/Controllers/ManegementController.php
@@ -31,17 +31,124 @@ class ManegementController extends Controller
 
     public function totalling($type)
     {
-        // type別の集計、6期間分の補完ロジック
+        // type別の集計、7期間分の補完ロジック
         $record = null;
         $user_id = 1;
-        // 一週間での集計
-        $record = Record::selectRaw('SUM(bet) as total_bet, SUM(payout) as total_payout, subdate(date, weekday(date)) as week')
-            ->where('user_id', $user_id)
-            ->groupBy('week')
-            ->get();
-        $title = $record->pluck('week')->all();
-        $total_bet = $record->pluck('total_bet')->all();
-        $total_payout = $record->pluck('total_payout')->all();
+        $from = null;
+        $to = null;
+        $sordedTmp = null;
+        $type = 'week';
+        // 日単位での集計
+        if ($type == 'day') {
+            // DBからデータ取得
+            $to = date('Y-m-d');
+            $from = date('Y-m-d', strtotime('-6 day'));
+            $record = Record::select('bet', 'payout', 'date')
+                ->where('user_id', $user_id)
+                ->whereBetween('date', [$from, $to])
+                ->get()
+                ->groupBy('date');
+            //groupby sum
+            $tmp = collect();
+            $record->each(function ($row, $key) use ($tmp) {
+                $tmp->push([
+                    'date' => $key,
+                    'total_bet' => $row->sum('bet'),
+                    'total_payout' => $row->sum('payout')
+                ]);
+            });
+            // データ補完
+            if ($tmp->count() <= 6) {
+                // 期間配列作る
+                $diff = (strtotime($to) - strtotime($from)) / (60 * 60 * 24);
+                $period = collect();
+                for ($i = 0; $i <= $diff; $i++) {
+                    $period->push(date('Y-m-d', strtotime($from . '+' . $i . 'days')));
+                }
+                // eachで一つ一つの期間の有無を確認。なければ期間とtotal_bet => 0, total_payout => 0を埋める
+                $getDataDate = $tmp->pluck('date');
+                $period->each(function ($date) use ($getDataDate, $tmp) {
+                    if (!($getDataDate->contains($date))) {
+                        $tmp->push([
+                            'date' => $date,
+                            'total_bet' => 0,
+                            'total_payout' => 0
+                        ]);
+                    }
+                });
+            }
+            // 日付順にソート
+            $sortedData = $tmp->sortBy('date');
+            // グラフ表示用に成形
+            $title = $sortedData->pluck('date')->all();
+            $total_bet = $sortedData->pluck('total_bet')->all();
+            $total_payout = $sortedData->pluck('total_payout')->all();
+        }
+        // 週単位での集計
+        if ($type == 'week') {
+            // DBからデータ取得
+            $to = date('Y-m-d');
+            // TODO 6週前の日曜日からにする
+            $from = date('Y-m-d', strtotime('-6 week'));
+            $record = Record::select('bet', 'payout', 'date')
+                ->where('user_id', $user_id)
+                ->whereBetween('date', [$from, $to])
+                ->get();
+            // dateからweekを算出
+            $recordAddWeek = $record->map(function ($row) {
+                $weekNo = date('w', strtotime($row->date));
+                $week = date('Y-m-d', strtotime("-{$weekNo} day", strtotime($row->date)));
+                // dd($weekNo, $row->date, $week);
+                return [
+                    'week' => $week,
+                    'bet' => $row->bet,
+                    'payout' => $row->payout
+                ];
+            })->groupBy('week');
+            //groupby sum
+            $tmp = collect();
+            $recordAddWeek->each(function ($row, $key) use ($tmp) {
+                $tmp->push([
+                    'week' => $key,
+                    'total_bet' => $row->sum('bet'),
+                    'total_payout' => $row->sum('payout')
+                ]);
+            });
+            // データ補完
+            if ($tmp->count() <= 6) {
+                // 期間配列作る
+                $toWeekNo = date('w', strtotime($to));
+                $weekstartTo = date('Y-m-d', strtotime("-{$toWeekNo} day", strtotime($to)));
+                $fromWeekNo = date('w', strtotime($from));
+                $weekStartFrom = date('Y-m-d', strtotime("-{$fromWeekNo} day", strtotime($from)));;
+                $diff = (strtotime($weekstartTo) - strtotime($weekStartFrom)) / (60 * 60 * 24 * 7);
+                $period = collect();
+                for ($i = 0; $i <= $diff; $i++) {
+                    $period->push(date('Y-m-d', strtotime($weekStartFrom . '+' . 7 * $i . 'days')));
+                }
+                // dd($period);
+                // eachで一つ一つの期間の有無を確認。なければ期間とtotal_bet => 0, total_payout => 0を埋める
+                $getDataDate = $tmp->pluck('week');
+                $period->each(function ($week) use ($getDataDate, $tmp) {
+                    if (!($getDataDate->contains($week))) {
+                        $tmp->push([
+                            'week' => $week,
+                            'total_bet' => 0,
+                            'total_payout' => 0
+                        ]);
+                    }
+                });
+            }
+            // 日付順にソート
+            $sortedData = $tmp->sortBy('week');
+            // グラフ表示用に成形
+            $title = $sortedData->pluck('week')->all();
+            $total_bet = $sortedData->pluck('total_bet')->all();
+            $total_payout = $sortedData->pluck('total_payout')->all();
+        }
+        // 年単位での集計
+        if ($type == 'year') {
+        }
         return view('manegement.totalling', compact('title', 'total_bet', 'total_payout'));
     }
 
