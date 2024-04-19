49acfcee5d97c1aa9f261d9e072b069c117ac708
kazunori_moriya <g.ff.k.96@gmail.com>
2024-04-19 21:06:04 +0900

    カレンダーページ集計表用コンポーネント作成

---
+++ portfolio/src/resources/views/livewire/calendar-result.blade.php
@@ -0,0 +1,30 @@
+<div class="relative overflow-x-auto">
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    総掛け金
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    総払い戻し金
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    回収率
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    100
+                </th>
+                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    200
+                </td>
+                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    200%
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
--- portfolio/src/resources/views/manegement/calendar.blade.php
+++ portfolio/src/resources/views/manegement/calendar.blade.php
@@ -13,14 +13,21 @@ $json_array = Js::from($json_array);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @vite(['resources/css/app.css','resources/css/calender.css' , 'resources/js/app.js', 'resources/js/calendar.js'])
     <title>Laravel</title>
+    @livewireStyles
 </head>
 
 <body>
     <x-nav-bar />
-    <div id="calendar" class="w-90"></div>
-    <script>
-        json_array = <?php echo $json_array ?>;
-    </script>
+    <article class="container mx-auto">
+        <section class="my-1">
+            <livewire:calendar-result />
+        </section>
+        <div id="calendar" class="my-1"></div>
+    </article>
+    @livewireScripts
 </body>
+<script>
+    json_array = <?php echo $json_array ?>;
+</script>
 
 </html>
