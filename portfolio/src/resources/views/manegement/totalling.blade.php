2e1f695dc54550f727cfbe312532244cb54fb838
kazunori_moriya <g.ff.k.96@gmail.com>
2024-04-19 21:05:09 +0900

    集計ページのcss調整

--- portfolio/src/resources/views/manegement/totalling.blade.php
+++ portfolio/src/resources/views/manegement/totalling.blade.php
@@ -10,12 +10,16 @@
 
 <body>
     <x-nav-bar />
-    <a href="{{ route('manegement.totalling.date') }}" @class(['bg-blue-500'=> !$selected_period['day'], 'bg-red-500' => $selected_period['day'], 'hover:bg-blue-700' => !$selected_period['day'], 'hover:bg-red-700' => $selected_period['day'], 'text-white' , 'font-bold' , 'py-2' , 'px-4' , 'rounded' ])>日次</a>
-    <a href="{{ route('manegement.totalling.week') }}" @class(['bg-blue-500'=> !$selected_period['week'], 'bg-red-500'=> $selected_period['week'] , 'hover:bg-blue-700' => !$selected_period['week'], 'hover:bg-red-700'=> $selected_period['week'] , 'text-white' , 'font-bold' , 'py-2' , 'px-4' , 'rounded' ])>週次</a>
-    <a href="{{ route('manegement.totalling.year') }}" @class(['bg-blue-500'=> !$selected_period['year'], 'bg-red-500'=> $selected_period['year'] , 'hover:bg-blue-700' => !$selected_period['year'], 'hover:bg-red-700'=> $selected_period['year'] , 'text-white' , 'font-bold' , 'py-2' , 'px-4' , 'rounded' ])>年次</a>
-    <div class="mx-atuo w-1/2">
-        <canvas class="" id="myChart"></canvas>
-    </div>
+    <article class="container mx-auto">
+        <section class="flex items-center justify-center my-1">
+            <a href="{{ route('manegement.totalling.date') }}" @class(['bg-blue-500'=> !$selected_period['day'], 'bg-red-500' => $selected_period['day'], 'hover:bg-blue-700' => !$selected_period['day'], 'hover:bg-red-700' => $selected_period['day'], 'text-white', 'font-bold', 'py-2', 'px-4', 'mx-4', 'rounded' ])>日次</a>
+            <a href="{{ route('manegement.totalling.week') }}" @class(['bg-blue-500'=> !$selected_period['week'], 'bg-red-500'=> $selected_period['week'], 'hover:bg-blue-700' => !$selected_period['week'], 'hover:bg-red-700'=> $selected_period['week'], 'text-white', 'font-bold' , 'py-2', 'px-4', 'mx-4', 'rounded' ])>週次</a>
+            <a href="{{ route('manegement.totalling.year') }}" @class(['bg-blue-500'=> !$selected_period['year'], 'bg-red-500'=> $selected_period['year'], 'hover:bg-blue-700' => !$selected_period['year'], 'hover:bg-red-700'=> $selected_period['year'], 'text-white', 'font-bold' , 'py-2', 'px-4', 'mx-4', 'rounded' ])>年次</a>
+        </section>
+        <section class="flex items-center justify-center my-1">
+            <canvas class="" id="myChart"></canvas>
+        </section>
+    </article>
     <script>
         title = <?php echo json_encode($title) ?>;
         total_bet = <?php echo json_encode($total_bet) ?>;
