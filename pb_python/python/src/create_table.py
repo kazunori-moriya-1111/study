c0af140b943d0650b59ce9b883b69909902cabd2
kazunori_moriya <g.ff.k.96@gmail.com>
2024-12-29 17:38:38 +0900

    テーブル作成時に初期値入力処理追加

--- pb_python/python/src/create_table.py
+++ pb_python/python/src/create_table.py
@@ -2,33 +2,35 @@ from model.racer_info import RacerInfo
 from model.race_result import RaceResult
 from model.race_field import RaceField
 from setting import Base, Engine, session
+from sqlalchemy.dialects.mysql import insert  # type: ignore
 
 if __name__ == "__main__":
     Base.metadata.create_all(bind=Engine)
-    session.bulk_save_objects([
-        RaceField(race_field_number="01", race_field_name="桐生"),
-        RaceField(race_field_number="02", race_field_name="戸田"),
-        RaceField(race_field_number="03", race_field_name="江戸川"),
-        RaceField(race_field_number="04", race_field_name="平和島"),
-        RaceField(race_field_number="05", race_field_name="多摩川"),
-        RaceField(race_field_number="06", race_field_name="浜名湖"),
-        RaceField(race_field_number="07", race_field_name="蒲郡"),
-        RaceField(race_field_number="08", race_field_name="常滑"),
-        RaceField(race_field_number="09", race_field_name="津"),
-        RaceField(race_field_number="10", race_field_name="三国"),
-        RaceField(race_field_number="11", race_field_name="びわこ"),
-        RaceField(race_field_number="12", race_field_name="住之江"),
-        RaceField(race_field_number="13", race_field_name="尼崎"),
-        RaceField(race_field_number="14", race_field_name="鳴門"),
-        RaceField(race_field_number="15", race_field_name="丸亀"),
-        RaceField(race_field_number="16", race_field_name="児島"),
-        RaceField(race_field_number="17", race_field_name="宮島"),
-        RaceField(race_field_number="18", race_field_name="徳山"),
-        RaceField(race_field_number="19", race_field_name="下関"),
-        RaceField(race_field_number="20", race_field_name="若松"),
-        RaceField(race_field_number="21", race_field_name="芦屋"),
-        RaceField(race_field_number="22", race_field_name="福岡"),
-        RaceField(race_field_number="23", race_field_name="唐津"),
-        RaceField(race_field_number="24", race_field_name="大村"),
+    insert_ignore_stmt = insert(RaceField).prefix_with("IGNORE")
+    session.execute(insert_ignore_stmt, [
+        {"race_field_number":"01", "race_field_name":"桐生"},
+        {"race_field_number":"02", "race_field_name":"戸田"},
+        {"race_field_number":"03", "race_field_name":"江戸川"},
+        {"race_field_number":"04", "race_field_name":"平和島"},
+        {"race_field_number":"05", "race_field_name":"多摩川"},
+        {"race_field_number":"06", "race_field_name":"浜名湖"},
+        {"race_field_number":"07", "race_field_name":"蒲郡"},    
+        {"race_field_number":"08", "race_field_name":"常滑"},
+        {"race_field_number":"09", "race_field_name":"津"},
+        {"race_field_number":"10", "race_field_name":"三国"},
+        {"race_field_number":"11", "race_field_name":"びわこ"},
+        {"race_field_number":"12", "race_field_name":"住之江"},
+        {"race_field_number":"13", "race_field_name":"尼崎"},
+        {"race_field_number":"14", "race_field_name":"鳴門"},
+        {"race_field_number":"15", "race_field_name":"丸亀"},    
+        {"race_field_number":"16", "race_field_name":"児島"},
+        {"race_field_number":"17", "race_field_name":"宮島"},
+        {"race_field_number":"18", "race_field_name":"徳山"},
+        {"race_field_number":"19", "race_field_name":"下関"},
+        {"race_field_number":"20", "race_field_name":"若松"},
+        {"race_field_number":"21", "race_field_name":"芦屋"},
+        {"race_field_number":"22", "race_field_name":"福岡"},
+        {"race_field_number":"23", "race_field_name":"唐津"},
+        {"race_field_number":"24", "race_field_name":"大村"},
     ])
     session.commit()
