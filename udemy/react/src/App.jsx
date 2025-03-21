ac37c78fffa3db8921d3f483e01e61fae2470b71
kazunori_moriya <g.ff.k.96@gmail.com>
2025-03-21 20:02:06 +0900

    node環境構築

---
+++ udemy/react/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="component">
+      <h3>Hello Component</h3>
+    </div>
+  );
+}
+
+export default App;
---
+++ udemy/ts/dist/index.js
@@ -0,0 +1,8 @@
+hello = "hello";
+console.log(hello);
+// export {};
+console.log(globalThis);
+console.log(globalThis.hello);
+console.log(this.hello);
+const package = 1;
+console.log(Proxy);
