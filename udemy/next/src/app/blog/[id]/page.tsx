8fa77159df27e844e120429988a821e750a0a1ff
kazunori_moriya <g.ff.k.96@gmail.com>
2025-04-13 23:28:37 +0900

    paramsの型設定

--- udemy/next/src/app/blog/[id]/page.tsx
+++ udemy/next/src/app/blog/[id]/page.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
-export default async function BlogPage({ params }: { params: { id: string } }) {
+type Params = {
+    params: Promise<{ id: string }>
+}
+
+export default async function BlogPage({ params }: Params) {
     const { id } = await params;
     return (
     <div>
