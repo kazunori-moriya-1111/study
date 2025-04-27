9a88a8c1ae87d3feca3984fb1370555ebfb5b093
kazunori_moriya <g.ff.k.96@gmail.com>
2025-04-27 19:57:07 +0900

    prettier適用

--- udemy/next/src/components/ClientComponent.tsx
+++ udemy/next/src/components/ClientComponent.tsx
@@ -1,15 +1,15 @@
-'use client'
+'use client';
 
-import { useState } from 'react'
+import { useState } from 'react';
 
 export default function RCCPage() {
-    const [count, setCount] = useState(0)
-    console.log('RCCPage')
-    return (
-        <div>
-            <h1>RCCPage</h1>
-            <p>Count: {count}</p>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
-        </div>
-        )
+  const [count, setCount] = useState(0);
+  console.log('RCCPage');
+  return (
+    <div>
+      <h1>RCCPage</h1>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
 }
--- udemy/next/src/lib/actions/contact.ts
+++ udemy/next/src/lib/actions/contact.ts
@@ -1,39 +1,40 @@
-'use server'
-import { redirect } from "next/navigation"
-import { ContactSchema } from "@/validations/contact"
+'use server';
+import { redirect } from 'next/navigation';
+import { ContactSchema } from '@/validations/contact';
 
 // ActionStateの型定義
 type ActionState = {
-    success: boolean
-    errors: {
-        name?: string[]
-        email?: string[]
-    }
-    serverError?: string
-}
+  success: boolean;
+  errors: {
+    name?: string[];
+    email?: string[];
+  };
+  serverError?: string;
+};
 
 export async function submitContactForm(
-    prevState: ActionState,
-    formData: FormData): Promise<ActionState> {
-    const name = formData.get('name')
-    const email = formData.get('email')
+  prevState: ActionState,
+  formData: FormData
+): Promise<ActionState> {
+  const name = formData.get('name');
+  const email = formData.get('email');
 
-    // バリデーション
-    const validationResult = ContactSchema.safeParse({name, email})
-    if (!validationResult.success) {
-        const errorMessage = validationResult.error.flatten().fieldErrors
-        console.log('サーバー側でエラーが発生しました', errorMessage)
-        return {
-            success: false,
-            errors: {
-                name: errorMessage.name || [],
-                email: errorMessage.email || [],
-            },
-        }
-    }
+  // バリデーション
+  const validationResult = ContactSchema.safeParse({ name, email });
+  if (!validationResult.success) {
+    const errorMessage = validationResult.error.flatten().fieldErrors;
+    console.log('サーバー側でエラーが発生しました', errorMessage);
+    return {
+      success: false,
+      errors: {
+        name: errorMessage.name || [],
+        email: errorMessage.email || [],
+      },
+    };
+  }
 
-    // DB登録
+  // DB登録
 
-    console.log('送信されたデータ:', {name, email})
-    redirect('/contacts/complete')
+  console.log('送信されたデータ:', { name, email });
+  redirect('/contacts/complete');
 }
