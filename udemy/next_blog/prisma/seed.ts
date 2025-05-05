5cc13f741fd99c3c786227b881ceccfb52ad60f9
kazunori_moriya <g.ff.k.96@gmail.com>
2025-05-05 12:58:54 +0900

    ダミーデータ設定

---
+++ udemy/next_blog/prisma/seed.ts
@@ -0,0 +1,52 @@
+import { PrismaClient } from "@prisma/client";
+import * as bcrypt from "bcryptjs";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // クリーンアップ
+  await prisma.post.deleteMany();
+  await prisma.user.deleteMany();
+
+  const hashedPassword = await bcrypt.hash("password", 10);
+
+  const dummyImages = [
+    "https://picsum.photos/seed/post1/600/400",
+    "https://picsum.photos/seed/post2/600/400",
+  ];
+
+  const user = await prisma.user.create({
+    data: {
+      email: "test@example.com",
+      name: "test",
+      password: hashedPassword,
+      posts: {
+        create: [
+          {
+            title: "初めてのブログ登録",
+            content: "これはテスト投稿です。",
+            topImage: dummyImages[0],
+            published: true,
+          },
+          {
+            title: "2ばんめのブログ登録",
+            content: "これは2ばんめのテスト投稿です。",
+            topImage: dummyImages[1],
+            published: true,
+          },
+        ],
+      },
+    },
+  });
+
+  console.log({ user });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
