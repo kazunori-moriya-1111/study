f5b949d822714107357487e3f8fcd6f7a725ff43
kazunori_moriya <g.ff.k.96@gmail.com>
2025-05-11 19:56:56 +0900

    記事詳細ページ追加

--- udemy/next_blog/src/lib/post.ts
+++ udemy/next_blog/src/lib/post.ts
@@ -13,3 +13,14 @@ export async function getPosts() {
     },
   });
 }
+
+export async function getPost(id: string) {
+  return await prisma.post.findUnique({
+    where: { id },
+    include: {
+      author: {
+        select: { name: true },
+      },
+    },
+  });
+}
