972e0161e77601f80216f7233a56efda0b231d30
kazunori_moriya <g.ff.k.96@gmail.com>
2025-05-17 00:13:46 +0900

    ログイン機能追加

---
+++ udemy/next_blog/src/app/(auth)/login/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import LoginForm from "@/components/auth/LoginForm";
+
+export default function LoginPage() {
+  return <LoginForm />;
+}
