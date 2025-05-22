8ab4bdbaff4d9662ac7fcf1181f2a12bd867b021
kazunori_moriya <g.ff.k.96@gmail.com>
2025-05-22 21:58:37 +0900

    buildエラー対応

--- udemy/next_blog/src/app/(auth)/login/page.tsx
+++ udemy/next_blog/src/app/(auth)/login/page.tsx
@@ -1,15 +1,4 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import LoginForm from "@/components/auth/LoginForm";
 
 export default function LoginPage() {
--- udemy/next_blog/src/components/post/SearchBox.tsx
+++ udemy/next_blog/src/components/post/SearchBox.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Input } from "@/components/ui/input";
-import { useState, useEffect, use } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 export default function SearchBox() {
