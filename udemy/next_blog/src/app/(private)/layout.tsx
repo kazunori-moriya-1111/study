80f60a698c9fde456eec8eedf50f221aebe35272
kazunori_moriya <g.ff.k.96@gmail.com>
2025-05-24 15:45:00 +0900

    フォーマット

--- udemy/next_blog/src/app/(private)/layout.tsx
+++ udemy/next_blog/src/app/(private)/layout.tsx
@@ -1,13 +1,9 @@
-import PrivateHeader from "@/components/layouts/PrivateHeader";
-export default function PrivateLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import PrivateHeader from '@/components/layouts/PrivateHeader';
+export default function PrivateLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <PrivateHeader />
-      <div className="container mx-auto px-4 py-8">{children}</div>
+      <div className='container mx-auto px-4 py-8'>{children}</div>
     </>
   );
 }
--- udemy/next_blog/src/components/layouts/Setting.tsx
+++ udemy/next_blog/src/components/layouts/Setting.tsx
@@ -3,25 +3,25 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { signOut } from "@/auth";
-import { Button } from "../ui/button";
-import { Session } from "next-auth";
+} from '@/components/ui/dropdown-menu';
+import { signOut } from '@/auth';
+import { Button } from '../ui/button';
+import { Session } from 'next-auth';
 
 export default function Setting({ session }: { session: Session }) {
   const handleLogout = async () => {
-    "use server";
+    'use server';
     await signOut();
   };
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="font-medium">
+        <Button variant='ghost' className='font-medium'>
           {session?.user?.name}
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="w-48">
-        <DropdownMenuItem onClick={handleLogout} className="cursor-point">
+      <DropdownMenuContent align='end' className='w-48'>
+        <DropdownMenuItem onClick={handleLogout} className='cursor-point'>
           ログアウト
         </DropdownMenuItem>
       </DropdownMenuContent>
