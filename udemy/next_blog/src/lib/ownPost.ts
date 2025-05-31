28d1b8fea1b6c198d363b0452a2036b37ffefda2
kazunori_moriya <g.ff.k.96@gmail.com>
2025-05-31 14:10:00 +0900

    記事編集機能

--- udemy/next_blog/src/lib/ownPost.ts
+++ udemy/next_blog/src/lib/ownPost.ts
@@ -1,4 +1,4 @@
-import { prisma } from "@/lib/prisma";
+import { prisma } from '@/lib/prisma';
 
 export async function getOwnPosts(userId: string) {
   return await prisma.post.findMany({
@@ -12,7 +12,25 @@ export async function getOwnPosts(userId: string) {
       updatedAt: true,
     },
     orderBy: {
-      updatedAt: "desc",
+      updatedAt: 'desc',
+    },
+  });
+}
+
+export async function getOwnPost(userId: string, postId: string) {
+  return await prisma.post.findFirst({
+    where: {
+      AND: [{ authorId: userId }, { id: postId }],
+    },
+    select: {
+      id: true,
+      title: true,
+      content: true,
+      topImage: true,
+      author: true,
+      published: true,
+      createdAt: true,
+      updatedAt: true,
     },
   });
 }
