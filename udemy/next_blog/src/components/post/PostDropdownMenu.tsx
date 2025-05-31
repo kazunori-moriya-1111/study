9394021e092f95f3b15c4ae5bac620971e0f41b0
kazunori_moriya <g.ff.k.96@gmail.com>
2025-05-31 19:16:32 +0900

    記事削除機能

--- udemy/next_blog/src/components/post/PostDropdownMenu.tsx
+++ udemy/next_blog/src/components/post/PostDropdownMenu.tsx
@@ -1,3 +1,4 @@
+'use client';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -5,11 +6,22 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import Link from 'next/link';
+import DeletePostDialog from '@/components/post/DeletePostDialog';
+import { useState } from 'react';
 
 export default function PostDropdownMenu({ postId }: { postId: string }) {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+  const handleDeleteDialogChange = (open: boolean) => {
+    setShowDeleteDialog(open);
+    if (!open) {
+      setIsDropdownOpen(false);
+    }
+  };
+
   return (
     <>
-      <DropdownMenu>
+      <DropdownMenu open={isDropdownOpen} onOpenChange={setIsDropdownOpen}>
         <DropdownMenuTrigger className='px-2 py-1 border rounded-md'>⋮</DropdownMenuTrigger>
         <DropdownMenuContent>
           <DropdownMenuItem asChild>
@@ -22,13 +34,20 @@ export default function PostDropdownMenu({ postId }: { postId: string }) {
               編集
             </Link>
           </DropdownMenuItem>
-          <DropdownMenuItem asChild>
-            <Link href={`/posts/delete/${postId}`} className='cursor-pointer text-red-500'>
-              削除
-            </Link>
+          <DropdownMenuItem
+            className='text-red-600 cursor-pointer'
+            onSelect={() => {
+              setIsDropdownOpen(false);
+              setShowDeleteDialog(true);
+            }}
+          >
+            削除
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
+      {showDeleteDialog && (
+        <DeletePostDialog postId={postId} isOpen={showDeleteDialog} onOpenChange={handleDeleteDialogChange} />
+      )}
     </>
   );
 }
