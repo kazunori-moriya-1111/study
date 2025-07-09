591c22c1ff67ba4131c48b7b0acb7e78181659fa
kazunori_moriya <g.ff.k.96@gmail.com>
2025-07-09 22:22:14 +0900

    不要ファイル削除

--- nestjs/app/flea-market/src/auth/decorator/role.decorator.ts
+++
@@ -1,4 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-
-export const Role = (...statuses: string[]) =>
-  SetMetadata('statuses', statuses);
--- nestjs/app/flea-market/src/items/example.spec.ts
+++
@@ -1,7 +0,0 @@
-describe('Jestの練習', () => {
-  it('test1', () => {
-    const result = 1;
-    const expected = 1;
-    expect(result).toEqual(expected);
-  });
-});
