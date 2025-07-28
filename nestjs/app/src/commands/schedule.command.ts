ac9302ab1e7d979e980a71c3a75e321cdca3b678
kazunori_moriya <g.ff.k.96@gmail.com>
2025-07-28 23:48:43 +0900

    racecardsインサート処理

--- nestjs/app/src/commands/schedule.command.ts
+++ nestjs/app/src/commands/schedule.command.ts
@@ -35,11 +35,43 @@ export class ScheduleCommand extends CommandRunner {
       `https://www.boatrace.jp/owsp/sp/spdata?hd=${date}&type=top`,
     );
     const json = await response.json();
-    this.logger.log(JSON.stringify(json['maindata']));
-    await this.prismaService.racecards.create({
-      data: {
-        date: new Date('2023-01-01'),
-      },
+    json['maindata']['infolist'].map(async (infolist) => {
+      const record = await this.prismaService.rawRacecards.create({
+        data: {
+          updatetime: json['maindata']['updatetime'],
+          beforeday: json['maindata']['beforeday'],
+          beforedayflag: json['maindata']['beforedayflag'],
+          hd: json['maindata']['hd'],
+          datamode: json['maindata']['datamode'],
+          today: json['maindata']['today'],
+          nextday: json['maindata']['nextday'],
+          nextdayflag: json['maindata']['nextdayflag'],
+          presalelinkflag: json['maindata']['presalelinkflag'],
+          presaleend: json['maindata']['presaleend'],
+          jcd: infolist['jcd'],
+          jname: infolist['jname'],
+          presaleflag: infolist['presaleflag'],
+          favracerflag: infolist['favracerflag'],
+          status: infolist['status'],
+          statusmessage1: infolist['statusmessage1'],
+          statusmessage2: infolist['statusmessage2'],
+          nowraceno: infolist['nowraceno'],
+          deadline: infolist['deadline'],
+          gradeicon: infolist['gradeicon'],
+          racelistflag: infolist['racelistflag'],
+          womanicon: infolist['womanicon'],
+          raceclscd: infolist['raceclscd'],
+          gradecode: infolist['gradecode'],
+          nightflag: infolist['nightflag'],
+          kfrom: infolist['kfrom'],
+          kto: infolist['kto'],
+          nj: infolist['nj'],
+          votedisplayflag: infolist['votedisplayflag'],
+          votelinkurl: infolist['votelinkurl'],
+          inpagelinkparam: infolist['inpagelinkparam'],
+        },
+      });
+      this.logger.log(`created : ${JSON.stringify(record)}`);
     });
   }
 }
