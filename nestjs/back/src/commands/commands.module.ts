845d5f6f609a565ae711a83fa722889014582f93
kazunori_moriya <g.ff.k.96@gmail.com>
2025-10-05 11:21:54 +0900

    racelist取得コマンド枠と学習用コマンド枠追加

--- nestjs/back/src/commands/commands.module.ts
+++ nestjs/back/src/commands/commands.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ScheduleCommand } from './schedule.command';
+import { RecelistCommand } from './racelist.command';
+import { StudyCommand } from './study.command';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { LoggerModule } from 'src/logger/logger.module';
 
 @Module({
   imports: [PrismaModule, LoggerModule],
-  providers: [ScheduleCommand],
-  exports: [ScheduleCommand],
+  providers: [ScheduleCommand, RecelistCommand, StudyCommand],
+  exports: [ScheduleCommand, RecelistCommand, StudyCommand],
 })
 export class CommandsModule {}
---
+++ nestjs/back/src/commands/racelist.command.ts
@@ -0,0 +1,88 @@
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Command, CommandRunner, Option } from 'nest-commander';
+import { LoggerService } from '@nestjs/common';
+import { LoggerFactoryService } from '../logger/logger-factory.service';
+
+type CommandOptions = {
+  date?: string;
+  jid?: string;
+};
+
+@Command({
+  name: 'recelist',
+  options: { isDefault: false },
+})
+export class RecelistCommand extends CommandRunner {
+  private readonly logger: LoggerService;
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly loggerFactory: LoggerFactoryService,
+  ) {
+    super();
+    this.logger = this.loggerFactory.createLogger(RecelistCommand.name);
+  }
+
+  @Option({
+    flags: '-d --date [string]',
+    description: '日付',
+    required: true,
+  })
+  parseDate(val: string): string {
+    return val;
+  }
+  @Option({
+    flags: '-j --jid [string]',
+    description: '場id',
+    required: true,
+  })
+  parseJid(val: string): string {
+    return val;
+  }
+  async run(inputs: string[], options?: CommandOptions): Promise<void> {
+    const date = options?.date;
+    const jid = options?.jid;
+    const response = await fetch(
+      `https://www.boatrace.jp/owsp/sp/spdata?hd=${date}&jcd=${jid}&type=racelist`,
+    );
+    const json = await response.json();
+    this.logger.log(`created : ${JSON.stringify(json)}`);
+    // json['maindata']['infolist'].map(async (infolist) => {
+    //   const record = await this.prismaService.rawRacecards.create({
+    //     data: {
+    //       updatetime: json['maindata']['updatetime'],
+    //       beforeday: json['maindata']['beforeday'],
+    //       beforedayflag: json['maindata']['beforedayflag'],
+    //       hd: json['maindata']['hd'],
+    //       datamode: json['maindata']['datamode'],
+    //       today: json['maindata']['today'],
+    //       nextday: json['maindata']['nextday'],
+    //       nextdayflag: json['maindata']['nextdayflag'],
+    //       presalelinkflag: json['maindata']['presalelinkflag'],
+    //       presaleend: json['maindata']['presaleend'],
+    //       jcd: infolist['jcd'],
+    //       jname: infolist['jname'],
+    //       presaleflag: infolist['presaleflag'],
+    //       favracerflag: infolist['favracerflag'],
+    //       status: infolist['status'],
+    //       statusmessage1: infolist['statusmessage1'],
+    //       statusmessage2: infolist['statusmessage2'],
+    //       nowraceno: infolist['nowraceno'],
+    //       deadline: infolist['deadline'],
+    //       gradeicon: infolist['gradeicon'],
+    //       racelistflag: infolist['racelistflag'],
+    //       womanicon: infolist['womanicon'],
+    //       raceclscd: infolist['raceclscd'],
+    //       gradecode: infolist['gradecode'],
+    //       nightflag: infolist['nightflag'],
+    //       kfrom: infolist['kfrom'],
+    //       kto: infolist['kto'],
+    //       nj: infolist['nj'],
+    //       votedisplayflag: infolist['votedisplayflag'],
+    //       votelinkurl: infolist['votelinkurl'],
+    //       inpagelinkparam: infolist['inpagelinkparam'],
+    //     },
+    //   });
+    //   this.logger.log(`created : ${JSON.stringify(record)}`);
+    // });
+  }
+}
